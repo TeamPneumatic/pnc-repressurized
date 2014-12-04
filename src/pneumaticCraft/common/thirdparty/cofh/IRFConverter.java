867f3969683221fb514aadf9015a4db805757b89
MineMaarten <loopycoaster_8@hotmail.com>
2014-12-04 17:56:38 +0100

    Bugfix: [SMP] Flux Compressor / Pneumatic Dynamo show negative RF values when over 32000 RF. Fixes #293

--- src/pneumaticCraft/common/thirdparty/cofh/IRFConverter.java
+++ src/pneumaticCraft/common/thirdparty/cofh/IRFConverter.java
@@ -1,7 +1,11 @@
 package pneumaticCraft.common.thirdparty.cofh;
 
+import cofh.api.energy.EnergyStorage;
+
 public interface IRFConverter{
     public int getRFRate();
 
     public int getAirRate();
+
+    public EnergyStorage getEnergyStorage();
 }
