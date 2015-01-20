bb0b98c957201f8ca8863efac8df4c77ba789973
MineMaarten <loopycoaster_8@hotmail.com>
2015-01-21 00:04:02 +0100

    Added Chisel 2 support

--- src/pneumaticCraft/common/thirdparty/Chisel.java
+++ src/pneumaticCraft/common/thirdparty/Chisel.java
@@ -18,14 +18,32 @@ public class Chisel implements IThirdParty{
 
     @Override
     public void postInit(){
+        Class modClass = null;
         try {
-            Class modClass = Class.forName("info.jbcs.minecraft.chisel.Chisel");
-            PneumaticRegistry.getInstance().registerConcealableRenderId(ReflectionHelper.findField(modClass, "RenderCTMId", "renderCTMId").getInt(null));
-            PneumaticRegistry.getInstance().registerConcealableRenderId(ReflectionHelper.findField(modClass, "RenderEldritchId", "renderEldritchId").getInt(null));
-            PneumaticRegistry.getInstance().registerConcealableRenderId(ReflectionHelper.findField(modClass, "RenderCarpetId", "renderCarpetId").getInt(null));
-        } catch(Throwable e) {
-            Log.error("Chisel reflection failed:");
-            e.printStackTrace();
+            modClass = Class.forName("info.jbcs.minecraft.chisel.Chisel");
+        } catch(ClassNotFoundException e) {
+
+        }
+
+        if(modClass == null) {
+            try {
+                modClass = Class.forName("com.cricketcraft.chisel.Chisel");
+            } catch(ClassNotFoundException e) {
+
+            }
+        }
+
+        if(modClass != null) {
+            try {
+                PneumaticRegistry.getInstance().registerConcealableRenderId(ReflectionHelper.findField(modClass, "RenderCTMId", "renderCTMId").getInt(null));
+                PneumaticRegistry.getInstance().registerConcealableRenderId(ReflectionHelper.findField(modClass, "RenderEldritchId", "renderEldritchId").getInt(null));
+                PneumaticRegistry.getInstance().registerConcealableRenderId(ReflectionHelper.findField(modClass, "RenderCarpetId", "renderCarpetId").getInt(null));
+            } catch(Throwable e) {
+                Log.error("Chisel reflection failed:");
+                e.printStackTrace();
+            }
+        } else {
+            Log.error("Neither Chisel 1 or Chisel 2's main mod class could be found, even though chisel is in the instance. Report to MineMaarten plax!");
         }
     }
 
