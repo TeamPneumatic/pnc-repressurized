bbd333cda7d827d9c68ba4172e8174f019a1a7dd
MineMaarten <loopycoaster_8@hotmail.com>
2015-02-19 23:03:20 +0100

    -Added Drone Interface recipe for when Open Computers is installed, but Computercraft isn't.

--- src/pneumaticCraft/common/thirdparty/computercraft/OpenComputers.java
+++ src/pneumaticCraft/common/thirdparty/computercraft/OpenComputers.java
@@ -2,10 +2,18 @@ package pneumaticCraft.common.thirdparty.computercraft;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.oredict.ShapedOreRecipe;
 import pneumaticCraft.common.block.Blockss;
+import pneumaticCraft.common.item.ItemMachineUpgrade;
+import pneumaticCraft.common.item.Itemss;
 import pneumaticCraft.common.thirdparty.IThirdParty;
 import pneumaticCraft.common.tileentity.TileEntityProgrammer;
+import pneumaticCraft.lib.ModIds;
+import pneumaticCraft.lib.Names;
 import pneumaticCraft.proxy.ClientProxy;
+import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.registry.GameRegistry;
 
 public class OpenComputers implements IThirdParty{
@@ -21,7 +29,9 @@ public class OpenComputers implements IThirdParty{
 
     @Override
     public void init(){
-
+        if(!Loader.isModLoaded(ModIds.COMPUTERCRAFT)) {
+            GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(droneInterface), true, " u ", "mp ", "iii", 'u', new ItemStack(Itemss.machineUpgrade, 1, ItemMachineUpgrade.UPGRADE_RANGE), 'm', Items.ender_pearl, 'p', Itemss.printedCircuitBoard, 'i', Names.INGOT_IRON_COMPRESSED));
+        }
     }
 
     @Override
