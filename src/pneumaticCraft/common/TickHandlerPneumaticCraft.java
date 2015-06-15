ae62900de502cbd0bfcd389358f34319e4231897
MineMaarten <loopycoaster_8@hotmail.com>
2015-06-15 17:01:00 +0200

    -Bugfix: Lightning and Potion plants still can appear providing the right conditions. Closes #488

--- src/pneumaticCraft/common/TickHandlerPneumaticCraft.java
+++ src/pneumaticCraft/common/TickHandlerPneumaticCraft.java
@@ -3,7 +3,6 @@ package pneumaticCraft.common;
 import java.util.ArrayList;
 import java.util.List;
 
-import net.minecraft.block.Block;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.effect.EntityLightningBolt;
 import net.minecraft.entity.projectile.EntityPotion;
@@ -12,7 +11,6 @@ import net.minecraft.world.World;
 import net.minecraftforge.common.util.ForgeDirection;
 import pneumaticCraft.common.ai.DroneClaimManager;
 import pneumaticCraft.common.block.Blockss;
-import pneumaticCraft.common.block.pneumaticPlants.BlockPneumaticPlantBase;
 import pneumaticCraft.common.tileentity.TileEntityElectrostaticCompressor;
 import pneumaticCraft.lib.PneumaticValues;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
@@ -27,22 +25,7 @@ public class TickHandlerPneumaticCraft{
         if(event.phase == TickEvent.Phase.END) {
             World world = event.world;
             checkLightning(world);
-            checkPotions(world);
             DroneClaimManager.getInstance(world).update();
-        }
-    }
-
-    private void checkPotions(World world){
-        for(int i = potionEntities.size() - 1; i >= 0; i--) {
-            EntityPotion potion = potionEntities.get(i);
-            if(potion != null) {
-                if(potion.isDead) {
-                    if(world.rand.nextInt(4) == 0) tryToPlant(world, potion, Blockss.potionPlant, 1);
-                    potionEntities.remove(i);
-                }
-            } else {
-                potionEntities.remove(i);
-            }
         }
     }
 
@@ -52,7 +35,6 @@ public class TickHandlerPneumaticCraft{
         for(int i = 0; i < world.weatherEffects.size(); i++) {
             Entity entity = (Entity)world.weatherEffects.get(i);
             if(entity.ticksExisted == 1 && entity instanceof EntityLightningBolt) {
-                if(world.rand.nextInt(4) == 0) tryToPlant(world, entity, Blockss.lightningPlant, 0);
                 handleElectrostaticGeneration(world, entity);
             }
         }
@@ -101,16 +83,6 @@ public class TickHandlerPneumaticCraft{
             if(coord[0] == x && coord[1] == y && coord[2] == z) return true;
         }
         return false;
-    }
-
-    private void tryToPlant(World world, Entity entity, Block plantBlock, int heightOffset){
-        int x = (int)Math.floor(entity.posX);
-        int y = (int)Math.floor(entity.posY) + heightOffset;
-        int z = (int)Math.floor(entity.posZ);
-        Block soil = world.getBlock(x, y - 1, z);
-        if(((BlockPneumaticPlantBase)plantBlock).canPlantGrowOnThisBlock(soil, world, x, y - 1, z) && world.isAirBlock(x, y, z)) {
-            world.setBlock(x, y, z, plantBlock, 0, 3);
-        }
     }
 
 }
