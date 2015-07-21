51236ef6c0b290ec8f98a0d4e4ceeab134facb7e
MineMaarten <loopycoaster_8@hotmail.com>
2015-07-21 15:22:56 +0200

    -Bugfix: Chisel compatibility broke. Closes #562

--- src/pneumaticCraft/common/thirdparty/Chisel.java
+++ src/pneumaticCraft/common/thirdparty/Chisel.java
@@ -35,15 +35,26 @@ public class Chisel implements IThirdParty{
 
         if(modClass != null) {
             try {
-                PneumaticRegistry.getInstance().registerConcealableRenderId(ReflectionHelper.findField(modClass, "RenderCTMId", "renderCTMId").getInt(null));
-                PneumaticRegistry.getInstance().registerConcealableRenderId(ReflectionHelper.findField(modClass, "RenderEldritchId", "renderEldritchId").getInt(null));
-                PneumaticRegistry.getInstance().registerConcealableRenderId(ReflectionHelper.findField(modClass, "RenderCarpetId", "renderCarpetId").getInt(null));
+                PneumaticRegistry.getInstance().registerConcealableRenderId(ReflectionHelper.findField(modClass, "renderEldritchId").getInt(null));
+                PneumaticRegistry.getInstance().registerConcealableRenderId(ReflectionHelper.findField(modClass, "renderLayeredId").getInt(null));
+                PneumaticRegistry.getInstance().registerConcealableRenderId(ReflectionHelper.findField(modClass, "renderGlowId").getInt(null));
+                PneumaticRegistry.getInstance().registerConcealableRenderId(ReflectionHelper.findField(modClass, "roadLineId").getInt(null));
             } catch(Throwable e) {
                 Log.error("Chisel reflection failed:");
                 e.printStackTrace();
             }
         } else {
             Log.error("Neither Chisel 1 or Chisel 2's main mod class could be found, even though chisel is in the instance. Report to MineMaarten plax!");
+        }
+
+        try {
+            Class ctmlibClass = Class.forName("com.cricketcraft.ctmlib.ClientUtils");
+            PneumaticRegistry.getInstance().registerConcealableRenderId(ReflectionHelper.findField(ctmlibClass, "renderCTMId").getInt(null));
+        } catch(ClassNotFoundException e) {
+            Log.error("Chisel's ClientUtils inside ctmlib couldn't be found! No support for connected texture blocks for camouflage is given");
+        } catch(Throwable e) {
+            Log.error("Chisel reflection failed, No support for connected texture blocks for camouflage is given. Stacktrace:");
+            e.printStackTrace();
         }
     }
 
