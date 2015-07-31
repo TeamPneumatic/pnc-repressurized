0538be6e5971397e73fdff199d8ced5127184b16
MineMaarten <loopycoaster_8@hotmail.com>
2015-07-31 23:27:48 +0200

    Start on AE integration for Logistics.

--- src/pneumaticCraft/common/semiblock/SemiBlockRequester.java
+++ src/pneumaticCraft/common/semiblock/SemiBlockRequester.java
@@ -1,18 +1,42 @@
 package pneumaticCraft.common.semiblock;
 
+import java.util.EnumSet;
+
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.ChunkPosition;
+import net.minecraft.world.World;
 import net.minecraftforge.common.util.ForgeDirection;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.FluidTankInfo;
 import net.minecraftforge.fluids.IFluidHandler;
+import pneumaticCraft.common.item.Itemss;
 import pneumaticCraft.common.util.IOHelper;
+import pneumaticCraft.lib.Log;
+import pneumaticCraft.lib.ModIds;
 import pneumaticCraft.proxy.CommonProxy;
+import appeng.api.AEApi;
+import appeng.api.exceptions.FailedConnection;
+import appeng.api.networking.GridFlags;
+import appeng.api.networking.GridNotification;
+import appeng.api.networking.IGrid;
+import appeng.api.networking.IGridBlock;
+import appeng.api.networking.IGridHost;
+import appeng.api.networking.IGridNode;
+import appeng.api.util.AECableType;
+import appeng.api.util.AEColor;
+import appeng.api.util.DimensionalCoord;
+import appeng.tile.misc.TileInterface;
+import cpw.mods.fml.common.Loader;
+import cpw.mods.fml.common.Optional;
+import cpw.mods.fml.common.Optional.Interface;
 
-public class SemiBlockRequester extends SemiBlockLogistics implements ISpecificRequester{
+@Optional.InterfaceList({@Interface(iface = "appeng.api.networking.IGridHost", modid = ModIds.AE2), @Interface(iface = "appeng.api.networking.IGridBlock", modid = ModIds.AE2)})
+public class SemiBlockRequester extends SemiBlockLogistics implements ISpecificRequester, IGridHost, IGridBlock{
 
     public static final String ID = "logisticFrameRequester";
+    private Object gridNode;
 
     @Override
     public int getColor(){
@@ -113,6 +137,121 @@ public class SemiBlockRequester extends SemiBlockLogistics implements ISpecificR
     @Override
     public boolean canFilterStack(){
         return true;
+    }
+
+    /*
+     ****************************************** Applied Energistics 2 Integration ***************************************************************
+     */
+
+    @Override
+    public void initialize(World world, ChunkPosition pos){
+        super.initialize(world, pos);
+        if(Loader.isModLoaded(ModIds.AE2) && !world.isRemote) checkForInterface();
+    }
+
+    @Override
+    public void invalidate(){
+        super.invalidate();
+        if(gridNode != null) {
+            disconnectFromInterface();
+        }
+    }
+
+    private void checkForInterface(){
+        TileEntity te = getTileEntity();
+        if(te instanceof TileInterface) {
+            try {
+                AEApi.instance().createGridConnection(getGridNode(null), ((TileInterface)te).getGridNode(null));
+                Log.info("connection created");
+            } catch(FailedConnection e) {
+                Log.info("Couldn't connect to an ME Interface!");
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void disconnectFromInterface(){
+        ((IGridNode)gridNode).destroy();
+    }
+
+    //IGridHost
+    @Override
+    @Optional.Method(modid = ModIds.AE2)
+    public AECableType getCableConnectionType(ForgeDirection arg0){
+        return AECableType.NONE;
+    }
+
+    @Override
+    @Optional.Method(modid = ModIds.AE2)
+    public IGridNode getGridNode(ForgeDirection d){
+        if(gridNode == null) {
+            gridNode = AEApi.instance().createGridNode(this);
+        }
+        return (IGridNode)gridNode;
+    }
+
+    @Override
+    public void securityBreak(){
+        drop();
+    }
+
+    //IGridBlock
+    @Override
+    public EnumSet<ForgeDirection> getConnectableSides(){
+        return null;//Shouldn't be called as isWorldAccessible is false.
+    }
+
+    @Override
+    public EnumSet<GridFlags> getFlags(){
+        return EnumSet.noneOf(GridFlags.class);
+    }
+
+    @Override
+    public AEColor getGridColor(){
+        return AEColor.Transparent;
+    }
+
+    @Override
+    public double getIdlePowerUsage(){
+        return 0;
+    }
+
+    @Override
+    public DimensionalCoord getLocation(){
+        return new DimensionalCoord(world, getX(), getY(), getZ());
+    }
+
+    @Override
+    public IGridHost getMachine(){
+        return this;
+    }
+
+    @Override
+    public ItemStack getMachineRepresentation(){
+        return new ItemStack(Itemss.logisticsFrameRequester);
+    }
+
+    @Override
+    public void gridChanged(){
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public boolean isWorldAccessible(){
+        return false;
+    }
+
+    @Override
+    public void onGridNotification(GridNotification arg0){
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void setNetworkStatus(IGrid arg0, int arg1){
+        // TODO Auto-generated method stub
+
     }
 
 }
