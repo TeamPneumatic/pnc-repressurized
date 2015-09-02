a738c116ecb5f329a0144c1111071a7b8ef2e334
MineMaarten <loopycoaster_8@hotmail.com>
2015-09-02 19:14:31 +0200

    -Added 'Check for Air' option for the Condition: Block piece. Closes #658

--- src/pneumaticCraft/common/progwidgets/ProgWidgetBlockCondition.java
+++ src/pneumaticCraft/common/progwidgets/ProgWidgetBlockCondition.java
@@ -1,10 +1,16 @@
 package pneumaticCraft.common.progwidgets;
 
+import java.util.List;
+
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.ChunkPosition;
 import pneumaticCraft.client.gui.GuiProgrammer;
 import pneumaticCraft.client.gui.programmer.GuiProgWidgetCondition;
+import pneumaticCraft.client.gui.widget.GuiCheckBox;
+import pneumaticCraft.client.gui.widget.IGuiWidget;
 import pneumaticCraft.common.ai.DroneAIBlockCondition;
 import pneumaticCraft.common.ai.DroneAIDig;
 import pneumaticCraft.common.ai.IDroneBase;
@@ -13,6 +19,7 @@ import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
 public class ProgWidgetBlockCondition extends ProgWidgetCondition{
+    private boolean checkingForAir;
 
     @Override
     public String getWidgetString(){
@@ -30,7 +37,11 @@ public class ProgWidgetBlockCondition extends ProgWidgetCondition{
 
             @Override
             protected boolean evaluate(ChunkPosition pos){
-                return DroneAIDig.isBlockValidForFilter(drone.getWorld(), drone, pos, widget);
+                if(checkingForAir) {
+                    return drone.getWorld().isAirBlock(pos.chunkPosX, pos.chunkPosY, pos.chunkPosZ);
+                } else {
+                    return DroneAIDig.isBlockValidForFilter(drone.getWorld(), drone, pos, widget);
+                }
             }
 
         };
@@ -41,6 +52,12 @@ public class ProgWidgetBlockCondition extends ProgWidgetCondition{
     public GuiScreen getOptionWindow(GuiProgrammer guiProgrammer){
         return new GuiProgWidgetCondition(this, guiProgrammer){
             @Override
+            public void initGui(){
+                super.initGui();
+                addWidget(new GuiCheckBox(500, guiLeft + 5, guiTop + 60, 0xFF000000, I18n.format("gui.progWidget.conditionBlock.checkForAir")).setChecked(checkingForAir).setTooltip(I18n.format("gui.progWidget.conditionBlock.checkForAir.tooltip")));
+            }
+
+            @Override
             protected boolean requiresNumber(){
                 return false;
             }
@@ -49,12 +66,39 @@ public class ProgWidgetBlockCondition extends ProgWidgetCondition{
             protected boolean isSidedWidget(){
                 return false;
             }
+
+            @Override
+            public void actionPerformed(IGuiWidget widget){
+                if(widget.getID() == 500) checkingForAir = !checkingForAir;
+                else super.actionPerformed(widget);
+            }
+
         };
     }
 
     @Override
     protected ResourceLocation getTexture(){
         return Textures.PROG_WIDGET_CONDITION_BLOCK;
+    }
+
+    @Override
+    public void writeToNBT(NBTTagCompound tag){
+        super.writeToNBT(tag);
+        tag.setBoolean("checkingForAir", checkingForAir);
+    }
+
+    @Override
+    public void readFromNBT(NBTTagCompound tag){
+        super.readFromNBT(tag);
+        checkingForAir = tag.getBoolean("checkingForAir");
+    }
+
+    @Override
+    public void addErrors(List<String> curInfo){
+        super.addErrors(curInfo);
+        if(checkingForAir && (getConnectedParameters()[1] != null || getConnectedParameters()[getParameters().length * 2 - 2] != null)) {
+            curInfo.add("gui.progWidget.conditionBlock.selectedAirAndItemFilter");
+        }
     }
 
 }
