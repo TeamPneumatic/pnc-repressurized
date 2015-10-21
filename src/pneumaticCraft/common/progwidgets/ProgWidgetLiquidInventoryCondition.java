2d4861f58926b24c82613536702ec9f3643c303b
MineMaarten <loopycoaster_8@hotmail.com>
2015-10-21 17:49:48 +0200

    -The Liquid Condition now also considers fluid blocks. Closes #737

--- src/pneumaticCraft/common/progwidgets/ProgWidgetLiquidInventoryCondition.java
+++ src/pneumaticCraft/common/progwidgets/ProgWidgetLiquidInventoryCondition.java
@@ -7,11 +7,14 @@ import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.ChunkPosition;
 import net.minecraftforge.common.util.ForgeDirection;
+import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.FluidTankInfo;
 import net.minecraftforge.fluids.IFluidHandler;
 import pneumaticCraft.common.ai.DroneAIBlockCondition;
 import pneumaticCraft.common.ai.IDroneBase;
+import pneumaticCraft.common.util.FluidUtils;
 import pneumaticCraft.lib.Textures;
 
 public class ProgWidgetLiquidInventoryCondition extends ProgWidgetCondition{
@@ -33,9 +36,9 @@ public class ProgWidgetLiquidInventoryCondition extends ProgWidgetCondition{
             @Override
             protected boolean evaluate(ChunkPosition pos){
                 TileEntity te = drone.getWorld().getTileEntity(pos.chunkPosX, pos.chunkPosY, pos.chunkPosZ);
+                int count = 0;
                 if(te instanceof IFluidHandler) {
                     List<FluidStack> visitedStacks = new ArrayList<FluidStack>();
-                    int count = 0;
                     IFluidHandler inv = (IFluidHandler)te;
                     for(int i = 0; i < 6; i++) {
                         if(((ISidedWidget)widget).getSides()[i]) {
@@ -50,9 +53,13 @@ public class ProgWidgetLiquidInventoryCondition extends ProgWidgetCondition{
                             }
                         }
                     }
-                    return ((ICondition)widget).getOperator() == ICondition.Operator.EQUALS ? count == ((ICondition)widget).getRequiredCount() : count >= ((ICondition)widget).getRequiredCount();
+                } else {
+                    Fluid fluid = FluidRegistry.lookupFluidForBlock(drone.getWorld().getBlock(pos.chunkPosX, pos.chunkPosY, pos.chunkPosZ));
+                    if(fluid != null && ProgWidgetLiquidFilter.isLiquidValid(fluid, widget, 1) && FluidUtils.isSourceBlock(drone.getWorld(), pos.chunkPosX, pos.chunkPosY, pos.chunkPosZ)) {
+                        count += 1000;
+                    }
                 }
-                return false;
+                return ((ICondition)widget).getOperator() == ICondition.Operator.EQUALS ? count == ((ICondition)widget).getRequiredCount() : count >= ((ICondition)widget).getRequiredCount();
             }
 
         };
