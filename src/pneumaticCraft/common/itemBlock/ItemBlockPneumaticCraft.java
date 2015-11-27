d3e95a75b8d1bd2de0c27bec5880268efc4dd146
MineMaarten <loopycoaster_8@hotmail.com>
2015-11-27 12:26:13 +0100

    -Bugfix: Log messages about blocks missing tooltips. Closes #765

--- src/pneumaticCraft/common/itemBlock/ItemBlockPneumaticCraft.java
+++ src/pneumaticCraft/common/itemBlock/ItemBlockPneumaticCraft.java
@@ -3,10 +3,13 @@ package pneumaticCraft.common.itemBlock;
 import java.util.List;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockAir;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.fluids.BlockFluidBase;
 import pneumaticCraft.common.block.BlockPneumaticCraft;
+import pneumaticCraft.common.block.pneumaticPlants.BlockPneumaticPlantBase;
 import pneumaticCraft.lib.Log;
 
 public class ItemBlockPneumaticCraft extends ItemBlock{
@@ -17,7 +20,9 @@ public class ItemBlockPneumaticCraft extends ItemBlock{
         if(block instanceof BlockPneumaticCraft) {
             this.block = (BlockPneumaticCraft)block;
         } else {
-            Log.warning("Block " + block.getUnlocalizedName() + " does not extend BlockPneumaticCraft! No tooltip displayed");
+            if(!(block instanceof BlockPneumaticPlantBase) && !(block instanceof BlockAir) && !(block instanceof BlockFluidBase)) {
+                Log.warning("Block " + block.getUnlocalizedName() + " does not extend BlockPneumaticCraft! No tooltip displayed");
+            }
         }
     }
 
