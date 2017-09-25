71ca39df440e7f6c118562a1677cfed66632b435
Des Herriott <des.herriott@gmail.com>
2017-09-25 14:28:29 +0100

    Re-enabled base lua methods

--- src/main/java/me/desht/pneumaticcraft/common/tileentity/TileEntityPneumaticBase.java
+++ src/main/java/me/desht/pneumaticcraft/common/tileentity/TileEntityPneumaticBase.java
@@ -4,6 +4,8 @@ import me.desht.pneumaticcraft.api.PneumaticRegistry;
 import me.desht.pneumaticcraft.api.tileentity.IAirHandler;
 import me.desht.pneumaticcraft.common.block.tubes.IPneumaticPosProvider;
 import me.desht.pneumaticcraft.common.network.GuiSynced;
+import me.desht.pneumaticcraft.common.thirdparty.computercraft.LuaConstant;
+import me.desht.pneumaticcraft.common.thirdparty.computercraft.LuaMethod;
 import net.minecraft.item.Item;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumFacing;
@@ -67,27 +69,27 @@ public class TileEntityPneumaticBase extends TileEntityBase implements IPneumati
         airHandler.onNeighborChange();
     }
 
-//    @Override
-//    protected void addLuaMethods() {
-//        super.addLuaMethods();
-//        luaMethods.add(new LuaMethod("getPressure") {
-//            @Override
-//            public Object[] call(Object[] args) throws Exception {
-//                if (args.length == 0) {
-//                    return new Object[]{airHandler.getPressure()};
-//                } else if (args.length == 1) {
-//                    IAirHandler handler = getAirHandler(getDirForString((String) args[0]));
-//                    return new Object[]{handler != null ? handler.getPressure() : 0};
-//                } else {
-//                    throw new IllegalArgumentException("getPressure method requires 0 or 1 argument (direction: up, down, east, west, north, south!");
-//                }
-//            }
-//        });
-//
-//        luaMethods.add(new LuaConstant("getDangerPressure", dangerPressure));
-//        luaMethods.add(new LuaConstant("getCriticalPressure", criticalPressure));
-//        luaMethods.add(new LuaConstant("getDefaultVolume", defaultVolume));
-//    }
+    @Override
+    protected void addLuaMethods() {
+        super.addLuaMethods();
+        luaMethods.add(new LuaMethod("getPressure") {
+            @Override
+            public Object[] call(Object[] args) throws Exception {
+                if (args.length == 0) {
+                    return new Object[]{airHandler.getPressure()};
+                } else if (args.length == 1) {
+                    IAirHandler handler = getAirHandler(getDirForString((String) args[0]));
+                    return new Object[]{handler != null ? handler.getPressure() : 0};
+                } else {
+                    throw new IllegalArgumentException("getPressure method requires 0 or 1 argument (direction: up, down, east, west, north, south)!");
+                }
+            }
+        });
+
+        luaMethods.add(new LuaConstant("getDangerPressure", dangerPressure));
+        luaMethods.add(new LuaConstant("getCriticalPressure", criticalPressure));
+        luaMethods.add(new LuaConstant("getDefaultVolume", defaultVolume));
+    }
 
     /*
      * End ComputerCraft API 
