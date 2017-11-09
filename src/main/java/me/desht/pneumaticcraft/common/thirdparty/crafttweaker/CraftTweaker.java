2dbfc1eeefaafcb4c6fab36cbf9369fcde580e57
Zixxl <Zixxl@users.noreply.github.com>
2017-11-09 12:14:06 +0100

    Apply recipe changes in post init phase

--- src/main/java/me/desht/pneumaticcraft/common/thirdparty/crafttweaker/CraftTweaker.java
+++ src/main/java/me/desht/pneumaticcraft/common/thirdparty/crafttweaker/CraftTweaker.java
@@ -1,9 +1,18 @@
 package me.desht.pneumaticcraft.common.thirdparty.crafttweaker;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import crafttweaker.CraftTweakerAPI;
+import crafttweaker.IAction;
 import me.desht.pneumaticcraft.common.thirdparty.IThirdParty;
 
 public class CraftTweaker implements IThirdParty {
 	
+    public static final List<IAction> REMOVALS = new LinkedList<>();
+    public static final List<IAction> ADDITIONS = new LinkedList<>();
+
+
     @Override
     public void preInit() {
     }
@@ -14,6 +23,8 @@ public class CraftTweaker implements IThirdParty {
 
     @Override
     public void postInit() {
+    	REMOVALS.forEach(CraftTweakerAPI::apply);
+    	ADDITIONS.forEach(CraftTweakerAPI::apply);
     }
 
     @Override
