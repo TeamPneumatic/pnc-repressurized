d7b7d379fc69b0c3c3f694b8939708045b9294ea
Zixxl <Zixxl@users.noreply.github.com>
2017-11-15 15:10:55 +0100

    Splitted api to own source folder desht/pnc-repressurized#27

--- src/api/java/me/desht/pneumaticcraft/api/tileentity/IPneumaticMachine.java
+++ src/api/java/me/desht/pneumaticcraft/api/tileentity/IPneumaticMachine.java
@@ -1,13 +1,12 @@
 package me.desht.pneumaticcraft.api.tileentity;
 
-import me.desht.pneumaticcraft.common.pressure.AirHandlerSupplier;
 import net.minecraft.util.EnumFacing;
 
 public interface IPneumaticMachine {
 
     /**
      * In your TileEntity class which is implementing this interface you should keep a reference of an IAirHandler.
-     * You can retrieve one by calling {@link AirHandlerSupplier#getAirHandler(net.minecraft.tileentity.TileEntity, float, float, float, float)}.
+     * You can retrieve one by calling {@link IPneumaticMachine#getAirHandler(net.minecraft.tileentity.TileEntity, float, float, float, float)}.
      * Do this when your TileEntity is initialized, i.e. getPos().getX(),getPos().getY(),getPos().getZ() and getWorld() have a value.
      * In this method you need to return this reference.
      * <p>
