2984c752cde1ce3e4706994968d3a9b07cd3a4ca
MineMaarten <loopycoaster_8@hotmail.com>
2017-11-18 18:03:34 +0100

    Bugfix: Drones getting stuck behind a wall (because the native MC flying pathfinding now has a smoothing over the path points which doesn't work well with Drones).

--- src/main/java/me/desht/pneumaticcraft/common/ai/NodeProcessorDrone.java
+++ src/main/java/me/desht/pneumaticcraft/common/ai/NodeProcessorDrone.java
@@ -3,6 +3,7 @@ package me.desht.pneumaticcraft.common.ai;
 import me.desht.pneumaticcraft.common.entity.living.EntityDrone;
 import net.minecraft.pathfinding.FlyingNodeProcessor;
 import net.minecraft.pathfinding.PathPoint;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 
 import javax.annotation.Nullable;
@@ -12,5 +13,23 @@ public class NodeProcessorDrone extends FlyingNodeProcessor {
     @Override
     protected PathPoint openPoint(int x, int y, int z) {
         return ((EntityDrone) entity).isBlockValidPathfindBlock(new BlockPos(x, y, z)) ? super.openPoint(x, y, z) : null;
+    }
+    
+    /**
+     * Override this, because the super method adds diagonals, this is fancy but doesn't work well with drones (drones stuck behind a wall).
+     */
+    @Override
+    public int findPathOptions(PathPoint[] pathOptions, PathPoint currentPoint, PathPoint targetPoint, float maxDistance)
+    {
+        int i = 0;
+        
+        for(EnumFacing dir : EnumFacing.VALUES){
+            PathPoint point = openPoint(currentPoint.x + dir.getFrontOffsetX(), currentPoint.y + dir.getFrontOffsetY(), currentPoint.z + dir.getFrontOffsetZ());
+            if(point != null && !point.visited && point.distanceTo(targetPoint) < maxDistance){
+                pathOptions[i++] = point;
+            }
+        }
+
+        return i;
     }
 }
