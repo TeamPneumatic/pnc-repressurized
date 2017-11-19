6eb15e2c90dc225065163da8bd73eb4040a2cb95
MineMaarten <loopycoaster_8@hotmail.com>
2017-11-19 13:26:54 +0100

    Bugfix: Unopened loot chests cause the Block Tracker to fail, because their loot isn't generated yet and the client can't do it.

--- src/main/java/me/desht/pneumaticcraft/common/network/PacketDescriptionPacketRequest.java
+++ src/main/java/me/desht/pneumaticcraft/common/network/PacketDescriptionPacketRequest.java
@@ -3,6 +3,7 @@ package me.desht.pneumaticcraft.common.network;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.tileentity.TileEntityLockableLoot;
 import net.minecraft.util.math.BlockPos;
 
 public class PacketDescriptionPacketRequest extends LocationIntPacket<PacketDescriptionPacketRequest> {
@@ -22,8 +23,20 @@ public class PacketDescriptionPacketRequest extends LocationIntPacket<PacketDesc
     public void handleServerSide(PacketDescriptionPacketRequest message, EntityPlayer player) {
         TileEntity te = message.getTileEntity(player.world);
         if (te != null) {
+            forceLootGeneration(te);
             NetworkHandler.sendTo(new PacketSendNBTPacket(te), (EntityPlayerMP) player);
         }
     }
-
+    
+    /**
+     * Force loot generation, as this is required on the client side to peek inside inventories.
+     * The client is not able to generate the loot.
+     * @param te
+     */
+    private void forceLootGeneration(TileEntity te){
+        if(te instanceof TileEntityLockableLoot){
+            TileEntityLockableLoot teLoot = (TileEntityLockableLoot)te;
+            teLoot.fillWithLoot(null);
+        }
+    }
 }
