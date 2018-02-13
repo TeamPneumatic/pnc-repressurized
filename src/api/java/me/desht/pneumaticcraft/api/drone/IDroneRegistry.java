TeamPneumatic/pnc-repressurized

commit 5e60a08c5161904e3530185109e7c4de97eefae7

Fixed bug where RF import/export pieces weren't getting registered. Also a little API docs cleanup See #111

src/api/java/me/desht/pneumaticcraft/api/drone/IDroneRegistry.java
@@ -13,61 +13,66 @@ public interface IDroneRegistry {
      * With this method you can register custom blocks to allow the drone to pathfind through them. If the block requires any special
      * handling, like allow pathfinding on certain conditions, you can pass a IPathFindHandler with the registry.
      *
-     * @param block
+     * @param block the block to allow pathfinding through
      * @param handler can be null, to always allow pathfinding through this block.
      */
     void addPathfindableBlock(Block block, IPathfindHandler handler);
 
     /**
      * This will add a puzzle piece that has only a Area white- and blacklist parameter (similar to a GoTo piece).
-     * It will do the specified behaviour. This can be used to create energy import/export widgets.
+     * It will do the specified behaviour. This can be used to create energy import/export widgets.  This must be called
+     * in the pre-init phase, or registration will be silently ignored.
      *
-     * @param interactor
+     * @param interactor the custom interactor object
      */
     void registerCustomBlockInteractor(ICustomBlockInteract interactor);
 
     /**
-     * Will spawn in a Drone a distance away from the given coordinate. When there is an inventory at the given x,y,z the drone will export the items in there. If there isn't or items don't fit, the drone will travel to 5 blocks above the specified
-     * y level, and drop the deliveredStacks. When there isn't a clear path for the items to fall these 5 blocks the Drone will deliver at a
-     * y level above the specified y that _is_ clear. If no clear blocks can be found (when there are only solid blocks), the Drone will
-     * drop the items very high up in the air instead, and drop them there.
+     * Will spawn in a Drone a distance away from the given coordinate. When there is an inventory at the given block
+     * position, the drone will export the items there. If there is no inventory or items don't fit, the drone will
+     * travel to 5 blocks above the specified Y level, and drop the deliveredStacks. When there isn't a clear path for
+     * the items to fall these 5 blocks the Drone will deliver at a Y level above the specified Y that <em>is</em>
+     * clear. If no clear blocks can be found (when there are only solid blocks), the Drone will drop the items very
+     * high up in the air instead.
      * <p>
-     * When the Drone is tried to be caught by a player (by wrenching it), the drone will only the drop the items that it was delivering (or
-     * none if it dropped those items already). The Drone itself never will be dropped.
+     * When a player attempts to catch the drone (by wrenching it), the drone will only the drop the items that it was
+     * delivering (or none if it dropped those items already). The Drone itself never will be dropped.
      *
-     * @param pos
-     * @param deliveredStacks stacks that are delivered by the drone. When no stacks, or more than 65 stacks are given, this will generate an IllegalArgumentException.
-     * @return the drone. You can use this to set a custom name for example (defaults to "Amadron Delivery Drone").
+     * @param world the world
+     * @param pos position to deliver items to
+     * @param deliveredStacks stacks that are delivered by the drone; when no stacks, or more than 65 stacks are given, this will generate an IllegalArgumentException.
+     * @return the drone; you can use this to set a custom name for example (defaults to "Amadron Delivery Drone").
      */
     EntityCreature deliverItemsAmazonStyle(World world, BlockPos pos, ItemStack... deliveredStacks);
 
     /**
      * The opposite of deliverItemsAmazonStyle. Will retrieve the queried items from an inventory at the specified location.
      *
-     * @param world
-     * @param pos
-     * @param queriedStacks
-     * @return
+     * @param world the world
+     * @param pos the position to retrieve items from
+     * @param queriedStacks the stacks to retrieve
+     * @return the drone
      */
     EntityCreature retrieveItemsAmazonStyle(World world, BlockPos pos, ItemStack... queriedStacks);
 
     /**
-     * Similar to deliverItemsAmazonStyle, but with Fluids. Will spawn in a Drone that will fill an IFluidHandler at the given x,y,z. If the fluid doesn't fit or there isn't a IFluidHandler, the fluid is lost.
+     * Similar to deliverItemsAmazonStyle, but with Fluids. Will spawn in a Drone that will fill an IFluidHandler at the
+     * given block position. If the fluid doesn't fit or there isn't a IFluidHandler, the fluid <em>will be lost</em>.
      *
-     * @param world
-     * @param pos
-     * @param deliveredFluid
-     * @return
+     * @param world the world
+     * @param pos the position to delivery the fluid to
+     * @param deliveredFluid the fluid to deliver
+     * @return the drone
      */
     EntityCreature deliverFluidAmazonStyle(World world, BlockPos pos, FluidStack deliveredFluid);
 
     /**
      * The opposite of deliverFluidAmazonStyle. Will retrieve the queried fluid from an IFluidHandler at the specified location.
      *
-     * @param world
-     * @param pos
-     * @param queriedStacks
-     * @return
+     * @param world the world
+     * @param pos the block position to retrieve fluid from
+     * @param queriedFluid the fluid to retrieve
+     * @return the drone
      */
     EntityCreature retrieveFluidAmazonStyle(World world, BlockPos pos, FluidStack queriedFluid);
 }

src/main/java/me/desht/pneumaticcraft/PneumaticCraftRepressurized.java
@@ -9,8 +9,6 @@ import me.desht.pneumaticcraft.common.EventHandlerPneumaticCraft;
 import me.desht.pneumaticcraft.common.EventHandlerUniversalSensor;
 import me.desht.pneumaticcraft.common.PneumaticCraftAPIHandler;
 import me.desht.pneumaticcraft.common.TickHandlerPneumaticCraft;
-import me.desht.pneumaticcraft.common.ai.DroneInteractRFExport;
-import me.desht.pneumaticcraft.common.ai.DroneInteractRFImport;
 import me.desht.pneumaticcraft.common.block.Blockss;
 import me.desht.pneumaticcraft.common.commands.PCCommandManager;
 import me.desht.pneumaticcraft.common.config.ConfigHandler;
@@ -102,10 +100,6 @@ public class PneumaticCraftRepressurized {
         tabPneumaticCraft = new CreativeTabPneumaticCraft("tabPneumaticCraft");
         Fluids.preInit();
         ThirdPartyManager.instance().preInit();
-        WidgetRegistrator.compileBlacklist();  // compile this after 3rd party pre-init has happened
-
-        PneumaticRegistry.getInstance().getDroneRegistry().registerCustomBlockInteractor(new DroneInteractRFExport());
-        PneumaticRegistry.getInstance().getDroneRegistry().registerCustomBlockInteractor(new DroneInteractRFImport());
 
         TileEntityRegistrator.init();
         EntityRegistrator.init();
@@ -129,6 +123,8 @@ public class PneumaticCraftRepressurized {
     @EventHandler
     public void onInit(FMLInitializationEvent event) {
         NetworkHandler.init();
+
+        WidgetRegistrator.compileBlacklist();  // compile this after 3rd party pre-init has happened
 
         Fluids.init();
         SemiBlockInitializer.init();

src/main/java/me/desht/pneumaticcraft/common/progwidgets/WidgetRegistrator.java
@@ -1,5 +1,7 @@
 package me.desht.pneumaticcraft.common.progwidgets;
 
+import me.desht.pneumaticcraft.common.ai.DroneInteractRFExport;
+import me.desht.pneumaticcraft.common.ai.DroneInteractRFImport;
 import me.desht.pneumaticcraft.common.config.ProgWidgetConfig;
 
 import java.util.*;
@@ -62,6 +64,10 @@ public class WidgetRegistrator {
         register(new ProgWidgetDroneConditionPressure());
         register(new ProgWidgetRFCondition());
         register(new ProgWidgetDroneConditionRF());
+
+        // TODO: should really be their own widgets, but historical reasons...
+        register(new ProgWidgetCustomBlockInteract().setInteractor(new DroneInteractRFExport()));
+        register(new ProgWidgetCustomBlockInteract().setInteractor(new DroneInteractRFImport()));
     }
 
     public static void register(IProgWidget widget) {

src/api/java/me/desht/pneumaticcraft/api/PneumaticRegistry.java
@@ -20,10 +20,19 @@ import net.minecraftforge.fml.common.Loader;
 public final class PneumaticRegistry {
     private static IPneumaticCraftInterface instance;
 
+    /**
+     * Get an instance of the top-level API interface.
+     *
+     * @return the top-level API interface
+     */
     public static IPneumaticCraftInterface getInstance() {
         return instance;
     }
 
+    /**
+     * Do not call this method yourself.  For PneumaticCraft internal usage only!
+     * @param inter reference to the API interface object
+     */
     public static void init(IPneumaticCraftInterface inter) {
         if (instance == null && Loader.instance().activeModContainer().getModId().equals("pneumaticcraft"))
             instance = inter;//only allow initialization once; by PneumaticCraft
@@ -67,7 +76,7 @@ public final class PneumaticRegistry {
          */
 
         /**
-         * Returns the amount of Security Stations that disallow interaction with the given coordinate for the given player.
+         * Returns the number of Security Stations that disallow interaction with the given coordinate for the given player.
          * Usually you'd disallow interaction when this returns > 0.
          *
          * @param world

src/api/java/me/desht/pneumaticcraft/api/drone/IBlockInteractHandler.java
@@ -6,7 +6,9 @@ package me.desht.pneumaticcraft.api.drone;
 public interface IBlockInteractHandler {
 
     /**
-     * Returns a boolean[6] of all sides. when true, this side is accessible
+     * Returns a boolean[6] of all sides. when true, this side is accessible.  The sides are in order D,U,N,S,W,E,
+     * same as for EnumFacing.  You can use {@link net.minecraft.util.EnumFacing#getFront(int)} with an index into this
+     * array to get the facing direction.
      *
      * @return
      */

src/api/java/me/desht/pneumaticcraft/api/drone/ICustomBlockInteract.java
@@ -4,7 +4,7 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 
 /**
- * Implement this and register it to PneumaticRegistry.registerCustomBlockInteractor().
+ * Implement this and register it with {@link IDroneRegistry#registerCustomBlockInteractor(ICustomBlockInteract)}.
  * This will add a puzzle piece that has only a Area white- and blacklist parameter (similar to a GoTo piece).
  * It will do the specified behaviour. This can be used to create energy import/export widgets.
  */
@@ -16,7 +16,8 @@ public interface ICustomBlockInteract {
     String getName();
 
     /**
-     * Should return the puzzle piece texture. Should be a multiple of 80x64 (width x height). I'd recommend starting out with copying the Go To widget texture.
+     * Should return the puzzle piece texture. Should be a multiple of 80x64 (width x height). I'd recommend starting
+     * out with copying the Go To widget texture.
      *
      * @return
      */
@@ -31,9 +32,9 @@ public interface ICustomBlockInteract {
      * In the interface of the puzzle piece users can specify a 'use count' and fill in the maximum count they want to use. When not simulating, you should only import/export up to interactHandler.getRemainingCount(), and you should notify the removed/added count by doing interactHandler.decreaseCount(int count).
      *
      * @param pos             current visited location
-     * @param drone
+     * @param drone a reference to the drone object
      * @param interactHandler object you can use to use to get accessible sides and give feedback about counts.
-     * @param simulate        will be true when trying to figure out whether or not the drone should navigate to this block, false when next to this block.
+     * @param simulate        true when trying to figure out whether or not the drone should navigate to this block, false when next to this block.
      * @return
      */
     boolean doInteract(BlockPos pos, IDrone drone, IBlockInteractHandler interactHandler, boolean simulate);

src/api/java/me/desht/pneumaticcraft/api/tileentity/IAirHandlerSupplier.java
@@ -9,17 +9,16 @@ public interface IAirHandlerSupplier {
      * Returns a new instance of an IAirHandler. This handler handles everything pressurized air related: Air dispersion,
      * blowing up when the pressure gets too high, providing a method for releasing air into the atmosphere...
      * PROVIDED THAT THE FOLLOWING METHODS ARE FORWARDED TO THIS INSTANCE:
-     * {@link net.minecraft.tileentity.TileEntity#update()},
+     * {@link net.minecraft.util.ITickable#update()},
      * {@link net.minecraft.tileentity.TileEntity#writeToNBT(net.minecraft.nbt.NBTTagCompound)}
      * {@link net.minecraft.tileentity.TileEntity#readFromNBT(net.minecraft.nbt.NBTTagCompound)}
      * {@link net.minecraft.tileentity.TileEntity#validate()}
      *
      * @param dangerPressure   minimal pressure on which this machine can explode (the yellow to red transition)
      * @param criticalPressure the absolute maximum pressure the machine can take 7 bar in tier 1 machines.
-     * @param maxFlow          maximum mL/tick that this machine can disperse. Tier one machines do 50mL/tick while Tier two have 200mL/tick.
      * @param volume           Volume of the machine's internal storage. These vary from 1000mL for small machines to 10,000mL for the big ones.
      *                         The higher the volume the slower the machine will charge/discharge.
-     * @return
+     * @return the air handler
      */
     IAirHandler createAirHandler(float dangerPressure, float criticalPressure, int volume);
 }

commit 6acf3014a91270827b977b67492405306a7107f8

Fix missing puzzle piece textures caused by https://github.com/TeamPneumatic/pnc-repressurized/commit/5e60a08c5161904e3530185109e7c4de97eefae7
Final widget list calculation (after any blacklisting) must be done in
preInit, not init, since models are registered between preInit & init.
Added a PuzzleRegistryEvent to give other mods a chance to register any
custom pieces at the right time.

src/api/java/me/desht/pneumaticcraft/api/drone/IDroneRegistry.java
@@ -21,7 +21,8 @@ public interface IDroneRegistry {
     /**
      * This will add a puzzle piece that has only a Area white- and blacklist parameter (similar to a GoTo piece).
      * It will do the specified behaviour. This can be used to create energy import/export widgets.  This must be called
-     * in the pre-init phase, or registration will be silently ignored.
+     * from an event handler for {@link me.desht.pneumaticcraft.api.event.PuzzleRegistryEvent} to ensure registration
+     * is done at the right time - don't call it directly from a (pre/post) init handler.
      *
      * @param interactor the custom interactor object
      */

src/api/java/me/desht/pneumaticcraft/api/event/PuzzleRegistryEvent.java
@@ -1,0 +1,22 @@
+package me.desht.pneumaticcraft.api.event;
+
+import me.desht.pneumaticcraft.api.PneumaticRegistry;
+import me.desht.pneumaticcraft.api.drone.ICustomBlockInteract;
+import me.desht.pneumaticcraft.api.drone.IDroneRegistry;
+import net.minecraftforge.fml.common.eventhandler.Event;
+
+/**
+ * Fired just before the finalised puzzle piece list is called.  If you need to register custom puzzle pieces, do it
+ * here, via {@link PuzzleRegistryEvent#getDroneRegistry()} and {@link IDroneRegistry#registerCustomBlockInteractor(ICustomBlockInteract)}
+ */
+public class PuzzleRegistryEvent extends Event {
+    private final IDroneRegistry droneRegistry;
+
+    public PuzzleRegistryEvent() {
+        droneRegistry = PneumaticRegistry.getInstance().getDroneRegistry();
+    }
+
+    public IDroneRegistry getDroneRegistry() {
+        return droneRegistry;
+    }
+}

src/main/java/me/desht/pneumaticcraft/PneumaticCraftRepressurized.java
@@ -91,14 +91,18 @@ public class PneumaticCraftRepressurized {
         Reflections.init();
 
         PneumaticRegistry.init(PneumaticCraftAPIHandler.getInstance());
+
         UpgradeRenderHandlerList.init();
+
         WidgetRegistrator.init();
+
         ConfigHandler.init(event.getSuggestedConfigurationFile());
         ThirdPartyManager.instance().index();
 
         NetworkRegistry.INSTANCE.registerGuiHandler(instance, proxy);
         tabPneumaticCraft = new CreativeTabPneumaticCraft("tabPneumaticCraft");
         Fluids.preInit();
+
         ThirdPartyManager.instance().preInit();
 
         TileEntityRegistrator.init();
@@ -124,7 +128,6 @@ public class PneumaticCraftRepressurized {
     public void onInit(FMLInitializationEvent event) {
         NetworkHandler.init();
 
-        WidgetRegistrator.compileBlacklist();  // compile this after 3rd party pre-init has happened
 
         Fluids.init();
         SemiBlockInitializer.init();

src/main/java/me/desht/pneumaticcraft/common/progwidgets/WidgetRegistrator.java
@@ -1,8 +1,10 @@
 package me.desht.pneumaticcraft.common.progwidgets;
 
+import me.desht.pneumaticcraft.api.event.PuzzleRegistryEvent;
 import me.desht.pneumaticcraft.common.ai.DroneInteractRFExport;
 import me.desht.pneumaticcraft.common.ai.DroneInteractRFImport;
 import me.desht.pneumaticcraft.common.config.ProgWidgetConfig;
+import net.minecraftforge.common.MinecraftForge;
 
 import java.util.*;
 
@@ -68,6 +70,10 @@ public class WidgetRegistrator {
         // TODO: should really be their own widgets, but historical reasons...
         register(new ProgWidgetCustomBlockInteract().setInteractor(new DroneInteractRFExport()));
         register(new ProgWidgetCustomBlockInteract().setInteractor(new DroneInteractRFImport()));
+
+        MinecraftForge.EVENT_BUS.post(new PuzzleRegistryEvent());
+
+        compileBlacklist();
     }
 
     public static void register(IProgWidget widget) {
@@ -82,7 +88,7 @@ public class WidgetRegistrator {
         return allRegisteredWidgets.get(name);
     }
 
-    public static void compileBlacklist() {
+    private static void compileBlacklist() {
         registeredWidgets.clear();
         for (Map.Entry<String, IProgWidget> entry : allRegisteredWidgets.entrySet()) {
             if (!ProgWidgetConfig.blacklistedPieces.contains(entry.getKey())) {