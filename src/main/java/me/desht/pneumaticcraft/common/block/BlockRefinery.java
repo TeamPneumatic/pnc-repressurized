fbc8d99ee900538e25a5c50aca55bfedd3d09f36
Des Herriott <des.herriott@gmail.com>
2018-07-08 11:45:26 +0100

    Better refinery behaviour when right-clicking with bucket or other tank Previously it just extracted from the bottom block of the refinery regardless of which actual block was clicked.  Now it extracts from the actual block which was clicked.

--- src/main/java/me/desht/pneumaticcraft/common/block/BlockRefinery.java
+++ src/main/java/me/desht/pneumaticcraft/common/block/BlockRefinery.java
@@ -12,6 +12,9 @@ import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.FluidUtil;
+import net.minecraftforge.fluids.capability.IFluidHandler;
 
 public class BlockRefinery extends BlockPneumaticCraftModeled {
 
@@ -28,8 +31,25 @@ public class BlockRefinery extends BlockPneumaticCraftModeled {
     public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand, EnumFacing side, float par7, float par8, float par9) {
         TileEntity te = world.getTileEntity(pos);
         if (te instanceof TileEntityRefinery) {
+            // normally, activating the block should open the master TE's gui
+            // but if we activate with a fluid tank in hand (which can accept the fluid), then try to
+            // extract from the actual refinery block that was activated
             TileEntityRefinery master = ((TileEntityRefinery) te).getMasterRefinery();
-            return super.onBlockActivated(world, master.getPos(), state, player, hand, side, par7, par8, par9);
+            BlockPos actualPos = master.getPos();
+            IFluidHandler handler = FluidUtil.getFluidHandler(player.getHeldItem(hand));
+            if (handler != null) {
+                IFluidHandler srcHandler = FluidUtil.getFluidHandler(world, pos, side);
+                if (srcHandler != null) {
+                    FluidStack f = FluidUtil.tryFluidTransfer(handler, srcHandler, srcHandler.getTankProperties()[0].getCapacity(), false);
+                    if (f == null || f.amount == 0) {
+                        return false;
+                    } else {
+                        actualPos = pos;
+                    }
+                }
+            }
+
+            return super.onBlockActivated(world, actualPos, state, player, hand, side, par7, par8, par9);
         }
         return false;
     }
