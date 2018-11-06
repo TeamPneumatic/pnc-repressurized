9935b6caa92c5cffb8bedeaf709b20e5161ed4ce
Des Herriott <des.herriott@gmail.com>
2018-11-06 08:32:45 +0000

    Minor charging station container cleanup

--- src/main/java/me/desht/pneumaticcraft/common/inventory/ContainerChargingStationItemInventory.java
+++ src/main/java/me/desht/pneumaticcraft/common/inventory/ContainerChargingStationItemInventory.java
@@ -5,17 +5,16 @@ import net.minecraft.entity.player.InventoryPlayer;
 
 public class ContainerChargingStationItemInventory extends ContainerPneumaticBase<TileEntityChargingStation> {
 
-    public ChargeableItemHandler chargeableItemHandler;
-
     public ContainerChargingStationItemInventory(InventoryPlayer inventoryPlayer, TileEntityChargingStation te) {
         super(te);
+
         if (te.getPrimaryInventory().getStackInSlot(TileEntityChargingStation.CHARGE_INVENTORY_INDEX).isEmpty())
             throw new IllegalArgumentException("instantiating ContainerChargingStationItemInventory with no chargeable item installed!");
-        chargeableItemHandler = te.getChargeableInventory();
 
+        ChargeableItemHandler chargeableItemHandler = te.getChargeableInventory();
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                addSlotToContainer(new SlotInventoryLimiting(chargeableItemHandler, i * 3 + j, 31 + j * 18, 24 + i * 18){
+                addSlotToContainer(new SlotInventoryLimiting(chargeableItemHandler, i * 3 + j, 31 + j * 18, 24 + i * 18) {
                     @Override
                     public void onSlotChanged() {
                         // needed to force client sync in some circumstances
@@ -26,10 +25,6 @@ public class ContainerChargingStationItemInventory extends ContainerPneumaticBas
         }
 
         addPlayerSlots(inventoryPlayer, 84);
-
         addArmorSlots(inventoryPlayer, 9, 8);
-
-        addSlotToContainer(new SlotUntouchable(te.getPrimaryInventory(), 0, -50000, -50000)); //Allows the charging stack to sync.
-
     }
 }
