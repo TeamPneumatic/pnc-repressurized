746a79246eb4ff32bcab36415aa8ba200b797d1a
Des Herriott <des.herriott@gmail.com>
2019-02-13 17:04:23 +0000

    Made the creative compressor item epic rarity

--- src/main/java/me/desht/pneumaticcraft/common/block/BlockCreativeCompressor.java
+++ src/main/java/me/desht/pneumaticcraft/common/block/BlockCreativeCompressor.java
@@ -3,9 +3,12 @@ package me.desht.pneumaticcraft.common.block;
 import me.desht.pneumaticcraft.common.GuiHandler.EnumGuiId;
 import me.desht.pneumaticcraft.common.tileentity.TileEntityCreativeCompressor;
 import net.minecraft.block.material.Material;
+import net.minecraft.item.EnumRarity;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 
-public class BlockCreativeCompressor extends BlockPneumaticCraftModeled {
+public class BlockCreativeCompressor extends BlockPneumaticCraftModeled implements ICustomItemBlock {
 
     BlockCreativeCompressor() {
         super(Material.IRON, "creative_compressor");
@@ -24,5 +27,21 @@ public class BlockCreativeCompressor extends BlockPneumaticCraftModeled {
     @Override
     public boolean getTickRandomly() {
         return super.getTickRandomly();
+    }
+
+    @Override
+    public ItemBlock getCustomItemBlock() {
+        return new ItemBlockCreativeCompressor(this);
+    }
+
+    public static class ItemBlockCreativeCompressor extends ItemBlock {
+        ItemBlockCreativeCompressor(BlockCreativeCompressor blockCreativeCompressor) {
+            super(blockCreativeCompressor);
+        }
+
+        @Override
+        public EnumRarity getRarity(ItemStack stack) {
+            return EnumRarity.EPIC;
+        }
     }
 }
