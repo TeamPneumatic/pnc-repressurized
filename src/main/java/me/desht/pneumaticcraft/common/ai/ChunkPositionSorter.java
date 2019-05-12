b8ed2055e1c125c17ae43dd6c7636ac297512c1f
Des Herriott <des.herriott@gmail.com>
2019-05-12 12:33:11 +0100

    Attempt to make the position sorter a bit more deterministic 1) Distance-sort from the centre of the block the drone is in 2) If dist to blocks is too close to call, sort by Y->Z->X blockpos May help with #342 ?

--- src/main/java/me/desht/pneumaticcraft/common/ai/ChunkPositionSorter.java
+++ src/main/java/me/desht/pneumaticcraft/common/ai/ChunkPositionSorter.java
@@ -7,14 +7,17 @@ import net.minecraft.util.math.Vec3d;
 import java.util.Comparator;
 
 public class ChunkPositionSorter implements Comparator<BlockPos> {
+    private static final double EPSILON = 0.01;
 
     private final double x, y, z;
 
-    public ChunkPositionSorter(IDroneBase entity) {
+    ChunkPositionSorter(IDroneBase entity) {
         Vec3d vec = entity.getDronePos();
-        x = vec.x;
-        y = vec.y;
-        z = vec.z;
+
+        // work from middle of the block the drone is in (try to minimize inconsistency)
+        x = Math.floor(vec.x) + 0.5;
+        y = Math.floor(vec.y) + 0.5;
+        z = Math.floor(vec.z) + 0.5;
     }
 
     public ChunkPositionSorter(double x, double y, double z) {
@@ -25,6 +28,11 @@ public class ChunkPositionSorter implements Comparator<BlockPos> {
 
     @Override
     public int compare(BlockPos c1, BlockPos c2) {
-        return Double.compare(PneumaticCraftUtils.distBetweenSq(c1.getX(), c1.getY(), c1.getZ(), x, y, z), PneumaticCraftUtils.distBetweenSq(c2.getX(), c2.getY(), c2.getZ(), x, y, z));
+        double d = PneumaticCraftUtils.distBetweenSq(c1.getX(), c1.getY(), c1.getZ(), x, y, z) - PneumaticCraftUtils.distBetweenSq(c2.getX(), c2.getY(), c2.getZ(), x, y, z);
+        if (Math.abs(d) < EPSILON) {
+            return c1.compareTo(c2);
+        } else {
+            return d < 0 ? -1 : 1;
+        }
     }
 }
