794b3460f9b96eca9393e49a8581398bdca0d448
Des Herriott <des.herriott@gmail.com>
2020-02-25 08:16:48 +0000

    Tweaked mechanic villager trades a bit

--- src/main/java/me/desht/pneumaticcraft/common/villages/VillagerTradesRegistration.java
+++ src/main/java/me/desht/pneumaticcraft/common/villages/VillagerTradesRegistration.java
@@ -22,7 +22,7 @@ public class VillagerTradesRegistration {
                     .setEmeraldPriceFor(10, 19, ModItems.PCB_BLUEPRINT.get(), 1)
                     .build()
             );
-            trades.get(1).add(new RandomTradeBuilder(8, 4, 0.05F)
+            trades.get(1).add(new RandomTradeBuilder(16, 4, 0.05F)
                     .setEmeraldPrice(3, 7)
                     .setForSale((rand) -> new ItemStack(ModBlocks.COMPRESSED_IRON_BLOCK.get()))
                     .build()
@@ -32,81 +32,85 @@ public class VillagerTradesRegistration {
                     .setForSale((rand) -> new ItemStack(ModBlocks.PRESSURE_TUBE.get(), 8))
                     .build()
             );
-            trades.get(1).add(new RandomTradeBuilder(16, 2, 0.05F)
+            trades.get(1).add(new RandomTradeBuilder(16, 4, 0.05F)
                     .setEmeraldPrice(3, 12)
                     .setForSale((rand) -> new ItemStack(ModItems.AIR_CANISTER.get(), 1))
                     .build()
             );
-            trades.get(1).add(new RandomTradeBuilder(16, 2, 0.05F)
+            trades.get(2).add(new RandomTradeBuilder(16, 4, 0.05F)
                     .setEmeraldPrice(2, 6)
                     .setForSale((rand) -> new ItemStack(ModItems.LOGISTICS_CORE.get(), 4))
                     .build()
             );
-            trades.get(2).add(new RandomTradeBuilder(8, 4, 0.05f)
+            trades.get(2).add(new RandomTradeBuilder(16, 6, 0.05f)
                     .setEmeraldPriceFor(3, 4, ModItems.TRANSISTOR.get(), 3)
                     .build()
             );
-            trades.get(2).add(new RandomTradeBuilder(8, 4, 0.05f)
+            trades.get(2).add(new RandomTradeBuilder(16, 6, 0.05f)
                     .setEmeraldPriceFor(3, 4, ModItems.CAPACITOR.get(), 3)
                     .build()
             );
-            trades.get(2).add(new RandomTradeBuilder(8, 4, 0.05f)
+            trades.get(2).add(new RandomTradeBuilder(16, 4, 0.05f)
                     .setEmeraldPriceFor(3, 4, ModItems.TURBINE_BLADE.get(), 3)
                     .build()
             );
-            trades.get(3).add(new RandomTradeBuilder(4, 15, 0.05F)
+            trades.get(3).add(new RandomTradeBuilder(8, 15, 0.05F)
                     .setEmeraldPriceFor(10, 20, ModItems.LOGISTICS_DRONE.get(), 1)
                     .build()
             );
-            trades.get(3).add(new RandomTradeBuilder(4, 15, 0.05F)
+            trades.get(3).add(new RandomTradeBuilder(8, 15, 0.05F)
                     .setEmeraldPriceFor(10, 20, ModItems.HARVESTING_DRONE.get(), 1)
                     .build()
             );
-            trades.get(3).add(new RandomTradeBuilder(1, 10, 0.05F)
+            trades.get(3).add(new RandomTradeBuilder(16, 10, 0.05F)
                     .setEmeraldPriceFor(6, 12, ModItems.PNEUMATIC_CYLINDER.get(), 1)
                     .build()
             );
-            trades.get(3).add(new RandomTradeBuilder(1, 10, 0.05F)
+            trades.get(3).add(new RandomTradeBuilder(8, 10, 0.05F)
                     .setEmeraldPriceFor(1, 5, ModItems.NUKE_VIRUS.get(), 1)
                     .build()
             );
-            trades.get(3).add(new RandomTradeBuilder(1, 10, 0.05F)
+            trades.get(3).add(new RandomTradeBuilder(8, 10, 0.05F)
                     .setEmeraldPriceFor(1, 5, ModItems.STOP_WORM.get(), 1)
                     .build()
             );
-            trades.get(3).add(new RandomTradeBuilder(4, 10, 0.05F)
+            trades.get(3).add(new RandomTradeBuilder(12, 10, 0.05F)
                     .setEmeraldPriceFor(8, 16, ModItems.GUN_AMMO.get(), 1)
                     .build()
             );
-            trades.get(4).add(new RandomTradeBuilder(4, 12, 0.05F)
+            trades.get(4).add(new RandomTradeBuilder(12, 12, 0.05F)
                     .setEmeraldPriceFor(10, 20, ModItems.GUN_AMMO_FREEZING.get(), 1)
                     .build()
             );
-            trades.get(4).add(new RandomTradeBuilder(4, 12, 0.05F)
+            trades.get(4).add(new RandomTradeBuilder(12, 12, 0.05F)
                     .setEmeraldPriceFor(10, 20, ModItems.GUN_AMMO_WEIGHTED.get(), 1)
                     .build()
             );
-            trades.get(4).add(new RandomTradeBuilder(4, 12, 0.05F)
+            trades.get(4).add(new RandomTradeBuilder(12, 12, 0.05F)
                     .setEmeraldPriceFor(10, 20, ModItems.GUN_AMMO_AP.get(), 1)
                     .build()
             );
-            trades.get(4).add(new RandomTradeBuilder(4, 12, 0.05F)
+            trades.get(4).add(new RandomTradeBuilder(12, 12, 0.05F)
                     .setEmeraldPriceFor(10, 20, ModItems.GUN_AMMO_EXPLOSIVE.get(), 1)
                     .build()
             );
-            trades.get(4).add(new RandomTradeBuilder(4, 12, 0.05F)
+            trades.get(4).add(new RandomTradeBuilder(12, 12, 0.05F)
                     .setEmeraldPriceFor(10, 20, ModItems.GUN_AMMO_INCENDIARY.get(), 1)
                     .build()
             );
-            trades.get(4).add(new RandomTradeBuilder(6, 15, 0.05F)
+            trades.get(4).add(new RandomTradeBuilder(8, 15, 0.05F)
                     .setEmeraldPriceFor(25, 40, ModItems.PROGRAMMING_PUZZLE.get(), 16)
                     .build()
             );
-            trades.get(5).add(new RandomTradeBuilder(4, 15, 0.05F)
+            trades.get(5).add(new RandomTradeBuilder(6, 15, 0.05F)
                     .setEmeraldPriceFor(25, 40, ModItems.PRINTED_CIRCUIT_BOARD.get(), 1)
                     .build()
             );
-            trades.get(5).add(new RandomTradeBuilder(4, 15, 0.05F)
+            trades.get(5).add(new RandomTradeBuilder(12, 15, 0.05F)
+                    .setEmeraldPriceFor(15, 35, ModItems.MICROMISSILES.get(), 1)
+                    .build()
+            );
+            trades.get(5).add(new RandomTradeBuilder(8, 15, 0.05F)
                     .setEmeraldPriceFor(30, 40, ModItems.DRONE.get(), 1)
                     .build()
             );
