7eea2102101e758fdc9cce3682030ade75252e96
Des Herriott <des.herriott@gmail.com>
2020-05-08 14:33:18 +0100

    Charging module: fixed some over-aggressive capability caching #469

--- src/main/java/me/desht/pneumaticcraft/common/block/tubes/ModuleCharging.java
+++ src/main/java/me/desht/pneumaticcraft/common/block/tubes/ModuleCharging.java
@@ -2,7 +2,6 @@ package me.desht.pneumaticcraft.common.block.tubes;
 
 import me.desht.pneumaticcraft.api.PNCCapabilities;
 import me.desht.pneumaticcraft.common.item.ItemTubeModule;
-import me.desht.pneumaticcraft.common.util.TileEntityCache;
 import me.desht.pneumaticcraft.lib.PneumaticValues;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
@@ -11,8 +10,7 @@ import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.IItemHandler;
 
 public class ModuleCharging extends TubeModule {
-    private TileEntityCache connectedInventory;
-    private LazyOptional<IItemHandler> cachedHandler = LazyOptional.empty();
+    private TileEntity neighbourTE = null;
 
     public ModuleCharging(ItemTubeModule itemTubeModule) {
         super(itemTubeModule);
@@ -24,59 +22,40 @@ public class ModuleCharging extends TubeModule {
 
         if (pressureTube.getWorld().isRemote || (pressureTube.getWorld().getGameTime() & 0x7) != 0) return;
 
-        if (!cachedHandler.isPresent()) {
-            cachedHandler = getConnectedInventory();
-            if (cachedHandler.isPresent()) {
-                cachedHandler.addListener(h -> cachedHandler = LazyOptional.empty());
-            }
-        }
-
-        cachedHandler.ifPresent(itemHandler -> {
+        getConnectedInventory().ifPresent(itemHandler -> {
             // times 8 because we only run every 8 ticks
             int airToTransfer = 8 * PneumaticValues.CHARGING_STATION_CHARGE_RATE * (upgraded ? 10 : 1);
             pressureTube.getCapability(PNCCapabilities.AIR_HANDLER_MACHINE_CAPABILITY).ifPresent(airHandler -> {
-                int airInTube = (int) (airHandler.getPressure() * airHandler.getVolume());
-
                 for (int slot = 0; slot < itemHandler.getSlots(); slot++) {
                     ItemStack chargedItem = itemHandler.getStackInSlot(slot);
-                    final int airInTube2 = airInTube;
-                    int toAdd = chargedItem.getCapability(PNCCapabilities.AIR_HANDLER_ITEM_CAPABILITY).map(h -> {
-                        float itemPressure = h.getPressure();
-                        float itemVolume = h.getVolume();
+                    chargedItem.getCapability(PNCCapabilities.AIR_HANDLER_ITEM_CAPABILITY).ifPresent(airHandlerItem -> {
+                        float itemPressure = airHandlerItem.getPressure();
+                        float itemVolume = airHandlerItem.getVolume();
                         float delta = Math.abs(airHandler.getPressure() - itemPressure) / 2.0F;
-                        int airInItem = (int) (itemPressure * itemVolume);
+                        int airInItem = airHandlerItem.getAir();
                         if (itemPressure > airHandler.getPressure() + 0.01F && itemPressure > 0F) {
                             // move air from item to charger (tube)
                             int airToMove = Math.min(Math.min(airToTransfer, airInItem), (int) (delta * airHandler.getVolume()));
-                            h.addAir(-airToMove);
+                            airHandlerItem.addAir(-airToMove);
                             airHandler.addAir(airToMove);
-                            return airToMove;
-                        } else if (itemPressure < airHandler.getPressure() - 0.01F && itemPressure < h.maxPressure()) {
+                        } else if (itemPressure < airHandler.getPressure() - 0.01F && itemPressure < airHandlerItem.maxPressure()) {
                             // move air from charger (tube) to item
-                            int maxAirInItem = (int) (h.maxPressure() * itemVolume);
-                            int airToMove = Math.min(Math.min(airToTransfer, airInTube2), maxAirInItem - airInItem);
+                            int maxAirInItem = (int) (airHandlerItem.maxPressure() * itemVolume);
+                            int airToMove = Math.min(Math.min(airToTransfer, airHandler.getAir()), maxAirInItem - airInItem);
                             airToMove = Math.min((int) (delta * itemVolume), airToMove);
-                            h.addAir(airToMove);
+                            airHandlerItem.addAir(airToMove);
                             airHandler.addAir(-airToMove);
                         }
-                        return 0;
-                    }).orElse(0);
-                    airInTube += toAdd;
+                    });
                 }
             });
         });
     }
 
-    @Override
-    public void onNeighborTileUpdate() {
-        connectedInventory = null;
-    }
-
     private LazyOptional<IItemHandler> getConnectedInventory() {
-        if (connectedInventory == null) {
-            connectedInventory = new TileEntityCache(pressureTube.getWorld(), pressureTube.getPos().offset(dir));
+        if (neighbourTE == null || neighbourTE.isRemoved()) {
+            neighbourTE = pressureTube.getWorld().getTileEntity(pressureTube.getPos().offset(dir));
         }
-        TileEntity te = connectedInventory.getTileEntity();
-        return te == null ? LazyOptional.empty() : te.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, dir.getOpposite());
+        return neighbourTE == null ? LazyOptional.empty() : neighbourTE.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, dir.getOpposite());
     }
 }
