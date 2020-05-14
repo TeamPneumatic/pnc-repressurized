7fa7020f11bc0bfbec1e47b935c46acf0df31388
Des Herriott <des.herriott@gmail.com>
2020-05-14 14:17:58 +0100

    GPS Area Line & Wall modes: fixed rounding error Caused lines/walls to be off by one for negative X/Z

--- src/main/java/me/desht/pneumaticcraft/common/progwidgets/area/AreaTypeLine.java
+++ src/main/java/me/desht/pneumaticcraft/common/progwidgets/area/AreaTypeLine.java
@@ -16,8 +16,7 @@ public class AreaTypeLine extends AreaType {
 
     @Override
     public void addArea(Consumer<BlockPos> areaAdder, BlockPos p1, BlockPos p2, int minX, int minY, int minZ, int maxX, int maxY, int maxZ){
-        Vec3d lineVec = new Vec3d(p2.getX() - p1.getX(), p2.getY() - p1.getY(), p2.getZ() - p1.getZ()).normalize();
-        lineVec = new Vec3d(lineVec.x / 10, lineVec.y / 10, lineVec.z / 10);
+        Vec3d lineVec = new Vec3d(p2.getX() - p1.getX(), p2.getY() - p1.getY(), p2.getZ() - p1.getZ()).normalize().scale(0.1);
         double curX = p1.getX() + 0.5;
         double curY = p1.getY() + 0.5;
         double curZ = p1.getZ() + 0.5;
@@ -29,7 +28,7 @@ public class AreaTypeLine extends AreaType {
             curY += lineVec.y;
             curZ += lineVec.z;
             if (curY >= 0 && curY < 256) {
-                BlockPos pos = new BlockPos((int) curX, (int) curY, (int) curZ);
+                BlockPos pos = new BlockPos(curX, curY, curZ);
                 areaAdder.accept(pos);
             }
         }
