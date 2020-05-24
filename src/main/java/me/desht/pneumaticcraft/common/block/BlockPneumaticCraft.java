b81684995ef9629f3af7db389548f4d8d21ade9d
Des Herriott <des.herriott@gmail.com>
2020-05-24 12:34:42 +0100

    Couple of fixes to block placement & tube toggle behaviour Better calculation of initial state

--- src/main/java/me/desht/pneumaticcraft/common/block/BlockPneumaticCraft.java
+++ src/main/java/me/desht/pneumaticcraft/common/block/BlockPneumaticCraft.java
@@ -131,6 +131,14 @@ public abstract class BlockPneumaticCraft extends Block implements IPneumaticWre
     @Override
     public BlockState getStateForPlacement(BlockItemUseContext ctx) {
         BlockState state = super.getStateForPlacement(ctx);
+        for (Direction facing : Direction.VALUES) {
+            if (state.has(connectionProperty(facing))) {
+                // handle pneumatic connections to neighbouring air handlers
+                TileEntity te = ctx.getWorld().getTileEntity(ctx.getPos().offset(facing));
+                boolean b = te != null && te.getCapability(PNCCapabilities.AIR_HANDLER_MACHINE_CAPABILITY, facing.getOpposite()).isPresent();
+                state = state.with(connectionProperty(facing), b);
+            }
+        }
         if (isRotatable()) {
             Direction f = canRotateToTopOrBottom() ? ctx.getNearestLookingDirection() : ctx.getPlacementHorizontalFacing();
             return state.with(directionProperty(), reversePlacementRotation() ? f.getOpposite() : f);
