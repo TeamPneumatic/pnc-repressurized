dbb3e52e82cbcf8ac3d45e96604b01ed8d40b5de
Des Herriott <des.herriott@gmail.com>
2020-05-24 17:23:31 +0100

    Fixed block shape for Refinery Controller

--- src/main/java/me/desht/pneumaticcraft/common/block/BlockRefineryController.java
+++ src/main/java/me/desht/pneumaticcraft/common/block/BlockRefineryController.java
@@ -2,14 +2,30 @@ package me.desht.pneumaticcraft.common.block;
 
 import me.desht.pneumaticcraft.common.core.ModBlocks;
 import me.desht.pneumaticcraft.common.tileentity.TileEntityRefineryController;
+import me.desht.pneumaticcraft.common.util.VoxelShapeUtils;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
+import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
 
 public class BlockRefineryController extends BlockPneumaticCraft {
+    private static final VoxelShape SHAPE_N = VoxelShapes.or(
+        makeCuboidShape(0, 0, 0, 16, 1, 16),
+        makeCuboidShape(0, 15, 0, 16, 16, 16),
+        makeCuboidShape(0, 1, 0, 16, 15, 8),
+        makeCuboidShape(1, 1, 8, 15, 15, 16)
+    );
+    private static final VoxelShape SHAPE_E = VoxelShapeUtils.rotateY(SHAPE_N, 90);
+    private static final VoxelShape SHAPE_S = VoxelShapeUtils.rotateY(SHAPE_E, 90);
+    private static final VoxelShape SHAPE_W = VoxelShapeUtils.rotateY(SHAPE_S, 90);
+    private static final VoxelShape[] SHAPES = new VoxelShape[] { SHAPE_S, SHAPE_W, SHAPE_N, SHAPE_E };
 
     public BlockRefineryController() {
         super(ModBlocks.defaultProps());
@@ -18,6 +34,12 @@ public class BlockRefineryController extends BlockPneumaticCraft {
     @Override
     protected Class<? extends TileEntity> getTileEntityClass() {
         return TileEntityRefineryController.class;
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+        Direction d = state.get(directionProperty());
+        return SHAPES[d.getHorizontalIndex()];
     }
 
     @Override
