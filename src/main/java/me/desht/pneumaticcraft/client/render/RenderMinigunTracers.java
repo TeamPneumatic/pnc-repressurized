fcdf7c26b20bb7c033ce05b61534191b9fd6ddc5
Des Herriott <des.herriott@gmail.com>
2020-06-21 00:00:17 +0100

    Fixed sentry turret bullet tracers rendering too low for some targets #533

--- src/main/java/me/desht/pneumaticcraft/client/render/RenderMinigunTracers.java
+++ src/main/java/me/desht/pneumaticcraft/client/render/RenderMinigunTracers.java
@@ -6,7 +6,6 @@ import me.desht.pneumaticcraft.client.util.ProgressingLine;
 import me.desht.pneumaticcraft.client.util.RenderUtils;
 import me.desht.pneumaticcraft.common.minigun.Minigun;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.math.Vec3d;
 
@@ -23,22 +22,23 @@ public class RenderMinigunTracers {
     }
 
     public static void render(Minigun minigun, MatrixStack matrixStack, IRenderTypeBuffer buffer, double x, double y, double z, double gunRadius) {
-        LivingEntity attackTarget = minigun.getAttackTarget();
+        LivingEntity target = minigun.getAttackTarget();
 
         matrixStack.push();
         matrixStack.translate(-x, -y, -z);
-        Vec3d vec = new Vec3d(attackTarget.getPosX() - x, attackTarget.getPosY() - y, attackTarget.getPosZ() - z).normalize();
+        Vec3d vec = new Vec3d(target.getPosX() - x, target.getPosY() + target.getHeight() / 2 - y, target.getPosZ() - z)
+                .normalize().scale(gunRadius);
         ProgressingLine minigunFire = new ProgressingLine().setProgress(1);
-        Random rand = attackTarget.getEntityWorld().rand;
-        minigunFire.startX = x + vec.x * gunRadius;
-        minigunFire.startY = y + vec.y * gunRadius;
-        minigunFire.startZ = z + vec.z * gunRadius;
-        IVertexBuilder builder = buffer.getBuffer(RenderType.LINES);
+        minigunFire.startX = x + vec.x ;
+        minigunFire.startY = y + vec.y;
+        minigunFire.startZ = z + vec.z;
+        Random rand = target.getEntityWorld().rand;
+        IVertexBuilder builder = buffer.getBuffer(ModRenderTypes.BLOCK_TRACKER);
         for (int i = 0; i < 5; i++) {
-            minigunFire.endX = attackTarget.getPosX() + rand.nextDouble() * 0.8 - 0.4;
-            minigunFire.endY = attackTarget.getPosY() + attackTarget.getHeight() / 2 + rand.nextDouble() * 0.8 - 0.4;
-            minigunFire.endZ = attackTarget.getPosZ() + rand.nextDouble() * 0.8 - 0.4;
-            RenderUtils.renderProgressingLine(minigunFire, matrixStack, builder, 0x60000000 | minigun.getAmmoColor());
+            minigunFire.endX = target.getPosX() + rand.nextDouble() * 0.8 - 0.4;
+            minigunFire.endY = target.getPosY() + target.getHeight() / 2 + rand.nextDouble() * 0.8 - 0.4;
+            minigunFire.endZ = target.getPosZ() + rand.nextDouble() * 0.8 - 0.4;
+            RenderUtils.renderProgressingLine(minigunFire, matrixStack, builder, 0x40000000 | minigun.getAmmoColor());
         }
         matrixStack.pop();
     }
