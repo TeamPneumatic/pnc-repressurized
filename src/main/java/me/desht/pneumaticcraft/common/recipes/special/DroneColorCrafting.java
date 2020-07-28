4e6b2becccc93c81d8750137e427480a48bce9a6
Des Herriott <des.herriott@gmail.com>
2020-07-28 15:32:44 +0100

    Some hacky tweaks to drone color & upgrade recipes Using tags in these "special" recipes really doesn't work well in 1.16...

--- src/main/java/me/desht/pneumaticcraft/common/recipes/special/DroneColorCrafting.java
+++ src/main/java/me/desht/pneumaticcraft/common/recipes/special/DroneColorCrafting.java
@@ -3,10 +3,10 @@ package me.desht.pneumaticcraft.common.recipes.special;
 import me.desht.pneumaticcraft.common.core.ModItems;
 import me.desht.pneumaticcraft.common.core.ModRecipes;
 import me.desht.pneumaticcraft.common.entity.living.EntityDrone;
-import me.desht.pneumaticcraft.common.item.ItemDrone;
 import net.minecraft.inventory.CraftingInventory;
 import net.minecraft.item.DyeColor;
 import net.minecraft.item.DyeItem;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipeSerializer;
 import net.minecraft.item.crafting.Ingredient;
@@ -14,46 +14,71 @@ import net.minecraft.item.crafting.ShapelessRecipe;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.common.Tags;
+import net.minecraft.world.World;
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.Arrays;
 
 public class DroneColorCrafting extends ShapelessRecipe {
     private NonNullList<Ingredient> ingredients = null;
 
+    // you'd think using Ingredient.fromTag(Tags.Items.Dyes) would work, but nope
+    private static final Item[] DYES = new Item[DyeColor.values().length];
+    static {
+        Arrays.setAll(DYES, i -> DyeItem.getItem(DyeColor.values()[i]));
+    }
+
     public DroneColorCrafting(ResourceLocation idIn) {
-        super(idIn, "", new ItemStack(ModItems.DRONE.get()), NonNullList.create());
+        super(idIn, "", new ItemStack(ModItems.DRONE.get()), NonNullList.from(Ingredient.EMPTY,
+                    Ingredient.fromItems(DYES), Ingredient.fromItems(ModItems.DRONE.get()))
+        );
     }
 
     @Override
     public NonNullList<Ingredient> getIngredients() {
-        if (ingredients == null) {
-            ingredients = NonNullList.from(Ingredient.EMPTY,
-                    Ingredient.fromTag(Tags.Items.DYES), Ingredient.fromItems(ModItems.DRONE.get())
-            );
+        return super.getIngredients();
+//        if (ingredients == null) {
+//            ingredients = NonNullList.from(Ingredient.EMPTY,
+//                    Ingredient.fromTag(Tags.Items.DYES), Ingredient.fromItems(ModItems.DRONE.get())
+//            );
+//        }
+//        return ingredients;
+    }
+
+    private Pair<ItemStack, DyeColor> findItems(CraftingInventory inv) {
+        ItemStack drone = ItemStack.EMPTY;
+        DyeColor dye = null;
+        for (int i = 0; i < inv.getSizeInventory(); i++) {
+            ItemStack stack = inv.getStackInSlot(i);
+            if (stack.getItem() == ModItems.DRONE.get()) {
+                if (!drone.isEmpty()) return null;
+                drone = stack.copy();
+            } else if (dye == null) {
+                DyeColor color = DyeColor.getColor(stack);
+                if (color != null) {
+                    dye = color;
+                }
+            } else {
+                return null;
+            }
         }
-        return ingredients;
+        return Pair.of(drone, dye);
+    }
+
+    @Override
+    public boolean matches(CraftingInventory inv, World worldIn) {
+        return findItems(inv) != null;
     }
 
     @Override
     public ItemStack getCraftingResult(CraftingInventory inv) {
-        ItemStack drone = ItemStack.EMPTY;
-        DyeColor dyeColor = null;
-        for (int i = 0; i < inv.getSizeInventory(); i++) {
-            ItemStack stack = inv.getStackInSlot(i);
-            if (!stack.isEmpty()) {
-                if (stack.getItem() instanceof ItemDrone) {
-                    drone = stack.copy();
-                } else if (dyeColor == null) {
-                    dyeColor = ((DyeItem) stack.getItem()).getDyeColor();
-                }
-            }
-        }
+        Pair<ItemStack, DyeColor> data = findItems(inv);
+        if (data == null) return ItemStack.EMPTY;
+        ItemStack drone = data.getLeft();
+        DyeColor dyeColor = data.getRight();
         if (drone.isEmpty() || dyeColor == null) return ItemStack.EMPTY;
 
-        CompoundNBT droneTag = drone.getTag();
-        if (droneTag == null) {
-            droneTag = new CompoundNBT();
-            drone.setTag(droneTag);
-        }
+        CompoundNBT droneTag = drone.getOrCreateTag();
         droneTag.putInt(EntityDrone.NBT_DRONE_COLOR, dyeColor.getId());
         return drone;
     }
--- src/main/java/me/desht/pneumaticcraft/common/recipes/special/DroneUpgradeCrafting.java
+++ src/main/java/me/desht/pneumaticcraft/common/recipes/special/DroneUpgradeCrafting.java
@@ -1,10 +1,10 @@
 package me.desht.pneumaticcraft.common.recipes.special;
 
-import me.desht.pneumaticcraft.common.PneumaticCraftTags;
 import me.desht.pneumaticcraft.common.core.ModItems;
 import me.desht.pneumaticcraft.common.core.ModRecipes;
 import me.desht.pneumaticcraft.common.item.ItemDrone;
 import net.minecraft.inventory.CraftingInventory;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipeSerializer;
 import net.minecraft.item.crafting.Ingredient;
@@ -12,23 +12,53 @@ import net.minecraft.item.crafting.ShapelessRecipe;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
 
 public class DroneUpgradeCrafting extends ShapelessRecipe {
     private NonNullList<Ingredient> ingredients = null;
 
+    // you'd think using Ingredient.fromTag(PneumaticCraftTags.Items.BASIC_DRONES) would work, but nope
+    private static final Item[] DRONES = {
+            ModItems.LOGISTICS_DRONE.get(),
+            ModItems.HARVESTING_DRONE.get(),
+            ModItems.GUARD_DRONE.get(),
+            ModItems.COLLECTOR_DRONE.get(),
+    };
+
     public DroneUpgradeCrafting(ResourceLocation idIn) {
-        super(idIn, "", new ItemStack(ModItems.DRONE.get()), NonNullList.create());
+        super(idIn, "", new ItemStack(ModItems.DRONE.get()), NonNullList.from(Ingredient.EMPTY,
+                    Ingredient.fromItems(ModItems.PRINTED_CIRCUIT_BOARD.get()),
+                    Ingredient.fromItems(DRONES))
+        );
     }
 
+//    @Override
+//    public NonNullList<Ingredient> getIngredients() {
+//        return NonNullList.create();
+////        if (ingredients == null) {
+////            ingredients = NonNullList.from(Ingredient.EMPTY,
+////                    Ingredient.fromItems(ModItems.PRINTED_CIRCUIT_BOARD.get()),
+////                    Ingredient.fromTag(PneumaticCraftTags.Items.BASIC_DRONES)
+////            );
+////        }
+////        return ingredients;
+//    }
+
     @Override
-    public NonNullList<Ingredient> getIngredients() {
-        if (ingredients == null) {
-            ingredients = NonNullList.from(Ingredient.EMPTY,
-                    Ingredient.fromItems(ModItems.PRINTED_CIRCUIT_BOARD.get()),
-                    Ingredient.fromTag(PneumaticCraftTags.Items.BASIC_DRONES)
-            );
+    public boolean matches(CraftingInventory inv, World worldIn) {
+        int basicDrone = 0, pcb = 0;
+        for (int i = 0; i < inv.getSizeInventory(); i++) {
+            ItemStack stack = inv.getStackInSlot(i);
+            if (isBasicDrone(stack)) {
+                basicDrone++;
+            } else if (stack.getItem() == ModItems.PRINTED_CIRCUIT_BOARD.get()) {
+                pcb++;
+            } else {
+                return false;
+            }
+            if (basicDrone > 1 || pcb > 1) return false;
         }
-        return ingredients;
+        return true;
     }
 
     @Override
