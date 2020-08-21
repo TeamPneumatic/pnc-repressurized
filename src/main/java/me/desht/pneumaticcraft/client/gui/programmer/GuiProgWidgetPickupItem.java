3d0d357848812e44c3793b7be6818bc3ac9f887d
Des Herriott <des.herriott@gmail.com>
2020-08-21 12:33:52 +0100

    Small backport compilation fix

--- src/main/java/me/desht/pneumaticcraft/client/gui/programmer/GuiProgWidgetPickupItem.java
+++ src/main/java/me/desht/pneumaticcraft/client/gui/programmer/GuiProgWidgetPickupItem.java
@@ -3,8 +3,7 @@ package me.desht.pneumaticcraft.client.gui.programmer;
 import me.desht.pneumaticcraft.client.gui.GuiProgrammer;
 import me.desht.pneumaticcraft.client.gui.widget.WidgetCheckBox;
 import me.desht.pneumaticcraft.common.progwidgets.ProgWidgetPickupItem;
-
-import static me.desht.pneumaticcraft.common.util.PneumaticCraftUtils.xlate;
+import net.minecraft.client.resources.I18n;
 
 public class GuiProgWidgetPickupItem extends GuiProgWidgetAreaShow<ProgWidgetPickupItem> {
     public GuiProgWidgetPickupItem(ProgWidgetPickupItem progWidget, GuiProgrammer guiProgrammer) {
@@ -16,8 +15,8 @@ public class GuiProgWidgetPickupItem extends GuiProgWidgetAreaShow<ProgWidgetPic
         super.init();
 
         addButton(new WidgetCheckBox(guiLeft + 8, guiTop + 25, 0xFF404040,
-                xlate("pneumaticcraft.gui.progWidget.pickup.canSteal"), b -> progWidget.setCanSteal(b.checked))
-                .setTooltip(xlate("pneumaticcraft.gui.progWidget.pickup.canSteal.tooltip"))
+                I18n.format("pneumaticcraft.gui.progWidget.pickup.canSteal"), b -> progWidget.setCanSteal(b.checked))
+                .setTooltip(I18n.format("pneumaticcraft.gui.progWidget.pickup.canSteal.tooltip"))
                 .setChecked(progWidget.canSteal())
         );
     }
