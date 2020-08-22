e104bbb17413530094893c5e90d686bd50964bda
Des Herriott <des.herriott@gmail.com>
2020-08-22 11:37:26 +0100

    Experimental change for programmer gui & gl scissor Testing with #584

--- src/main/java/me/desht/pneumaticcraft/client/gui/GuiUnitProgrammer.java
+++ src/main/java/me/desht/pneumaticcraft/client/gui/GuiUnitProgrammer.java
@@ -149,10 +149,15 @@ public class GuiUnitProgrammer extends Screen {
         lastZoom = scaleScroll.getState();
 
         MainWindow mw = minecraft.getMainWindow();
-        float sfX = (float) mw.getWidth() / mw.getScaledWidth();
-        float sfY = (float) mw.getHeight() / mw.getScaledHeight();
-        GL11.glScissor((int)((guiLeft + startX) * sfX), (int)((mw.getScaledHeight() - areaHeight - (guiTop + startY)) * sfY) + 1,
-                (int)(areaWidth * sfX), (int)(areaHeight * sfY));
+//        float sfX = (float) mw.getWidth() / mw.getScaledWidth();
+//        float sfY = (float) mw.getHeight() / mw.getScaledHeight();
+//        GL11.glScissor((int)((guiLeft + startX) * sfX), (int)((mw.getScaledHeight() - areaHeight - (guiTop + startY)) * sfY) + 1,
+//                (int)(areaWidth * sfX), (int)(areaHeight * sfY));
+        double sf = mw.getGuiScaleFactor();
+        GL11.glScissor(
+                (int)((guiLeft + startX) * sf), (int)((mw.getScaledHeight() - areaHeight - (guiTop + startY)) * sf),
+                (int)(areaWidth * sf), (int)(areaHeight * sf)
+        );
         GL11.glEnable(GL11.GL_SCISSOR_TEST);
 
         RenderSystem.pushMatrix();
