ba4ae22865f4a795ae3164fddb638c734d7e094b
Des Herriott <des.herriott@gmail.com>
2020-08-26 08:40:45 +0100

    Fixed "departing" and "arrived" being switched for Goto widget  #594

--- src/main/java/me/desht/pneumaticcraft/client/gui/programmer/GuiProgWidgetGoToLocation.java
+++ src/main/java/me/desht/pneumaticcraft/client/gui/programmer/GuiProgWidgetGoToLocation.java
@@ -21,17 +21,17 @@ public class GuiProgWidgetGoToLocation<T extends ProgWidgetGoToLocation> extends
 
         List<WidgetRadioButton> radioButtons = new ArrayList<>();
         WidgetRadioButton radioButton = new WidgetRadioButton(guiLeft + 8, guiTop + 24, 0xFF404040,
-                I18n.format("pneumaticcraft.gui.progWidget.goto.doneWhenDeparting"),b -> progWidget.doneWhenDeparting = false);
-        radioButton.checked = !progWidget.doneWhenDeparting;
-        radioButton.setTooltip(PneumaticCraftUtils.splitString(I18n.format("pneumaticcraft.gui.progWidget.goto.doneWhenDeparting.tooltip")));
+                I18n.format("pneumaticcraft.gui.progWidget.goto.doneWhenArrived"),b -> progWidget.setDoneWhenDeparting(false));
+        radioButton.checked = !progWidget.doneWhenDeparting();
+        radioButton.setTooltip(PneumaticCraftUtils.splitString(I18n.format("pneumaticcraft.gui.progWidget.goto.doneWhenArrived.tooltip")));
         addButton(radioButton);
         radioButtons.add(radioButton);
         radioButton.otherChoices = radioButtons;
 
         WidgetRadioButton radioButton2 = new WidgetRadioButton(guiLeft + 8, guiTop + 38, 0xFF404040,
-                I18n.format("pneumaticcraft.gui.progWidget.goto.doneWhenArrived"), b -> progWidget.doneWhenDeparting = true);
-        radioButton2.checked = progWidget.doneWhenDeparting;
-        radioButton2.setTooltip(PneumaticCraftUtils.splitString(I18n.format("pneumaticcraft.gui.progWidget.goto.doneWhenArrived.tooltip")));
+                I18n.format("pneumaticcraft.gui.progWidget.goto.doneWhenDeparting"), b -> progWidget.setDoneWhenDeparting(true));
+        radioButton2.checked = progWidget.doneWhenDeparting();
+        radioButton2.setTooltip(PneumaticCraftUtils.splitString(I18n.format("pneumaticcraft.gui.progWidget.goto.doneWhenDeparting.tooltip")));
         addButton(radioButton2);
         radioButtons.add(radioButton2);
         radioButton2.otherChoices = radioButtons;
