ff5c09d0d94a4e5655d0bb64940202eae94cd759
Des Herriott <des.herriott@gmail.com>
2020-09-01 14:27:18 +0100

    Fix heat-tintable blocks flashing blue briefly when placed down #602

--- src/main/java/me/desht/pneumaticcraft/common/heat/SyncedTemperature.java
+++ src/main/java/me/desht/pneumaticcraft/common/heat/SyncedTemperature.java
@@ -18,7 +18,9 @@ public class SyncedTemperature {
      * @return the synced temperature
      */
     public int getSyncedTemp() {
-        return syncedTemp;
+        // -1 indicates no temp synced yet, so use default temp
+        // https://github.com/TeamPneumatic/pnc-repressurized/issues/602
+        return syncedTemp == -1 ? currentTemp : syncedTemp;
     }
 
     /**
