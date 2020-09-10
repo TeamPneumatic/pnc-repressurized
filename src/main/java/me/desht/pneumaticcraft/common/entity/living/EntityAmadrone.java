6c694dcb5dcc3ed6035357275c70bdbfeab3e857
Des Herriott <des.herriott@gmail.com>
2020-09-10 10:03:39 +0100

    Fix Amadrones thinking they only had one inv slot #608

--- src/main/java/me/desht/pneumaticcraft/common/entity/living/EntityAmadrone.java
+++ src/main/java/me/desht/pneumaticcraft/common/entity/living/EntityAmadrone.java
@@ -1,16 +1,25 @@
 package me.desht.pneumaticcraft.common.entity.living;
 
+import me.desht.pneumaticcraft.api.PNCCapabilities;
 import me.desht.pneumaticcraft.api.item.EnumUpgrade;
+import me.desht.pneumaticcraft.common.core.ModEntities;
+import me.desht.pneumaticcraft.common.core.ModItems;
+import me.desht.pneumaticcraft.common.util.UpgradableItemUtils;
 import net.minecraft.entity.EntityType;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
+import net.minecraft.world.gen.Heightmap;
+import net.minecraftforge.items.ItemStackHandler;
 
 import javax.annotation.Nonnull;
 
 public class EntityAmadrone extends EntityDrone {
+    private static ItemStack amadroneStack = ItemStack.EMPTY;
+
     public enum AmadronAction { TAKING_PAYMENT, RESTOCKING }
 
     private ResourceLocation handlingOffer; // ID of an Amadron offer
@@ -22,8 +31,32 @@ public class EntityAmadrone extends EntityDrone {
     public EntityAmadrone(EntityType<? extends EntityDrone> type, World world) {
         super(type, world, null);
 
-        getAirHandler().addAir(100000);
         setCustomName(new TranslationTextComponent("pneumaticcraft.drone.amadronDeliveryDrone"));
+    }
+
+    public static EntityAmadrone makeAmadrone(World world, BlockPos pos) {
+        EntityAmadrone drone = new EntityAmadrone(ModEntities.AMADRONE.get(), world);
+        drone.readFromItemStack(getAmadroneStack());
+
+        int startY = world.getHeight(Heightmap.Type.WORLD_SURFACE, pos.add(30, 0, 0)).getY() + 27 + world.rand.nextInt(6);
+        drone.setPosition(pos.getX() + 27 + world.rand.nextInt(6), startY, pos.getZ() + world.rand.nextInt(6) - 3);
+
+        return drone;
+    }
+
+    private static ItemStack getAmadroneStack() {
+        if (amadroneStack.isEmpty()) {
+            amadroneStack = new ItemStack(ModItems.DRONE.get());
+            ItemStackHandler upgradeInv = new ItemStackHandler(9);
+            upgradeInv.setStackInSlot(0, EnumUpgrade.SPEED.getItemStack(10));
+            upgradeInv.setStackInSlot(1, EnumUpgrade.INVENTORY.getItemStack(35));
+            upgradeInv.setStackInSlot(2, EnumUpgrade.ITEM_LIFE.getItemStack(10));
+            upgradeInv.setStackInSlot(3, EnumUpgrade.SECURITY.getItemStack());
+            UpgradableItemUtils.setUpgrades(amadroneStack, upgradeInv);
+            amadroneStack.getCapability(PNCCapabilities.AIR_HANDLER_ITEM_CAPABILITY)
+                    .orElseThrow(RuntimeException::new).addAir(100000);
+        }
+        return amadroneStack;
     }
 
     public void setHandlingOffer(ResourceLocation offerId, int times, @Nonnull ItemStack usedTablet, String buyingPlayer, AmadronAction amadronAction) {
