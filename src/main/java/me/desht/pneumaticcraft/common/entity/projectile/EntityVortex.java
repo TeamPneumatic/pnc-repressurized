c9d8270f5ee1bfee7a3d5b54a3cfbd0d4f70cb56
Des Herriott <des.herriott@gmail.com>
2020-09-18 11:56:20 +0100

    Fixed vortex cannon vortices not breaking plants Looks like as of MC 1.16.2+, ThrowableEntity#onImpact() is no longer called for blocks with no collision box

--- src/main/java/me/desht/pneumaticcraft/common/entity/projectile/EntityVortex.java
+++ src/main/java/me/desht/pneumaticcraft/common/entity/projectile/EntityVortex.java
@@ -6,6 +6,7 @@ import net.minecraft.block.WebBlock;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.ThrowableEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
@@ -37,6 +38,12 @@ public class EntityVortex extends ThrowableEntity {
     @Override
     public void tick() {
         super.tick();
+
+        // onImpact() is no longer called for blocks with no collision box, like shrubs & crops, as of MC 1.16.2
+        if (!world.isRemote && vortexBreakable(world.getBlockState(getPosition()).getBlock())) {
+            handleVortexCollision(getOnPosition());
+        }
+
         setMotion(getMotion().scale(0.95));
         if (getMotion().lengthSquared() < 0.1D) {
             remove();
@@ -75,30 +82,17 @@ public class EntityVortex extends ThrowableEntity {
             Entity entity = ((EntityRayTraceResult) rtr).getEntity();
             entity.setMotion(entity.getMotion().add(this.getMotion().add(0, 0.4, 0)));
             ItemStack shears = new ItemStack(Items.SHEARS);
+            // func_234616_v_ = getShooter
             if (entity instanceof LivingEntity) {
-                shears.getItem().itemInteractionForEntity(shears, null, (LivingEntity) entity, Hand.MAIN_HAND);
+                PlayerEntity shooter = func_234616_v_() instanceof PlayerEntity ? (PlayerEntity) func_234616_v_() : null;
+                if (shooter != null) shears.getItem().itemInteractionForEntity(shears, shooter, (LivingEntity) entity, Hand.MAIN_HAND);
             }
         } else if (rtr.getType() == RayTraceResult.Type.BLOCK) {
             BlockPos pos = ((BlockRayTraceResult) rtr).getPos();
             Block block = world.getBlockState(pos).getBlock();
             if (vortexBreakable(block)) {
                 if (!world.isRemote) {
-                    BlockPos.Mutable mPos = new BlockPos.Mutable(pos.getX(), pos.getY(), pos.getZ());
-                    if (tryCutPlants(pos)) {
-                        int plantsCut = 1;
-                        for (int x = -2; x <= 2; x++) {
-                            for (int y = -2; y <= 2; y++) {
-                                for (int z = -2; z <= 2; z++) {
-                                    if (x == 0 && y == 0 && z == 0) continue;
-                                    mPos.setPos(pos.getX() + x, pos.getY() + y, pos.getZ() + z);
-                                    if (tryCutPlants(mPos)) plantsCut++;
-                                }
-                            }
-                        }
-                        // slow the vortex down for each plant it broke
-                        double mult = Math.pow(0.85D, plantsCut);
-                        setMotion(getMotion().scale(mult));
-                    }
+                    handleVortexCollision(pos);
                 }
             } else {
                 remove();
@@ -106,6 +100,25 @@ public class EntityVortex extends ThrowableEntity {
         }
         hitCounter++;
         if (hitCounter > 20) remove();
+    }
+
+    private void handleVortexCollision(BlockPos pos) {
+        BlockPos.Mutable mPos = new BlockPos.Mutable(pos.getX(), pos.getY(), pos.getZ());
+        if (tryCutPlants(pos)) {
+            int plantsCut = 1;
+            for (int x = -2; x <= 2; x++) {
+                for (int y = -2; y <= 2; y++) {
+                    for (int z = -2; z <= 2; z++) {
+                        if (x == 0 && y == 0 && z == 0) continue;
+                        mPos.setPos(pos.getX() + x, pos.getY() + y, pos.getZ() + z);
+                        if (tryCutPlants(mPos)) plantsCut++;
+                    }
+                }
+            }
+            // slow the vortex down for each plant it broke
+            double mult = Math.pow(0.85D, plantsCut);
+            setMotion(getMotion().scale(mult));
+        }
     }
 
     private boolean vortexBreakable(Block block) {
