890278d7c8a90212df98a90c23817e064a308d25
Des Herriott <des.herriott@gmail.com>
2020-09-25 14:48:28 +0100

    Build with Forge 34.1.0 RB, bit of worldgen cleanup Lose a couple of AT lines Don't bother with decorated feature (oil lake) registration

--- src/main/java/me/desht/pneumaticcraft/common/event/EventHandlerWorldGen.java
+++ src/main/java/me/desht/pneumaticcraft/common/event/EventHandlerWorldGen.java
@@ -3,12 +3,10 @@ package me.desht.pneumaticcraft.common.event;
 import me.desht.pneumaticcraft.common.config.PNCConfig;
 import me.desht.pneumaticcraft.common.core.ModBlocks;
 import me.desht.pneumaticcraft.common.core.ModDecorators;
-import me.desht.pneumaticcraft.lib.Names;
 import net.minecraft.world.gen.GenerationStage;
 import net.minecraft.world.gen.feature.BlockStateFeatureConfig;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 import net.minecraft.world.gen.feature.Feature;
-import net.minecraft.world.gen.feature.Features;
 import net.minecraft.world.gen.placement.ChanceConfig;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
@@ -20,9 +18,9 @@ public class EventHandlerWorldGen {
     @SubscribeEvent(priority = EventPriority.HIGH)
     public void onBiomeLoading(BiomeLoadingEvent event) {
         if (oilLake == null) {
-            oilLake = Features.register(Names.MOD_ID + ":" + "oil_lake", Feature.LAKE
+            oilLake = Feature.LAKE
                     .withConfiguration(new BlockStateFeatureConfig(ModBlocks.OIL.get().getDefaultState()))
-                    .withPlacement(ModDecorators.OIL_LAKE.get().configure(new ChanceConfig(PNCConfig.Common.General.oilGenerationChance))));
+                    .withPlacement(ModDecorators.OIL_LAKE.get().configure(new ChanceConfig(PNCConfig.Common.General.oilGenerationChance)));
         }
 
         if (!PNCConfig.Common.General.oilWorldGenBlacklist.contains(event.getName())) {
