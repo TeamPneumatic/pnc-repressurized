ad5248297598b1458c1c2b056876f87f24f32f99
Des Herriott <des.herriott@gmail.com>
2021-03-04 10:51:16 +0000

    register mek paxel harvest handler in our own namespace

--- src/main/java/me/desht/pneumaticcraft/common/thirdparty/mekanism/PaxelHandler.java
+++ src/main/java/me/desht/pneumaticcraft/common/thirdparty/mekanism/PaxelHandler.java
@@ -7,6 +7,8 @@ import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
+import static me.desht.pneumaticcraft.common.util.PneumaticCraftUtils.RL;
+
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, modid = Names.MOD_ID)
 public class PaxelHandler extends HoeHandler {
     public PaxelHandler() {
@@ -18,6 +20,6 @@ public class PaxelHandler extends HoeHandler {
 
     @SubscribeEvent
     public static void registerPaxelHandler(RegistryEvent.Register<HoeHandler> event) {
-        event.getRegistry().register(new PaxelHandler().setRegistryName(new ResourceLocation("mekanismtools:paxels")));
+        event.getRegistry().register(new PaxelHandler().setRegistryName(RL("mekanism_paxels")));
     }
 }
