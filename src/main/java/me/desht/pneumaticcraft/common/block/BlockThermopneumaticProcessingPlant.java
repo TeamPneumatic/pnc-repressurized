cfa4288a8bc54e77d0712925e110222ef15d978c
Des Herriott <des.herriott@gmail.com>
2021-03-25 17:41:08 +0000

    Better block shape for the TPP Also fixes culling issue, yay

--- src/main/java/me/desht/pneumaticcraft/common/block/BlockThermopneumaticProcessingPlant.java
+++ src/main/java/me/desht/pneumaticcraft/common/block/BlockThermopneumaticProcessingPlant.java
@@ -2,24 +2,33 @@ package me.desht.pneumaticcraft.common.block;
 
 import me.desht.pneumaticcraft.common.core.ModBlocks;
 import me.desht.pneumaticcraft.common.tileentity.TileEntityThermopneumaticProcessingPlant;
+import me.desht.pneumaticcraft.common.util.VoxelShapeUtils;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
 
 public class BlockThermopneumaticProcessingPlant extends BlockPneumaticCraft {
-    private static final VoxelShape BOUNDS = Block.makeCuboidShape(0.01, 0, 0, 16, 12, 16);
+    private static final VoxelShape TANK_IN1 = Block.makeCuboidShape(0, 0, 9, 7, 12, 16);
+    private static final VoxelShape TANK_IN2 = Block.makeCuboidShape(9, 0, 9, 16, 12, 16);
+    private static final VoxelShape TANK_OUT = Block.makeCuboidShape(5, 0, 0, 11, 8, 6);
+    private static final VoxelShape SHAPE_N = VoxelShapes.or(TANK_IN1, TANK_IN2, TANK_OUT);
+    private static final VoxelShape SHAPE_E = VoxelShapeUtils.rotateY(SHAPE_N, 90);
+    private static final VoxelShape SHAPE_S = VoxelShapeUtils.rotateY(SHAPE_N, 180);
+    private static final VoxelShape SHAPE_W = VoxelShapeUtils.rotateY(SHAPE_N, 270);
+    private static final VoxelShape[] SHAPES = { SHAPE_S, SHAPE_W, SHAPE_N, SHAPE_E };
 
     public BlockThermopneumaticProcessingPlant() {
-        super(ModBlocks.defaultProps());
+        super(ModBlocks.defaultProps().notSolid());
     }
 
     @Override
-    public VoxelShape getShape(BlockState p_220053_1_, IBlockReader p_220053_2_, BlockPos p_220053_3_, ISelectionContext p_220053_4_) {
-        return BOUNDS;
+    public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+        return SHAPES[state.get(directionProperty()).getHorizontalIndex()];
     }
 
     @Override
