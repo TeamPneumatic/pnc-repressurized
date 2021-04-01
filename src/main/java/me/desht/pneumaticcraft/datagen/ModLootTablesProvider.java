5258a4294f9e57f097229ffbaefbb16a2dbe0c18
Ridanis <khaled.chikhou@gmail.com>
2021-04-01 20:45:06 +0200

    Compressed Stone

--- src/main/java/me/desht/pneumaticcraft/datagen/ModLootTablesProvider.java
+++ src/main/java/me/desht/pneumaticcraft/datagen/ModLootTablesProvider.java
@@ -78,7 +78,7 @@ public class ModLootTablesProvider extends LootTableProvider {
                         && ForgeRegistries.ITEMS.containsKey(b.getRegistryName())) {
                     addStandardSerializedDrop(b);
                 } else if (b == ModBlocks.REINFORCED_BRICK_SLAB.get() || b == ModBlocks.REINFORCED_STONE_SLAB.get() || b == ModBlocks.COMPRESSED_BRICK_SLAB.get() || b == ModBlocks.COMPRESSED_STONE_SLAB.get()) {
-                    add(b, BlockLootTables::createSlabItemTable);
+                    add(b, BlockLootTables::droppingSlab);
                 } else if (b.asItem() != Items.AIR) {
                     dropSelf(b);
                 }
