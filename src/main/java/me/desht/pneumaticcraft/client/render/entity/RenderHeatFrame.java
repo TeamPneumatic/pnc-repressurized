7f9258ade043c116d6859afd7fc573aad750f7c7
Ridanis <khaled.chikhou@gmail.com>
2021-04-01 16:40:17 +0200

    HeatFrame
    - Added texture for the 9 states to the render class

--- src/main/java/me/desht/pneumaticcraft/client/render/entity/RenderHeatFrame.java
+++ src/main/java/me/desht/pneumaticcraft/client/render/entity/RenderHeatFrame.java
@@ -5,19 +5,33 @@ import com.mojang.blaze3d.vertex.IVertexBuilder;
 import me.desht.pneumaticcraft.client.model.entity.semiblocks.ModelHeatFrame;
 import me.desht.pneumaticcraft.client.util.TintColor;
 import me.desht.pneumaticcraft.common.entity.semiblock.EntityHeatFrame;
+import me.desht.pneumaticcraft.common.entity.semiblock.EntityTransferGadget;
 import me.desht.pneumaticcraft.common.heat.HeatUtil;
+import me.desht.pneumaticcraft.common.heat.TemperatureCategory;
 import me.desht.pneumaticcraft.lib.Textures;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.util.Direction;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.util.math.vector.Vector3f;
 import net.minecraftforge.fml.client.registry.IRenderFactory;
 
 public class RenderHeatFrame extends RenderSemiblockBase<EntityHeatFrame> {
     public static final IRenderFactory<EntityHeatFrame> FACTORY = RenderHeatFrame::new;
+
+    private static final ResourceLocation[] TEXTURES = new ResourceLocation[TemperatureCategory.values().length];
+    static {
+        for (TemperatureCategory tc : TemperatureCategory.values()) {
+            TEXTURES[tc.getIndex()] = tc.getTextureName("heat_frame");
+        }
+    }
 
     private final ModelHeatFrame model = new ModelHeatFrame();
 
@@ -27,8 +41,6 @@ public class RenderHeatFrame extends RenderSemiblockBase<EntityHeatFrame> {
 
     @Override
     public void render(EntityHeatFrame entityIn, float entityYaw, float partialTicks, MatrixStack matrixStackIn, IRenderTypeBuffer bufferIn, int packedLightIn) {
-        TintColor tint = HeatUtil.getColourForTemperature(entityIn.getSyncedTemperature());
-        float[] f = tint.getComponents(null);
         AxisAlignedBB aabb = entityIn.getBoundingBox();
 
         matrixStackIn.push();
@@ -40,13 +52,14 @@ public class RenderHeatFrame extends RenderSemiblockBase<EntityHeatFrame> {
         }
 
         IVertexBuilder builder = bufferIn.getBuffer(RenderType.getEntityCutout(getEntityTexture(entityIn)));
-        model.render(matrixStackIn, builder, packedLightIn, OverlayTexture.getPackedUV(0F, false), f[0], f[1], f[2], f[3]);
+        model.render(matrixStackIn, builder, packedLightIn, OverlayTexture.getPackedUV(0F, false), 1f, 1f, 1f, 1f);
 
         matrixStackIn.pop();
     }
 
     @Override
     public ResourceLocation getEntityTexture(EntityHeatFrame entityHeatFrame) {
-        return Textures.MODEL_HEAT_FRAME;
+        TemperatureCategory tc = TemperatureCategory.forTemperature(entityHeatFrame.getSyncedTemperature());
+        return TEXTURES[tc.getIndex()];
     }
 }
