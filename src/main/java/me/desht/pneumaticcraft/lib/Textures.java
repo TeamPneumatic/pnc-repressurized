17913ee0975fbc417bb5d1946c3e0fbd7dbbea20
Ridanis <khaled.chikhou@gmail.com>
2021-04-01 10:57:14 +0200

    Initial Push
    - New Texture
    - Updated Models and Renders (Drone, DroneCore, DroneColorLayer, DroneFrameLayer, TransferGadget, Crop Support, HeatFrame, Minigun, SpawnerAgitator)
    - New Models (SpawnerAgitator)

--- src/main/java/me/desht/pneumaticcraft/lib/Textures.java
+++ src/main/java/me/desht/pneumaticcraft/lib/Textures.java
@@ -1,20 +1,3 @@
-/*
- * This file is part of pnc-repressurized.
- *
- *     pnc-repressurized is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- *
- *     pnc-repressurized is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
- *
- *     You should have received a copy of the GNU General Public License
- *     along with pnc-repressurized.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 package me.desht.pneumaticcraft.lib;
 
 import me.desht.pneumaticcraft.api.lib.Names;
@@ -51,11 +34,6 @@ public class Textures {
     public static final ResourceLocation MODEL_UNIVERSAL_SENSOR = modelTexture("universal_sensor.png");
     public static final ResourceLocation MODEL_DRONE_MINIGUN = modelTexture("drone_minigun.png");
     public static final ResourceLocation MODEL_HEAT_FRAME = modelTexture("heat_frame.png");
-    public static final ResourceLocation MODEL_LOGISTICS_FRAME_ACTIVE_PROVIDER = modelTexture("logistics_frame_active_provider.png");
-    public static final ResourceLocation MODEL_LOGISTICS_FRAME_PASSIVE_PROVIDER = modelTexture("logistics_frame_passive_provider.png");
-    public static final ResourceLocation MODEL_LOGISTICS_FRAME_DEFAULT_STORAGE = modelTexture("logistics_frame_default_storage.png");
-    public static final ResourceLocation MODEL_LOGISTICS_FRAME_STORAGE = modelTexture("logistics_frame_storage.png");
-    public static final ResourceLocation MODEL_LOGISTICS_FRAME_REQUESTER = modelTexture("logistics_frame_requester.png");
     public static final ResourceLocation MODEL_CROP_SUPPORT = modelTexture("crop_support.png");
     public static final ResourceLocation MODEL_SPAWNER_EXTRACTOR = modelTexture("spawner_extractor_top.png");
     public static final ResourceLocation MODEL_SPAWNER_AGITATOR = modelTexture("spawner_agitator.png");
@@ -64,19 +42,13 @@ public class Textures {
 
     // Tube Module textures
     public static final ResourceLocation MODEL_FLOW_DETECTOR = tubeModuleTexture("flow_detector.png");
-    public static final ResourceLocation MODEL_LOGISTICS_MODULE = tubeModuleTexture("logistics_module.png");
     public static final ResourceLocation MODEL_GAUGE = tubeModuleTexture("gauge_module.png");
-    public static final ResourceLocation MODEL_GAUGE_UPGRADED = tubeModuleTexture("gauge_module_upgraded.png");
     public static final ResourceLocation MODEL_AIR_GRATE = tubeModuleTexture("air_grate.png");
-    public static final ResourceLocation MODEL_AIR_GRATE_UPGRADED = tubeModuleTexture("air_grate_upgraded.png");
     public static final ResourceLocation MODEL_CHARGING_MODULE = tubeModuleTexture("charging_module.png");
-    public static final ResourceLocation MODEL_CHARGING_MODULE_UPGRADED = tubeModuleTexture("charging_module_upgraded.png");
+    public static final ResourceLocation MODEL_LOGISTICS_MODULE = tubeModuleTexture("logistics_module.png");
     public static final ResourceLocation MODEL_SAFETY_VALVE = tubeModuleTexture("safety_valve.png");
-    public static final ResourceLocation MODEL_SAFETY_VALVE_UPGRADED = tubeModuleTexture("safety_valve_upgraded.png");
     public static final ResourceLocation MODEL_REGULATOR_MODULE = tubeModuleTexture("regulator.png");
-    public static final ResourceLocation MODEL_REGULATOR_MODULE_UPGRADED = tubeModuleTexture("regulator_upgraded.png");
     public static final ResourceLocation MODEL_REDSTONE_MODULE = tubeModuleTexture("redstone.png");
-    public static final ResourceLocation MODEL_REDSTONE_MODULE_UPGRADED = tubeModuleTexture("redstone_upgraded.png");
 
     // Progwidget textures
     public static final ResourceLocation PROG_WIDGET_COMMENT = progWidgetTexture("comment_piece.png");
@@ -230,8 +202,6 @@ public class Textures {
     public static final ResourceLocation GUI_CHARGE_IDLE = guiIconTexture("gui_charge_idle.png");
     public static final ResourceLocation GUI_WHITELIST = guiIconTexture("gui_whitelist.png");
     public static final ResourceLocation GUI_BLACKLIST = guiIconTexture("gui_blacklist.png");
-    public static final ResourceLocation GUI_BAD_LOCATION = guiIconTexture("gui_bad_location.png");
-    public static final ResourceLocation GUI_OK_LOCATION = guiIconTexture("gui_ok_location.png");
 
     // widget textures
     public static final ResourceLocation WIDGET_ENERGY = guiTexture("widget/widget_energy.png");
