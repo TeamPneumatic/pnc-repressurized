0d8f7aa0cd441d08a7dcae5b7fb99ff4d99aebf7
Ridanis <khaled.chikhou@gmail.com>
2021-04-01 10:57:14 +0200

    Initial Push
    - New Texture
    - Updated Models and Renders (Drone, DroneCore, DroneColorLayer, DroneFrameLayer, TransferGadget, Crop Support, HeatFrame, Minigun, SpawnerAgitator)
    - New Models (SpawnerAgitator)

--- src/main/java/me/desht/pneumaticcraft/client/model/entity/ModelTransferGadget.java
+++ src/main/java/me/desht/pneumaticcraft/client/model/entity/ModelTransferGadget.java
@@ -14,36 +14,36 @@ public class ModelTransferGadget extends EntityModel<EntityTransferGadget> {
     private final ModelRenderer outPart2;
 
     public ModelTransferGadget() {
-        textureWidth = 64;
-        textureHeight = 16;
+        texWidth = 64;
+        texHeight = 16;
 
         inPart1 = new ModelRenderer(this);
-        inPart1.setRotationPoint(2.0F, 12.0F, 0.0F);
-        inPart1.setTextureOffset(32, 0).addBox(-1.5F, 2.0F, -4.0F, 1.0F, 8.0F, 8.0F, 0.0F, false);
+        inPart1.setPos(2.0F, 12.0F, 0.0F);
+        inPart1.texOffs(32, 0).addBox(-1.5F, 2.0F, -4.0F, 1.0F, 8.0F, 8.0F, 0.0F, false);
 
         inPart2 = new ModelRenderer(this);
-        inPart2.setRotationPoint(-2.0F, 12.0F, 0.0F);
-        inPart2.setTextureOffset(30, 0).addBox(0.5F, 4.0F, -2.0F, 1.0F, 4.0F, 4.0F, 0.0F, false);
+        inPart2.setPos(-2.0F, 12.0F, 0.0F);
+        inPart2.texOffs(30, 0).addBox(0.5F, 4.0F, -2.0F, 1.0F, 4.0F, 4.0F, 0.0F, false);
 
         betweenPart = new ModelRenderer(this);
-        betweenPart.setRotationPoint(0.0F, 12.0F, 0.0F);
-        betweenPart.setTextureOffset(18, 4).addBox(-0.5F, 3.0F, -3.0F, 1.0F, 6.0F, 6.0F, 0.0F, false);
+        betweenPart.setPos(0.0F, 12.0F, 0.0F);
+        betweenPart.texOffs(18, 4).addBox(-0.5F, 3.0F, -3.0F, 1.0F, 6.0F, 6.0F, 0.0F, false);
 
         outPart1 = new ModelRenderer(this);
-        outPart1.setRotationPoint(-2.0F, 12.0F, 0.0F);
-        outPart1.setTextureOffset(0, 0).addBox(0.5F, 2.0F, -4.0F, 1.0F, 8.0F, 8.0F, 0.0F, false);
+        outPart1.setPos(-2.0F, 12.0F, 0.0F);
+        outPart1.texOffs(0, 0).addBox(0.5F, 2.0F, -4.0F, 1.0F, 8.0F, 8.0F, 0.0F, false);
 
         outPart2 = new ModelRenderer(this);
-        outPart2.setRotationPoint(0.0F, 12.0F, 0.0F);
-        outPart2.setTextureOffset(10, 0).addBox(0.5F, 4.0F, -2.0F, 1.0F, 4.0F, 4.0F, 0.0F, false);
+        outPart2.setPos(0.0F, 12.0F, 0.0F);
+        outPart2.texOffs(10, 0).addBox(0.5F, 4.0F, -2.0F, 1.0F, 4.0F, 4.0F, 0.0F, false);
     }
 
     @Override
-    public void setRotationAngles(EntityTransferGadget entityIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
+    public void setupAnim(EntityTransferGadget entityIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
     }
 
     @Override
-    public void render(MatrixStack matrixStack, IVertexBuilder buffer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
+    public void renderToBuffer(MatrixStack matrixStack, IVertexBuilder buffer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
         inPart1.render(matrixStack, buffer, packedLight, packedOverlay);
         inPart2.render(matrixStack, buffer, packedLight, packedOverlay);
         betweenPart.render(matrixStack, buffer, packedLight, packedOverlay);
