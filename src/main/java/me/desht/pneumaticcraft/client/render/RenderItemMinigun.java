c127adf8e2f9f2be6a9776f8f6c9e8c047115536
Ridanis <khaled.chikhou@gmail.com>
2021-04-01 10:57:14 +0200

    Initial Push
    - New Texture
    - Updated Models and Renders (Drone, DroneCore, DroneColorLayer, DroneFrameLayer, TransferGadget, Crop Support, HeatFrame, Minigun, SpawnerAgitator)
    - New Models (SpawnerAgitator)

--- src/main/java/me/desht/pneumaticcraft/client/render/RenderItemMinigun.java
+++ src/main/java/me/desht/pneumaticcraft/client/render/RenderItemMinigun.java
@@ -8,7 +8,7 @@ import me.desht.pneumaticcraft.common.minigun.Minigun;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screen.inventory.InventoryScreen;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
-import net.minecraft.client.renderer.model.ItemCameraTransforms.TransformType;
+import net.minecraft.client.renderer.model.ItemCameraTransforms;
 import net.minecraft.client.renderer.tileentity.ItemStackTileEntityRenderer;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -20,42 +20,40 @@ public class RenderItemMinigun extends ItemStackTileEntityRenderer {
     private final ModelMinigun model = new ModelMinigun();
 
     @Override
-    public void renderByItem(ItemStack stack, TransformType transformType, MatrixStack matrixStack, IRenderTypeBuffer buffer, int combinedLightIn, int combinedOverlayIn) {
+    public void func_239207_a_(ItemStack stack, ItemCameraTransforms.TransformType transformType, MatrixStack matrixStack, IRenderTypeBuffer buffer, int combinedLightIn, int combinedOverlayIn) {
         if (stack.getItem() == ModItems.MINIGUN.get() && stack.hasTag()) {
             Minecraft mc = Minecraft.getInstance();
-            int id = stack.getTag().getInt(ItemMinigun.OWNING_PLAYER_ID);
-            Entity owningPlayer = Minecraft.getInstance().level.getEntity(id);
+            PlayerEntity player = mc.player;
+            int id = stack.getTag().getInt("owningPlayerId");
+            Entity owningPlayer = Minecraft.getInstance().world.getEntityByID(id);
             if (owningPlayer instanceof PlayerEntity) {
                 Minigun minigun = ((ItemMinigun) stack.getItem()).getMinigun(stack, (PlayerEntity) owningPlayer);
-                matrixStack.pushPose();
-                boolean thirdPerson = transformType == TransformType.THIRD_PERSON_RIGHT_HAND || transformType == TransformType.THIRD_PERSON_LEFT_HAND;
-                if (thirdPerson) {
-                    if (mc.screen instanceof InventoryScreen) {
-                        // our own gun in the rendered player model in inventory screen
-                        matrixStack.mulPose(Vector3f.XP.rotationDegrees(-180f));
-                        matrixStack.translate(0.5, -1, -0.5);
-                    } else {
-                        // rendering our own gun in 3rd person, or rendering someone else's gun
-                        matrixStack.scale(1f, -1f, -1f);
-                        matrixStack.mulPose(Vector3f.XP.rotationDegrees(75f));
-                        matrixStack.mulPose(Vector3f.YP.rotationDegrees(180));
-                        matrixStack.mulPose(Vector3f.ZP.rotationDegrees(0f));
-                        matrixStack.translate(-0.5, -2, -0.3);
-                    }
+                matrixStack.push();
+                if (!mc.gameSettings.getPointOfView().func_243192_a() || player.getEntityId() != owningPlayer.getEntityId()) {
+                    // rendering our own gun in 3rd person, or rendering someone else's gun
+                    matrixStack.scale(1f, -1f, -1f);
+                    matrixStack.rotate(Vector3f.XP.rotationDegrees(75f));
+                    matrixStack.rotate(Vector3f.YP.rotationDegrees(180));
+                    matrixStack.rotate(Vector3f.ZP.rotationDegrees(0f));
+                    matrixStack.translate(-0.5, -2, -0.3);
+                } else if (mc.currentScreen instanceof InventoryScreen) {
+                    // our own gun in the rendered player model in inventory screen
+                    matrixStack.rotate(Vector3f.XP.rotationDegrees(-180f));
+                    matrixStack.translate(0.5, -1, -0.5);
                 } else {
                     // our own gun in 1st person
                     matrixStack.scale(1.5f, 1.5f, 1.5f);
-                    matrixStack.mulPose(Vector3f.XP.rotationDegrees(0));
-                    matrixStack.mulPose(Vector3f.YP.rotationDegrees(0));
-                    matrixStack.mulPose(Vector3f.ZP.rotationDegrees(180));
-                    if (mc.options.mainHand == HandSide.RIGHT) {
+                    matrixStack.rotate(Vector3f.XP.rotationDegrees(0));
+                    matrixStack.rotate(Vector3f.YP.rotationDegrees(0));
+                    matrixStack.rotate(Vector3f.ZP.rotationDegrees(180));
+                    if (mc.gameSettings.mainHand == HandSide.RIGHT) {
                         matrixStack.translate(-1, -1.7, 0.1);
                     } else {
                         matrixStack.translate(0, 0, 0);
                     }
                 }
-                model.renderMinigun(matrixStack, buffer, combinedLightIn, combinedOverlayIn, minigun, mc.getFrameTime(), false);
-                matrixStack.popPose();
+                model.renderMinigun(matrixStack, buffer, combinedLightIn, combinedOverlayIn, minigun, mc.getRenderPartialTicks(), true);
+                matrixStack.pop();
             }
         }
     }
