0b9e7acca652432b3e28b00b9376a8a8f3cdd9e5
Ridanis <khaled.chikhou@gmail.com>
2021-04-22 13:21:37 +0200

    Pressurized Chamber

--- src/main/java/me/desht/pneumaticcraft/common/block/BlockPressureChamberInterface.java
+++ src/main/java/me/desht/pneumaticcraft/common/block/BlockPressureChamberInterface.java
@@ -1,20 +1,3 @@
-/*
- * This file is part of pnc-repressurized.
- *
- *     pnc-repressurized is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- *
- *     pnc-repressurized is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
- *
- *     You should have received a copy of the GNU General Public License
- *     along with pnc-repressurized.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 package me.desht.pneumaticcraft.common.block;
 
 import me.desht.pneumaticcraft.common.advancements.AdvancementTriggers;
@@ -47,24 +30,28 @@ public class BlockPressureChamberInterface extends BlockPneumaticCraft implement
     private static final EnumMap<Direction,VoxelShape> DOORS = new EnumMap<>(Direction.class);
     static {
         SHAPES.put(Axis.Z, VoxelShapes.or(
-                Block.box(0, 0, 0, 16, 3, 16),
-                Block.box(0, 13, 0, 16, 16, 16),
-                Block.box(0, 0, 0, 3, 16, 16),
-                Block.box(13, 0, 0, 16, 16, 16),
-                Block.box(3, 3, 0, 5, 5, 16),
-                Block.box(11, 3, 0, 13, 5, 16),
-                Block.box(3, 11, 0, 5, 13, 16),
-                Block.box(11, 11, 0, 13, 13, 16)
+                Block.makeCuboidShape(0, 12, 0, 16, 16, 16),
+                Block.makeCuboidShape(0, 0, 0, 16, 4, 16),
+                Block.makeCuboidShape(0, 4, 0, 4, 12, 16),
+                Block.makeCuboidShape(12, 4, 0, 16, 12, 16),
+                Block.makeCuboidShape(4, 4, 1, 12, 5, 2),
+                Block.makeCuboidShape(11, 5, 1, 12, 11, 2),
+                Block.makeCuboidShape(4, 11, 1, 12, 12, 2),
+                Block.makeCuboidShape(4, 5, 1, 5, 11, 2),
+                Block.makeCuboidShape(11, 5, 14, 12, 11, 15),
+                Block.makeCuboidShape(4, 4, 14, 12, 5, 15),
+                Block.makeCuboidShape(4, 5, 14, 5, 11, 15),
+                Block.makeCuboidShape(4, 11, 14, 12, 12, 15)
         ));
         SHAPES.put(Axis.Y, VoxelShapeUtils.rotateX(SHAPES.get(Axis.Z), 90));
         SHAPES.put(Axis.X, VoxelShapeUtils.rotateY(SHAPES.get(Axis.Z), 90));
 
-        DOORS.put(Direction.DOWN, Block.box(3, 1, 3, 13, 2, 13));
-        DOORS.put(Direction.UP, Block.box(3, 14, 3, 13, 15, 13));
-        DOORS.put(Direction.NORTH, Block.box(3, 3, 1, 13, 13, 2));
-        DOORS.put(Direction.SOUTH, Block.box(3, 3, 14, 13, 13, 15));
-        DOORS.put(Direction.WEST, Block.box(1, 3, 3, 2, 13, 13));
-        DOORS.put(Direction.EAST, Block.box(14, 3, 3, 15, 13, 13));
+        DOORS.put(Direction.DOWN, Block.makeCuboidShape(3, 1, 3, 13, 2, 13));
+        DOORS.put(Direction.UP, Block.makeCuboidShape(3, 14, 3, 13, 15, 13));
+        DOORS.put(Direction.NORTH, Block.makeCuboidShape(3, 3, 1, 13, 13, 2));
+        DOORS.put(Direction.SOUTH, Block.makeCuboidShape(3, 3, 14, 13, 13, 15));
+        DOORS.put(Direction.WEST, Block.makeCuboidShape(1, 3, 3, 2, 13, 13));
+        DOORS.put(Direction.EAST, Block.makeCuboidShape(14, 3, 3, 15, 13, 13));
     }
 
     public BlockPressureChamberInterface() {
@@ -78,9 +65,9 @@ public class BlockPressureChamberInterface extends BlockPneumaticCraft implement
 
         return PneumaticCraftUtils.getTileEntityAt(worldIn, pos, TileEntityPressureChamberInterface.class).map(teI -> {
             if (teI.outputProgress < TileEntityPressureChamberInterface.MAX_PROGRESS) {
-                return VoxelShapes.join(main, DOORS.get(dir), IBooleanFunction.OR);
+                return VoxelShapes.combineAndSimplify(main, DOORS.get(dir), IBooleanFunction.OR);
             } else if (teI.inputProgress < TileEntityPressureChamberInterface.MAX_PROGRESS) {
-                return VoxelShapes.join(main, DOORS.get(dir.getOpposite()), IBooleanFunction.OR);
+                return VoxelShapes.combineAndSimplify(main, DOORS.get(dir.getOpposite()), IBooleanFunction.OR);
             } else {
                 return main;
             }
@@ -106,19 +93,19 @@ public class BlockPressureChamberInterface extends BlockPneumaticCraft implement
      * Called when the block is placed in the world.
      */
     @Override
-    public void setPlacedBy(World par1World, BlockPos pos, BlockState state, LivingEntity par5EntityLiving, ItemStack iStack) {
-        super.setPlacedBy(par1World, pos, state, par5EntityLiving, iStack);
-        if (!par1World.isClientSide && TileEntityPressureChamberValve.checkIfProperlyFormed(par1World, pos)) {
+    public void onBlockPlacedBy(World par1World, BlockPos pos, BlockState state, LivingEntity par5EntityLiving, ItemStack iStack) {
+        super.onBlockPlacedBy(par1World, pos, state, par5EntityLiving, iStack);
+        if (!par1World.isRemote && TileEntityPressureChamberValve.checkIfProperlyFormed(par1World, pos)) {
             AdvancementTriggers.PRESSURE_CHAMBER.trigger((ServerPlayerEntity) par5EntityLiving);
         }
     }
 
     @Override
-    public void onRemove(BlockState state, World world, BlockPos pos, BlockState newState, boolean isMoving) {
-        if (state.getBlock() != newState.getBlock() && !world.isClientSide) {
+    public void onReplaced(BlockState state, World world, BlockPos pos, BlockState newState, boolean isMoving) {
+        if (state.getBlock() != newState.getBlock() && !world.isRemote) {
             PneumaticCraftUtils.getTileEntityAt(world, pos, TileEntityPressureChamberInterface.class)
                     .ifPresent(TileEntityPressureChamberWall::onBlockBreak);
         }
-        super.onRemove(state, world, pos, newState, isMoving);
+        super.onReplaced(state, world, pos, newState, isMoving);
     }
 }
