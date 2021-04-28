7123c69f96c96b578ab8fd4b76c921bf6d1095fb
Ridanis <khaled.chikhou@gmail.com>
2021-04-28 11:42:52 +0200

    Tube Modules (2 of 2)

--- src/main/java/me/desht/pneumaticcraft/client/render/tube_module/RenderFlowDetectorModule.java
+++ src/main/java/me/desht/pneumaticcraft/client/render/tube_module/RenderFlowDetectorModule.java
@@ -9,23 +9,23 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.MathHelper;
 
 public class RenderFlowDetectorModule extends TubeModuleRendererBase<ModuleFlowDetector> {
-    private static final int TUBE_PARTS = 9;
+    private static final int TUBE_PARTS = 4;
 
-    private final ModelRenderer shape1;
+    private final ModelRenderer face;
 
     public RenderFlowDetectorModule() {
-        shape1 = new ModelRenderer(64, 32, 0, 8);
-        shape1.addBox(-1F, -3F, -2F, 2, 1, 5);
-        shape1.setRotationPoint(0F, 16F, 4.5F);
-        shape1.mirror = true;
+        face = new ModelRenderer(32, 32, 0, 0);
+        face.addBox(-2.0F, -3.0F, -2.0F, 4.0F, 1.0F, 5.0F);
+        face.setRotationPoint(0.0F, 16.0F, 4.5F);
+        face.mirror = true;
     }
 
     @Override
     protected void renderDynamic(ModuleFlowDetector module, MatrixStack matrixStack, IVertexBuilder builder, float partialTicks, int combinedLight, int combinedOverlay, float r, float g, float b, float a) {
         float rot = module != null ? MathHelper.lerp(partialTicks, module.oldRotation, module.rotation) : 0f;
         for (int i = 0; i < TUBE_PARTS; i++) {
-            shape1.rotateAngleZ = (float)i / TUBE_PARTS * 2 * (float)Math.PI + rot;
-            shape1.render(matrixStack, builder, combinedLight, combinedOverlay);
+            face.rotateAngleZ = (float)i / TUBE_PARTS * 2 * (float)Math.PI + rot;
+            face.render(matrixStack, builder, combinedLight, combinedOverlay);
         }
     }
 
