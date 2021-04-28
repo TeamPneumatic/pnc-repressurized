19561ca2c4a0199a83dcb1675a7dfaee058f1199
Ridanis <khaled.chikhou@gmail.com>
2021-04-28 17:30:21 +0200

    Updated Tube Modules in player Inv

--- src/main/java/me/desht/pneumaticcraft/client/render/tube_module/RenderAirGrateModule.java
+++ src/main/java/me/desht/pneumaticcraft/client/render/tube_module/RenderAirGrateModule.java
@@ -20,47 +20,47 @@ public class RenderAirGrateModule extends TubeModuleRendererBase<ModuleAirGrate>
 
     public RenderAirGrateModule() {
         top = new ModelRenderer(128, 64, 42, 19);
-        top.addBox(0F, 0F, 0F, 14, 0, 14);
+        top.addBox(0F, 0F, 0F, 14F, 0F, 14F);
         top.setRotationPoint(-7F, 9F, 8F);
         top.mirror = true;
         top.rotateAngleX = -1.570796F;
 
         side1 = new ModelRenderer(128, 64, 0, 18);
-        side1.addBox(0F, 0F, 0F, 16, 1, 1);
+        side1.addBox(0F, 0F, 0F, 16F, 1F, 1F);
         side1.setRotationPoint(-8F, 23F, 7F);
         side1.mirror = true;
 
         side2 = new ModelRenderer(128, 64, 0, 21);
-        side2.addBox(0F, 0F, 0F, 16, 1, 1);
+        side2.addBox(0F, 0F, 0F, 16F, 1F, 1F);
         side2.setRotationPoint(-8F, 8F, 7F);
         side2.mirror = true;
 
         side3 = new ModelRenderer(128, 64, 50, 0);
-        side3.addBox(0F, 0F, 0F, 1, 1, 14);
+        side3.addBox(0F, 0F, 0F, 1F, 1F, 14F);
         side3.setRotationPoint(-8F, 23F, 7F);
         side3.mirror = true;
         side3.rotateAngleX = 1.570796F;
 
         side4 = new ModelRenderer(128, 64, 82, 0);
-        side4.addBox(0F, 0F, 0F, 1, 1, 14);
+        side4.addBox(0F, 0F, 0F, 1F, 1F, 14F);
         side4.setRotationPoint(7F, 23F, 7F);
         side4.mirror = true;
         side4.rotateAngleX = 1.570796F;
 
         base1 = new ModelRenderer(128, 64, 69, 0);
-        base1.addBox(0F, 0F, 0F, 6, 2, 6);
+        base1.addBox(0F, 0F, 0F, 6F, 2F, 6F);
         base1.setRotationPoint(-3F, 13F, 4F);
         base1.mirror = true;
         base1.rotateAngleX = -1.570796F;
 
         base2 = new ModelRenderer(128, 64, 0, 25);
-        base2.addBox(0F, 0F, 0F, 12, 2, 12);
+        base2.addBox(0F, 0F, 0F, 12F, 2F, 12F);
         base2.setRotationPoint(-6F, 10F, 6F);
         base2.mirror = true;
         base2.rotateAngleX = -1.570796F;
 
         base3 = new ModelRenderer(128, 64, 0, 0);
-        base3.addBox(2F, 0F, 0F, 16, 1, 16);
+        base3.addBox(2F, 0F, 0F, 16F, 1F, 16F);
         base3.setRotationPoint(-10F, 8F, 7F);
         base3.mirror = true;
         base3.rotateAngleX = -1.570796F;
