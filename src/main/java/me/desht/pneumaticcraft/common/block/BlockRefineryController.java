3fae930304e252827f6156df87200d9e3f1b7b3d
Ridanis <khaled.chikhou@gmail.com>
2021-06-22 23:04:48 +0200

    Refinery Controller, and Output
    - Fluid Rendering (missing Oil)
    - Updated textures
    - Updated voxelshapes

--- src/main/java/me/desht/pneumaticcraft/common/block/BlockRefineryController.java
+++ src/main/java/me/desht/pneumaticcraft/common/block/BlockRefineryController.java
@@ -20,39 +20,39 @@ import java.util.stream.Stream;
 
 public class BlockRefineryController extends BlockPneumaticCraft {
     private static final VoxelShape SHAPE_N = Stream.of(
-            Block.makeCuboidShape(6, 11, 12.5, 7, 12, 14.5),
-            Block.makeCuboidShape(14, 10, 0, 16, 13, 2),
-            Block.makeCuboidShape(14, 10, 14, 16, 13, 16),
-            Block.makeCuboidShape(0, 10, 0, 2, 13, 2),
-            Block.makeCuboidShape(0, 10, 14, 2, 13, 16),
-            Block.makeCuboidShape(0, 0, 0, 16, 1, 16),
-            Block.makeCuboidShape(7.5, 11, 12.5, 8.5, 12, 14.5),
-            Block.makeCuboidShape(4.5, 11, 12.5, 5.5, 12, 14.5),
-            Block.makeCuboidShape(3, 11, 12.5, 4, 12, 14.5),
-            Block.makeCuboidShape(7.5, 12, 13.5, 8.5, 14, 14.5),
-            Block.makeCuboidShape(6, 12, 13.5, 7, 14, 14.5),
-            Block.makeCuboidShape(4.5, 12, 13.5, 5.5, 14, 14.5),
-            Block.makeCuboidShape(3, 12, 13.5, 4, 14, 14.5),
-            Block.makeCuboidShape(2, 14, 13, 9, 16, 16),
-            Block.makeCuboidShape(3, 1, 0, 10, 7, 1),
-            Block.makeCuboidShape(2, 1, 13, 9, 9, 15),
-            Block.makeCuboidShape(2, 1, 1, 14, 16, 13),
-            Block.makeCuboidShape(9, 15, 15, 16, 16, 16),
-            Block.makeCuboidShape(0, 15, 15, 2, 16, 16),
-            Block.makeCuboidShape(0.5, 1, 0.5, 1.5, 15, 1.5),
-            Block.makeCuboidShape(14.5, 1, 0.5, 15.5, 15, 1.5),
-            Block.makeCuboidShape(0.5, 1, 14.5, 1.5, 15, 15.5),
-            Block.makeCuboidShape(0, 15, 6, 1, 16, 15),
-            Block.makeCuboidShape(0, 14, 3, 2, 16, 6),
-            Block.makeCuboidShape(0, 15, 1, 1, 16, 3),
-            Block.makeCuboidShape(0, 15, 0, 16, 16, 1),
-            Block.makeCuboidShape(15, 15, 1, 16, 16, 7),
-            Block.makeCuboidShape(14, 14, 7, 16, 16, 11),
-            Block.makeCuboidShape(15, 15, 11, 16, 16, 15),
-            Block.makeCuboidShape(14.5, 1, 14.5, 15.5, 15, 15.5),
-            Block.makeCuboidShape(1.5, 11, 0.5, 14.5, 12, 1.5),
-            Block.makeCuboidShape(2.5, 10.5, 10.5, 9, 12.5, 12.5),
-            Block.makeCuboidShape(2.75, 1, 10.75, 8.75, 11, 12.25)
+            Block.box(6, 11, 12.5, 7, 12, 14.5),
+            Block.box(14, 10, 0, 16, 13, 2),
+            Block.box(14, 10, 14, 16, 13, 16),
+            Block.box(0, 10, 0, 2, 13, 2),
+            Block.box(0, 10, 14, 2, 13, 16),
+            Block.box(0, 0, 0, 16, 1, 16),
+            Block.box(7.5, 11, 12.5, 8.5, 12, 14.5),
+            Block.box(4.5, 11, 12.5, 5.5, 12, 14.5),
+            Block.box(3, 11, 12.5, 4, 12, 14.5),
+            Block.box(7.5, 12, 13.5, 8.5, 14, 14.5),
+            Block.box(6, 12, 13.5, 7, 14, 14.5),
+            Block.box(4.5, 12, 13.5, 5.5, 14, 14.5),
+            Block.box(3, 12, 13.5, 4, 14, 14.5),
+            Block.box(2, 14, 13, 9, 16, 16),
+            Block.box(3, 1, 0, 10, 7, 1),
+            Block.box(2, 1, 13, 9, 9, 15),
+            Block.box(2, 1, 1, 14, 16, 13),
+            Block.box(9, 15, 15, 16, 16, 16),
+            Block.box(0, 15, 15, 2, 16, 16),
+            Block.box(0.5, 1, 0.5, 1.5, 15, 1.5),
+            Block.box(14.5, 1, 0.5, 15.5, 15, 1.5),
+            Block.box(0.5, 1, 14.5, 1.5, 15, 15.5),
+            Block.box(0, 15, 6, 1, 16, 15),
+            Block.box(0, 14, 3, 2, 16, 6),
+            Block.box(0, 15, 1, 1, 16, 3),
+            Block.box(0, 15, 0, 16, 16, 1),
+            Block.box(15, 15, 1, 16, 16, 7),
+            Block.box(14, 14, 7, 16, 16, 11),
+            Block.box(15, 15, 11, 16, 16, 15),
+            Block.box(14.5, 1, 14.5, 15.5, 15, 15.5),
+            Block.box(1.5, 11, 0.5, 14.5, 12, 1.5),
+            Block.box(2.5, 10.5, 10.5, 9, 12.5, 12.5),
+            Block.box(2.75, 1, 10.75, 8.75, 11, 12.25)
     ).reduce((v1, v2) -> {return VoxelShapes.combineAndSimplify(v1, v2, IBooleanFunction.OR);}).get();
 
     private static final VoxelShape SHAPE_E = VoxelShapeUtils.rotateY(SHAPE_N, 90);
@@ -71,8 +71,8 @@ public class BlockRefineryController extends BlockPneumaticCraft {
 
     @Override
     public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
-        Direction d = state.get(directionProperty());
-        return SHAPES[d.getHorizontalIndex()];
+        Direction d = state.getValue(directionProperty());
+        return SHAPES[d.get2DDataValue()];
     }
 
     @Override
--- src/main/java/me/desht/pneumaticcraft/common/block/BlockRefineryOutput.java
+++ src/main/java/me/desht/pneumaticcraft/common/block/BlockRefineryOutput.java
@@ -37,40 +37,40 @@ import java.util.stream.Stream;
 
 public class BlockRefineryOutput extends BlockPneumaticCraft {
     private static final VoxelShape SHAPE_N = Stream.of(
-            Block.makeCuboidShape(0, 0, 0, 16, 1, 16),
-            Block.makeCuboidShape(13, 11, 14.5, 14.5, 12, 15.5),
-            Block.makeCuboidShape(12, 1, 14, 13, 13, 16),
-            Block.makeCuboidShape(3, 1, 14, 4, 13, 16),
-            Block.makeCuboidShape(1.5, 11, 14.5, 3, 12, 15.5),
-            Block.makeCuboidShape(0, 10, 14, 2, 13, 16),
-            Block.makeCuboidShape(14, 10, 14, 16, 13, 16),
-            Block.makeCuboidShape(14.5, 1, 14.5, 15.5, 15, 15.5),
-            Block.makeCuboidShape(14.5, 1, 0.5, 15.5, 15, 1.5),
-            Block.makeCuboidShape(14, 10, 0, 16, 13, 2),
-            Block.makeCuboidShape(1.5, 11, 0.5, 14.5, 12, 1.5),
-            Block.makeCuboidShape(2, 1, 1, 14, 16, 13),
-            Block.makeCuboidShape(4, 1, 13, 12, 14, 16),
-            Block.makeCuboidShape(0, 15, 15, 16, 16, 16),
-            Block.makeCuboidShape(0, 15, 0, 16, 16, 1),
-            Block.makeCuboidShape(0, 15, 1, 1, 16, 3),
-            Block.makeCuboidShape(0, 15, 6, 1, 16, 15),
-            Block.makeCuboidShape(15, 15, 1, 16, 16, 7),
-            Block.makeCuboidShape(15, 15, 11, 16, 16, 15),
-            Block.makeCuboidShape(0.5, 11, 1.5, 1.5, 12, 14.5),
-            Block.makeCuboidShape(0.5, 1, 0.5, 1.5, 15, 1.5),
-            Block.makeCuboidShape(0.5, 1, 14.5, 1.5, 15, 15.5),
-            Block.makeCuboidShape(0, 10, 0, 2, 13, 2),
-            Block.makeCuboidShape(14, 14, 7, 16, 16, 11),
-            Block.makeCuboidShape(13.25, 7.5, 9.25, 14.25, 9.5, 11.25),
-            Block.makeCuboidShape(13.25, 7.5, 6.75, 14.25, 9.5, 8.75),
-            Block.makeCuboidShape(0, 13, 3, 2, 16, 6),
-            Block.makeCuboidShape(0, 3.5, 3, 2, 5.5, 6),
-            Block.makeCuboidShape(13.5, 8, 7.25, 15.5, 9, 8.25),
-            Block.makeCuboidShape(13.5, 8, 9.75, 15.5, 9, 10.75),
-            Block.makeCuboidShape(14.5, 1, 7.25, 15.5, 8, 8.25),
-            Block.makeCuboidShape(14.5, 1, 9.75, 15.5, 8, 10.75),
-            Block.makeCuboidShape(0.5, 1, 3.5, 2.5, 4, 5.5),
-            Block.makeCuboidShape(14.5, 11, 1.5, 15.5, 12, 14.5)
+            Block.box(0, 0, 0, 16, 1, 16),
+            Block.box(13, 11, 14.5, 14.5, 12, 15.5),
+            Block.box(12, 1, 14, 13, 13, 16),
+            Block.box(3, 1, 14, 4, 13, 16),
+            Block.box(1.5, 11, 14.5, 3, 12, 15.5),
+            Block.box(0, 10, 14, 2, 13, 16),
+            Block.box(14, 10, 14, 16, 13, 16),
+            Block.box(14.5, 1, 14.5, 15.5, 15, 15.5),
+            Block.box(14.5, 1, 0.5, 15.5, 15, 1.5),
+            Block.box(14, 10, 0, 16, 13, 2),
+            Block.box(1.5, 11, 0.5, 14.5, 12, 1.5),
+            Block.box(2, 1, 1, 14, 16, 13),
+            Block.box(4, 1, 13, 12, 14, 16),
+            Block.box(0, 15, 15, 16, 16, 16),
+            Block.box(0, 15, 0, 16, 16, 1),
+            Block.box(0, 15, 1, 1, 16, 3),
+            Block.box(0, 15, 6, 1, 16, 15),
+            Block.box(15, 15, 1, 16, 16, 7),
+            Block.box(15, 15, 11, 16, 16, 15),
+            Block.box(0.5, 11, 1.5, 1.5, 12, 14.5),
+            Block.box(0.5, 1, 0.5, 1.5, 15, 1.5),
+            Block.box(0.5, 1, 14.5, 1.5, 15, 15.5),
+            Block.box(0, 10, 0, 2, 13, 2),
+            Block.box(14, 14, 7, 16, 16, 11),
+            Block.box(13.25, 7.5, 9.25, 14.25, 9.5, 11.25),
+            Block.box(13.25, 7.5, 6.75, 14.25, 9.5, 8.75),
+            Block.box(0, 13, 3, 2, 16, 6),
+            Block.box(0, 3.5, 3, 2, 5.5, 6),
+            Block.box(13.5, 8, 7.25, 15.5, 9, 8.25),
+            Block.box(13.5, 8, 9.75, 15.5, 9, 10.75),
+            Block.box(14.5, 1, 7.25, 15.5, 8, 8.25),
+            Block.box(14.5, 1, 9.75, 15.5, 8, 10.75),
+            Block.box(0.5, 1, 3.5, 2.5, 4, 5.5),
+            Block.box(14.5, 11, 1.5, 15.5, 12, 14.5)
     ).reduce((v1, v2) -> {return VoxelShapes.combineAndSimplify(v1, v2, IBooleanFunction.OR);}).get();
 
     private static final VoxelShape SHAPE_E = VoxelShapeUtils.rotateY(SHAPE_N, 90);
@@ -93,22 +93,22 @@ public class BlockRefineryOutput extends BlockPneumaticCraft {
     }
 
     @Override
-    public ActionResultType onBlockActivated(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockRayTraceResult brtr) {
+    public ActionResultType use(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockRayTraceResult brtr) {
         return PneumaticCraftUtils.getTileEntityAt(world, pos, TileEntityRefineryOutput.class).map(te -> {
             // normally, activating any refinery block would open the controller TE's gui, but if we
             // activate with a fluid tank in hand (which can actually transfer fluid out),
             // then we must activate the actual refinery output that was clicked
-            boolean canTransferFluid = FluidUtil.getFluidHandler(ItemHandlerHelper.copyStackWithSize(player.getHeldItem(hand), 1))
-                    .map(heldHandler -> FluidUtil.getFluidHandler(world, pos, brtr.getFace())
+            boolean canTransferFluid = FluidUtil.getFluidHandler(ItemHandlerHelper.copyStackWithSize(player.getItemInHand(hand), 1))
+                    .map(heldHandler -> FluidUtil.getFluidHandler(world, pos, brtr.getDirection())
                             .map(refineryHandler -> couldTransferFluidOut(heldHandler, refineryHandler))
                             .orElse(false))
                     .orElse(false);
             if (canTransferFluid) {
-                return super.onBlockActivated(state, world, pos, player, hand, brtr);
-            } else if (!world.isRemote) {
+                return super.use(state, world, pos, player, hand, brtr);
+            } else if (!world.isClientSide) {
                 TileEntityRefineryController master = te.getRefineryController();
                 if (master != null) {
-                    NetworkHooks.openGui((ServerPlayerEntity) player, master, master.getPos());
+                    NetworkHooks.openGui((ServerPlayerEntity) player, master, master.getBlockPos());
                 }
             }
             return ActionResultType.SUCCESS;
@@ -127,24 +127,24 @@ public class BlockRefineryOutput extends BlockPneumaticCraft {
     }
 
     @Override
-    public boolean isValidPosition(BlockState state, IWorldReader worldIn, BlockPos pos) {
+    public boolean canSurvive(BlockState state, IWorldReader worldIn, BlockPos pos) {
         int nOutputs = 0;
         int up = 1, down = 1;
-        while (worldIn.getBlockState(pos.up(up++)).getBlock() instanceof BlockRefineryOutput) {
+        while (worldIn.getBlockState(pos.above(up++)).getBlock() instanceof BlockRefineryOutput) {
             nOutputs++;
         }
-        while (worldIn.getBlockState(pos.down(down++)).getBlock() instanceof BlockRefineryOutput) {
+        while (worldIn.getBlockState(pos.below(down++)).getBlock() instanceof BlockRefineryOutput) {
             nOutputs++;
         }
-        return nOutputs < RefineryRecipe.MAX_OUTPUTS  && super.isValidPosition(state, worldIn, pos);
+        return nOutputs < RefineryRecipe.MAX_OUTPUTS  && super.canSurvive(state, worldIn, pos);
     }
 
     @Override
-    public BlockState updatePostPlacement(BlockState stateIn, Direction facing, BlockState facingState, IWorld worldIn, BlockPos currentPos, BlockPos facingPos) {
-        if (!worldIn.isRemote() && facingState.getBlock() == ModBlocks.REFINERY_OUTPUT.get()) {
+    public BlockState updateShape(BlockState stateIn, Direction facing, BlockState facingState, IWorld worldIn, BlockPos currentPos, BlockPos facingPos) {
+        if (!worldIn.isClientSide() && facingState.getBlock() == ModBlocks.REFINERY_OUTPUT.get()) {
             recache(worldIn, currentPos);
         }
-        return super.updatePostPlacement(stateIn, facing, facingState, worldIn, currentPos, facingPos);
+        return super.updateShape(stateIn, facing, facingState, worldIn, currentPos, facingPos);
     }
 
     private void recache(IWorld world, BlockPos pos) {
