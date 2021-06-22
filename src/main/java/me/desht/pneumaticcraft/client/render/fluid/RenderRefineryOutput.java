5b4325e64345275154d28786d6bcab20c347fc33
Ridanis <khaled.chikhou@gmail.com>
2021-06-22 23:04:48 +0200

    Refinery Controller, and Output
    - Fluid Rendering (missing Oil)
    - Updated textures
    - Updated voxelshapes

--- src/main/java/me/desht/pneumaticcraft/client/render/fluid/RenderRefineryOutput.java
+++ src/main/java/me/desht/pneumaticcraft/client/render/fluid/RenderRefineryOutput.java
@@ -1,20 +1,3 @@
-/*
- * This file is part of pnc-repressurized.
- *
- *     pnc-repressurized is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- *
- *     pnc-repressurized is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
- *
- *     You should have received a copy of the GNU General Public License
- *     along with pnc-repressurized.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 package me.desht.pneumaticcraft.client.render.fluid;
 
 import me.desht.pneumaticcraft.common.tileentity.TileEntityRefineryOutput;
@@ -43,7 +26,7 @@ public class RenderRefineryOutput extends AbstractFluidTER<TileEntityRefineryOut
 
     @Override
     Collection<TankRenderInfo> getTanksToRender(TileEntityRefineryOutput te) {
-        return Collections.singletonList(new TankRenderInfo(te.getOutputTank(), BOUNDS[te.getRotation().get2DDataValue()]).without(Direction.DOWN));
+        return Collections.singletonList(new TankRenderInfo(te.getOutputTank(), BOUNDS[te.getRotation().getHorizontalIndex()]).without(Direction.DOWN));
     }
 
 
