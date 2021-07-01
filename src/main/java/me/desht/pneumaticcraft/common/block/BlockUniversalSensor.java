e8e1795b14f2a5ecc8ca477f6684b02610e28f46
Ridanis <khaled.chikhou@gmail.com>
2021-07-01 11:55:38 +0200

    More stuff!
    - Universal Sensor
    - UV Light Box
    - TPP
    - Vacuum Pump

--- src/main/java/me/desht/pneumaticcraft/common/block/BlockUniversalSensor.java
+++ src/main/java/me/desht/pneumaticcraft/common/block/BlockUniversalSensor.java
@@ -1,20 +1,3 @@
-/*
- * This file is part of pnc-repressurized.
- *
- *     pnc-repressurized is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- *
- *     pnc-repressurized is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
- *
- *     You should have received a copy of the GNU General Public License
- *     along with pnc-repressurized.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 package me.desht.pneumaticcraft.common.block;
 
 import me.desht.pneumaticcraft.common.core.ModBlocks;
@@ -26,21 +9,23 @@ import net.minecraft.state.StateContainer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.IBooleanFunction;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
 
 public class BlockUniversalSensor extends BlockPneumaticCraft {
-    private static final VoxelShape SHAPE = Block.box(0, 0, 0, 16, 4, 16);
+    private static final VoxelShape SHAPE = VoxelShapes.combineAndSimplify(Block.makeCuboidShape(4, 2, 4, 12, 7, 12), Block.makeCuboidShape(1, 0, 1, 15, 2, 15), IBooleanFunction.OR);
 
     public BlockUniversalSensor() {
         super(ModBlocks.defaultProps());
 
-        registerDefaultState(getStateDefinition().any()
-                .setValue(NORTH, false)
-                .setValue(SOUTH, false)
-                .setValue(WEST, false)
-                .setValue(EAST, false)
+        setDefaultState(getStateContainer().getBaseState()
+                .with(NORTH, false)
+                .with(SOUTH, false)
+                .with(WEST, false)
+                .with(EAST, false)
         );
     }
 
@@ -50,8 +35,8 @@ public class BlockUniversalSensor extends BlockPneumaticCraft {
     }
 
     @Override
-    protected void createBlockStateDefinition(StateContainer.Builder<Block, BlockState> builder) {
-        super.createBlockStateDefinition(builder);
+    protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+        super.fillStateContainer(builder);
 
         builder.add(BlockPneumaticCraft.NORTH, BlockPneumaticCraft.SOUTH, BlockPneumaticCraft.WEST, BlockPneumaticCraft.EAST);
     }
@@ -62,20 +47,20 @@ public class BlockUniversalSensor extends BlockPneumaticCraft {
     }
 
     @Override
-    public int getDirectSignal(BlockState blockState, IBlockReader blockAccess, BlockPos pos, Direction side) {
+    public int getStrongPower(BlockState blockState, IBlockReader blockAccess, BlockPos pos, Direction side) {
         return PneumaticCraftUtils.getTileEntityAt(blockAccess, pos, TileEntityUniversalSensor.class)
                 .map(te -> side == Direction.UP ? te.redstoneStrength : 0)
                 .orElse(0);
     }
 
     @Override
-    public int getSignal(BlockState blockState, IBlockReader blockAccess, BlockPos pos, Direction side) {
+    public int getWeakPower(BlockState blockState, IBlockReader blockAccess, BlockPos pos, Direction side) {
         return PneumaticCraftUtils.getTileEntityAt(blockAccess, pos, TileEntityUniversalSensor.class)
                 .map(te -> te.redstoneStrength).orElse(0);
     }
 
     @Override
-    public boolean isSignalSource(BlockState state) {
+    public boolean canProvidePower(BlockState state) {
         return true;
     }
 }
