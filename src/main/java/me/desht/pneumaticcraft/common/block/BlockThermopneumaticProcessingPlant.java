4ce5631b1a33c310ba78ed153dfde10af579a795
Ridanis <khaled.chikhou@gmail.com>
2021-07-01 11:55:38 +0200

    More stuff!
    - Universal Sensor
    - UV Light Box
    - TPP
    - Vacuum Pump

--- src/main/java/me/desht/pneumaticcraft/common/block/BlockThermopneumaticProcessingPlant.java
+++ src/main/java/me/desht/pneumaticcraft/common/block/BlockThermopneumaticProcessingPlant.java
@@ -17,59 +17,59 @@ import java.util.stream.Stream;
 
 public class BlockThermopneumaticProcessingPlant extends BlockPneumaticCraft {
     private static final VoxelShape SHAPE_N = Stream.of(
-            Block.makeCuboidShape(14, 5, 5, 16, 10, 9),
-            Block.makeCuboidShape(0, 5, 5, 2, 10, 9),
-            Block.makeCuboidShape(4, 1, 0, 12, 13, 1),
-            Block.makeCuboidShape(0, 10, 0, 2, 13, 2),
-            Block.makeCuboidShape(0.5, 13, 14.5, 1.5, 15, 15.5),
-            Block.makeCuboidShape(14.5, 1, 0.5, 15.5, 15, 1.5),
-            Block.makeCuboidShape(0.5, 1, 0.5, 1.5, 15, 1.5),
-            Block.makeCuboidShape(0.5, 11, 1.5, 1.5, 12, 9),
-            Block.makeCuboidShape(14.5, 11, 1.5, 15.5, 12, 9),
-            Block.makeCuboidShape(15, 15, 1, 16, 16, 15),
-            Block.makeCuboidShape(0, 15, 1, 1, 16, 15),
-            Block.makeCuboidShape(0, 15, 0, 16, 16, 1),
-            Block.makeCuboidShape(0, 0, 0, 16, 1, 16),
-            Block.makeCuboidShape(9, 13, 11, 16, 13, 16),
-            Block.makeCuboidShape(0, 13, 11, 7, 13, 16),
-            Block.makeCuboidShape(16, 1, 11, 16, 13, 16),
-            Block.makeCuboidShape(7, 1, 11, 7, 13, 16),
-            Block.makeCuboidShape(9, 1, 11, 9, 13, 16),
-            Block.makeCuboidShape(0, 1, 11, 0, 13, 16),
-            Block.makeCuboidShape(9, 1, 16, 16, 13, 16),
-            Block.makeCuboidShape(0, 1, 16, 7, 13, 16),
-            Block.makeCuboidShape(14, 1, 9, 16, 13, 11),
-            Block.makeCuboidShape(0, 1, 9, 2, 13, 11),
-            Block.makeCuboidShape(2, 1, 1, 14, 16, 11),
-            Block.makeCuboidShape(14, 10, 0, 16, 13, 2),
-            Block.makeCuboidShape(14.5, 13, 14.5, 15.5, 15, 15.5),
-            Block.makeCuboidShape(7, 2.5, 13.5, 9, 3.5, 14.5),
-            Block.makeCuboidShape(11.5, 13.5, 10.5, 12.5, 14.5, 12.5),
-            Block.makeCuboidShape(3.5, 13.5, 10.5, 4.5, 14.5, 12.5),
-            Block.makeCuboidShape(11.5, 13.25, 12.5, 12.5, 14.5, 13.5),
-            Block.makeCuboidShape(3.5, 13.25, 12.5, 4.5, 14.5, 13.5),
-            Block.makeCuboidShape(8.75, 2, 13, 9, 4, 15),
-            Block.makeCuboidShape(7, 2, 13, 7.25, 4, 15),
-            Block.makeCuboidShape(11, 13, 11, 13, 15, 11.25),
-            Block.makeCuboidShape(3, 13, 11, 5, 15, 11.25),
-            Block.makeCuboidShape(11, 13, 12, 13, 13.25, 14),
-            Block.makeCuboidShape(3, 13, 12, 5, 13.25, 14),
-            Block.makeCuboidShape(0, 15, 15, 16, 16, 16),
-            Block.makeCuboidShape(12, 11, 0.5, 14, 12, 1.5),
-            Block.makeCuboidShape(2, 11, 0.5, 4, 12, 1.5)
-    ).reduce((v1, v2) -> {return VoxelShapes.combineAndSimplify(v1, v2, IBooleanFunction.OR);}).get();
+            Block.box(14, 5, 5, 16, 10, 9),
+            Block.box(0, 5, 5, 2, 10, 9),
+            Block.box(4, 1, 0, 12, 13, 1),
+            Block.box(0, 10, 0, 2, 13, 2),
+            Block.box(0.5, 13, 14.5, 1.5, 15, 15.5),
+            Block.box(14.5, 1, 0.5, 15.5, 15, 1.5),
+            Block.box(0.5, 1, 0.5, 1.5, 15, 1.5),
+            Block.box(0.5, 11, 1.5, 1.5, 12, 9),
+            Block.box(14.5, 11, 1.5, 15.5, 12, 9),
+            Block.box(15, 15, 1, 16, 16, 15),
+            Block.box(0, 15, 1, 1, 16, 15),
+            Block.box(0, 15, 0, 16, 16, 1),
+            Block.box(0, 0, 0, 16, 1, 16),
+            Block.box(9, 13, 11, 16, 13, 16),
+            Block.box(0, 13, 11, 7, 13, 16),
+            Block.box(16, 1, 11, 16, 13, 16),
+            Block.box(7, 1, 11, 7, 13, 16),
+            Block.box(9, 1, 11, 9, 13, 16),
+            Block.box(0, 1, 11, 0, 13, 16),
+            Block.box(9, 1, 16, 16, 13, 16),
+            Block.box(0, 1, 16, 7, 13, 16),
+            Block.box(14, 1, 9, 16, 13, 11),
+            Block.box(0, 1, 9, 2, 13, 11),
+            Block.box(2, 1, 1, 14, 16, 11),
+            Block.box(14, 10, 0, 16, 13, 2),
+            Block.box(14.5, 13, 14.5, 15.5, 15, 15.5),
+            Block.box(7, 2.5, 13.5, 9, 3.5, 14.5),
+            Block.box(11.5, 13.5, 10.5, 12.5, 14.5, 12.5),
+            Block.box(3.5, 13.5, 10.5, 4.5, 14.5, 12.5),
+            Block.box(11.5, 13.25, 12.5, 12.5, 14.5, 13.5),
+            Block.box(3.5, 13.25, 12.5, 4.5, 14.5, 13.5),
+            Block.box(8.75, 2, 13, 9, 4, 15),
+            Block.box(7, 2, 13, 7.25, 4, 15),
+            Block.box(11, 13, 11, 13, 15, 11.25),
+            Block.box(3, 13, 11, 5, 15, 11.25),
+            Block.box(11, 13, 12, 13, 13.25, 14),
+            Block.box(3, 13, 12, 5, 13.25, 14),
+            Block.box(0, 15, 15, 16, 16, 16),
+            Block.box(12, 11, 0.5, 14, 12, 1.5),
+            Block.box(2, 11, 0.5, 4, 12, 1.5)
+    ).reduce((v1, v2) -> {return VoxelShapes.join(v1, v2, IBooleanFunction.OR);}).get();
     private static final VoxelShape SHAPE_E = VoxelShapeUtils.rotateY(SHAPE_N, 90);
     private static final VoxelShape SHAPE_S = VoxelShapeUtils.rotateY(SHAPE_N, 180);
     private static final VoxelShape SHAPE_W = VoxelShapeUtils.rotateY(SHAPE_N, 270);
     private static final VoxelShape[] SHAPES = { SHAPE_S, SHAPE_W, SHAPE_N, SHAPE_E };
 
     public BlockThermopneumaticProcessingPlant() {
-        super(ModBlocks.defaultProps().notSolid());
+        super(ModBlocks.defaultProps().noOcclusion());
     }
 
     @Override
     public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
-        return SHAPES[state.get(directionProperty()).getHorizontalIndex()];
+        return SHAPES[state.getValue(directionProperty()).get2DDataValue()];
     }
 
     @Override
--- src/main/java/me/desht/pneumaticcraft/common/block/BlockVacuumPump.java
+++ src/main/java/me/desht/pneumaticcraft/common/block/BlockVacuumPump.java
@@ -18,20 +18,20 @@ import java.util.stream.Stream;
 
 public class BlockVacuumPump extends BlockPneumaticCraft {
     private static final VoxelShape SHAPE_N = Stream.of(
-            Block.makeCuboidShape(3, 7.75, 7.75, 9, 8.25, 8.25),
-            Block.makeCuboidShape(15, 5, 5, 16, 11, 11),
-            Block.makeCuboidShape(9, 5, 4, 15, 12, 12),
-            Block.makeCuboidShape(3, 11, 5, 9, 11, 11),
-            Block.makeCuboidShape(0, 1, 3, 16, 5, 13),
-            Block.makeCuboidShape(13, 0, 4, 15, 1, 6),
-            Block.makeCuboidShape(13, 0, 10, 15, 1, 12),
-            Block.makeCuboidShape(1, 0, 4, 3, 1, 6),
-            Block.makeCuboidShape(1, 0, 10, 3, 1, 12),
-            Block.makeCuboidShape(3, 5, 5, 9, 11, 5),
-            Block.makeCuboidShape(3, 5, 11, 9, 11, 11),
-            Block.makeCuboidShape(0, 5, 5, 3, 11, 11),
-            Block.makeCuboidShape(12.5, 12, 7, 14.5, 12.25, 9),
-            Block.makeCuboidShape(0.5, 11, 7, 2.5, 11.25, 9)
+            Block.box(3, 7.75, 7.75, 9, 8.25, 8.25),
+            Block.box(15, 5, 5, 16, 11, 11),
+            Block.box(9, 5, 4, 15, 12, 12),
+            Block.box(3, 11, 5, 9, 11, 11),
+            Block.box(0, 1, 3, 16, 5, 13),
+            Block.box(13, 0, 4, 15, 1, 6),
+            Block.box(13, 0, 10, 15, 1, 12),
+            Block.box(1, 0, 4, 3, 1, 6),
+            Block.box(1, 0, 10, 3, 1, 12),
+            Block.box(3, 5, 5, 9, 11, 5),
+            Block.box(3, 5, 11, 9, 11, 11),
+            Block.box(0, 5, 5, 3, 11, 11),
+            Block.box(12.5, 12, 7, 14.5, 12.25, 9),
+            Block.box(0.5, 11, 7, 2.5, 11.25, 9)
     ).reduce((v1, v2) -> {return VoxelShapes.combineAndSimplify(v1, v2, IBooleanFunction.OR);}).get();
 
     private static final VoxelShape SHAPE_E = VoxelShapeUtils.rotateY(SHAPE_N, 90);
@@ -40,7 +40,7 @@ public class BlockVacuumPump extends BlockPneumaticCraft {
 
     private static final VoxelShape[] SHAPES = new VoxelShape[] { SHAPE_E, SHAPE_S, SHAPE_W, SHAPE_N };
 
-//    private static final VoxelShape COLLISION_SHAPE = Block.makeCuboidShape(2, 2, 2, 14, 14, 14);
+//    private static final VoxelShape COLLISION_SHAPE = Block.box(2, 2, 2, 14, 14, 14);
 
     public BlockVacuumPump() {
         super(ModBlocks.defaultProps());
