b82d3531b7dd5ab0b70fde63ae4ae38451a5639b
Ridanis <khaled.chikhou@gmail.com>
2021-07-01 11:55:38 +0200

    More stuff!
    - Universal Sensor
    - UV Light Box
    - TPP
    - Vacuum Pump

--- src/main/java/me/desht/pneumaticcraft/client/render/tileentity/RenderUniversalSensor.java
+++ src/main/java/me/desht/pneumaticcraft/client/render/tileentity/RenderUniversalSensor.java
@@ -12,53 +12,159 @@ import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.vector.Vector3f;
 
 public class RenderUniversalSensor extends AbstractTileModelRenderer<TileEntityUniversalSensor> {
-    private final ModelRenderer dish1;
-    private final ModelRenderer dish2;
-    private final ModelRenderer dish3;
-    private final ModelRenderer dish4;
-    private final ModelRenderer dish5;
-    private final ModelRenderer dish6;
+    private final ModelRenderer part1;
+    private final ModelRenderer part2;
+    private final ModelRenderer part3;
+    private final ModelRenderer part4;
+    private final ModelRenderer part5;
+    private final ModelRenderer part6;
+    private final ModelRenderer part7;
+    private final ModelRenderer part8;
+    private final ModelRenderer part8_r1;
+    private final ModelRenderer part9;
+    private final ModelRenderer part9_r1;
+    private final ModelRenderer part10;
+    private final ModelRenderer part10_r1;
+    private final ModelRenderer part11;
+    private final ModelRenderer part11_r1;
+    private final ModelRenderer part12;
+    private final ModelRenderer part12_r1;
+    private final ModelRenderer part13;
+    private final ModelRenderer part13_r1;
+    private final ModelRenderer part14;
+    private final ModelRenderer part14_r1;
+    private final ModelRenderer part15;
+    private final ModelRenderer part15_r1;
 
     public RenderUniversalSensor(TileEntityRendererDispatcher dispatcher) {
         super(dispatcher);
 
-        dish1 = new ModelRenderer(64, 64, 0, 33);
-        dish1.addBox(-2F, 0F, -2F, 4, 1, 4);
-        dish1.setPos(0F, 16F, 0F);
-        dish1.mirror = true;
+        part1 = new ModelRenderer(32, 32, 0, 0);
+        part1.setPos(0.0F, 16.0F, 0.0F);
+        part1.texOffs(0, 0).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 1.0F, 4.0F, 0.0F, true);
 
-        dish2 = new ModelRenderer(64, 64, 0, 38);
-        dish2.addBox(-3F, -1F, 0F, 1, 8, 4);
-        dish2.setPos(0F, 9F, -2F);
-        dish2.mirror = true;
-        dish2.zRot = -0.2268928F;
+        part2 = new ModelRenderer(32, 32, 0, 9);
+        part2.setPos(0.0F, 9.0F, -2.0F);
+        setRotationAngle(part2, 0.0F, 0.0F, -0.2269F);
+        part2.texOffs(0, 9).addBox(-3.0F, 4.0F, 0.5F, 1.0F, 3.0F, 3.0F, 0.0F, true);
 
-        dish3 = new ModelRenderer(64, 64, 0, 50);
-        dish3.addBox(-3.8F, 0F, 0.8F, 1, 4, 4);
-        dish3.setPos(0F, 8F, 0F);
-        dish3.mirror = true;
-        dish3.xRot = 0.0698132F;
-        dish3.yRot = 0.3839724F;
-        dish3.zRot = -0.2268928F;
+        part3 = new ModelRenderer(32, 32, 16, 3);
+        part3.setPos(0.0F, 12.0F, 0.0F);
+        setRotationAngle(part3, 0.0F, 0.0F, -0.2269F);
+        part3.texOffs(16, 3).addBox(-2.0F, 1.25F, -0.5F, 6.0F, 1.0F, 1.0F, 0.0F, true);
 
-        dish4 = new ModelRenderer(64, 64, 10, 50);
-        dish4.addBox(-3.8F, 0F, -4.7F, 1, 4, 4);
-        dish4.setPos(0F, 8F, 0F);
-        dish4.mirror = true;
-        dish4.xRot = -0.0698132F;
-        dish4.yRot = -0.3839724F;
-        dish4.zRot = -0.2268928F;
+        part4 = new ModelRenderer(32, 32, 16, 0);
+        part4.setPos(0.0F, 10.2F, 0.0F);
+        part4.texOffs(16, 0).addBox(3.25F, 1.25F, -1.0F, 1.0F, 1.0F, 2.0F, 0.0F, true);
 
-        dish5 = new ModelRenderer(64, 64, 0, 58);
-        dish5.addBox(-2F, 0F, -0.5F, 6, 1, 1);
-        dish5.setPos(0F, 12F, 0F);
-        dish5.mirror = true;
-        dish5.zRot = -0.2268928F;
+        part5 = new ModelRenderer(32, 32, 0, 5);
+        part5.setPos(0.0F, 9.0F, -2.0F);
+        setRotationAngle(part5, 0.0F, 0.0F, -0.2269F);
+        part5.texOffs(0, 5).addBox(-3.0F, 3.0F, 0.5F, 1.0F, 1.0F, 3.0F, 0.0F, true);
 
-        dish6 = new ModelRenderer(64, 64, 0, 60);
-        dish6.addBox(3F, 0F, -1F, 1, 1, 2);
-        dish6.setPos(0F, 10.2F, 0F);
-        dish6.mirror = true;
+        part6 = new ModelRenderer(32, 32, 0, 5);
+        part6.setPos(0.0F, 9.0F, -2.0F);
+        setRotationAngle(part6, 0.0F, 0.0F, -0.2269F);
+        part6.texOffs(0, 5).addBox(-3.0F, -2.0F, 0.5F, 1.0F, 1.0F, 3.0F, 0.0F, true);
+
+        part7 = new ModelRenderer(32, 32, 18, 9);
+        part7.setPos(0.0F, 9.0F, -2.0F);
+        setRotationAngle(part7, 0.0F, 0.0F, -0.2269F);
+        part7.texOffs(18, 9).addBox(-2.5F, -1.0F, 0.5F, 0.0F, 4.0F, 3.0F, 0.0F, true);
+
+        part8 = new ModelRenderer(32, 32, 20, 6);
+        part8.setPos(0.0F, 9.0F, -2.0F);
+        setRotationAngle(part8, 0.0F, 0.0F, -0.2269F);
+
+
+        part8_r1 = new ModelRenderer(32, 32, 20, 6);
+        part8_r1.setPos(-3.0F, 3.5F, 3.5F);
+        part8.addChild(part8_r1);
+        setRotationAngle(part8_r1, 0.0F, 0.3927F, 0.0F);
+        part8_r1.texOffs(20, 6).addBox(0.0F, -0.5F, 0.0F, 1.0F, 1.0F, 5.0F, 0.0F, true);
+
+        part9 = new ModelRenderer(32, 32, 20, 6);
+        part9.setPos(0.0F, 9.0F, -2.0F);
+        setRotationAngle(part9, 0.0F, 0.0F, -0.2269F);
+
+
+        part9_r1 = new ModelRenderer(32, 32, 20, 6);
+        part9_r1.setPos(-3.0F, -1.5F, 3.5F);
+        part9.addChild(part9_r1);
+        setRotationAngle(part9_r1, 0.0F, 0.3927F, 0.0F);
+        part9_r1.texOffs(20, 6).addBox(0.0F, -0.5F, 0.0F, 1.0F, 1.0F, 5.0F, 0.0F, true);
+
+        part10 = new ModelRenderer(32, 32, 15, 2);
+        part10.setPos(0.0F, 9.0F, -2.0F);
+        setRotationAngle(part10, 0.0F, 0.0F, -0.2269F);
+
+
+        part10_r1 = new ModelRenderer(32, 32, 15, 2);
+        part10_r1.setPos(-2.5F, -0.5F, 3.5F);
+        part10.addChild(part10_r1);
+        setRotationAngle(part10_r1, 0.0F, 0.3927F, 0.0F);
+        part10_r1.texOffs(15, 2).addBox(0.0F, -0.5F, 0.0F, 0.0F, 4.0F, 5.0F, 0.0F, true);
+
+        part11 = new ModelRenderer(32, 32, 8, 6);
+        part11.setPos(0.0F, 9.0F, 2.0F);
+        setRotationAngle(part11, 0.0F, 0.0F, -0.2269F);
+
+
+        part11_r1 = new ModelRenderer(32, 32, 8, 6);
+        part11_r1.setPos(-3.0F, 3.5F, -3.5F);
+        part11.addChild(part11_r1);
+        setRotationAngle(part11_r1, 0.0F, -0.3927F, 0.0F);
+        part11_r1.texOffs(8, 6).addBox(0.0F, -0.5F, -5.0F, 1.0F, 1.0F, 5.0F, 0.0F, true);
+
+        part12 = new ModelRenderer(32, 32, 8, 6);
+        part12.setPos(0.0F, 9.0F, 2.0F);
+        setRotationAngle(part12, 0.0F, 0.0F, -0.2269F);
+
+
+        part12_r1 = new ModelRenderer(32, 32, 8, 6);
+        part12_r1.setPos(-3.0F, -1.5F, -3.5F);
+        part12.addChild(part12_r1);
+        setRotationAngle(part12_r1, 0.0F, -0.3927F, 0.0F);
+        part12_r1.texOffs(8, 6).addBox(0.0F, -0.5F, -5.0F, 1.0F, 1.0F, 5.0F, 0.0F, true);
+
+        part13 = new ModelRenderer(32, 32, 8, 7);
+        part13.setPos(0.0F, 9.0F, 2.0F);
+        setRotationAngle(part13, 0.0F, 0.0F, -0.2269F);
+
+
+        part13_r1 = new ModelRenderer(32, 32, 8, 7);
+        part13_r1.setPos(-2.5F, -0.5F, -3.5F);
+        part13.addChild(part13_r1);
+        setRotationAngle(part13_r1, 0.0F, -0.3927F, 0.0F);
+        part13_r1.texOffs(8, 7).addBox(0.0F, -0.5F, -5.0F, 0.0F, 4.0F, 5.0F, 0.0F, true);
+
+        part14 = new ModelRenderer(32, 32, 28, 12);
+        part14.setPos(0.0F, 9.0F, -2.0F);
+        setRotationAngle(part14, 0.0F, 0.0F, -0.2269F);
+
+
+        part14_r1 = new ModelRenderer(32, 32, 28, 12);
+        part14_r1.setPos(-3.0F, 2.5F, 3.5F);
+        part14.addChild(part14_r1);
+        setRotationAngle(part14_r1, 0.0F, 0.3927F, 0.0F);
+        part14_r1.texOffs(28, 12).addBox(0.0F, -3.5F, 4.0F, 1.0F, 4.0F, 1.0F, 0.0F, true);
+
+        part15 = new ModelRenderer(32, 32, 28, 12);
+        part15.setPos(0.0F, 9.0F, 2.0F);
+        setRotationAngle(part15, 0.0F, 0.0F, -0.2269F);
+
+
+        part15_r1 = new ModelRenderer(32, 32, 28, 12);
+        part15_r1.setPos(-3.0F, 2.5F, -3.5F);
+        part15.addChild(part15_r1);
+        setRotationAngle(part15_r1, 0.0F, -0.3927F, 0.0F);
+        part15_r1.texOffs(28, 12).addBox(0.0F, -3.5F, -5.0F, 1.0F, 4.0F, 1.0F, 0.0F, true);
+    }
+
+    public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
+        modelRenderer.rotateAngleX = x;
+        modelRenderer.rotateAngleY = y;
+        modelRenderer.rotateAngleZ = z;
     }
 
     @Override
@@ -67,11 +173,20 @@ public class RenderUniversalSensor extends AbstractTileModelRenderer<TileEntityU
 
         float rotation = MathHelper.lerp(partialTicks, te.oldDishRotation, te.dishRotation);
         matrixStackIn.mulPose(Vector3f.YP.rotationDegrees(rotation));
-        dish1.render(matrixStackIn, builder, combinedLightIn, combinedOverlayIn);
-        dish2.render(matrixStackIn, builder, combinedLightIn, combinedOverlayIn);
-        dish3.render(matrixStackIn, builder, combinedLightIn, combinedOverlayIn);
-        dish4.render(matrixStackIn, builder, combinedLightIn, combinedOverlayIn);
-        dish5.render(matrixStackIn, builder, combinedLightIn, combinedOverlayIn);
-        dish6.render(matrixStackIn, builder, combinedLightIn, combinedOverlayIn);
+        part1.render(matrixStackIn, builder, combinedLightIn, combinedOverlayIn);
+        part2.render(matrixStackIn, builder, combinedLightIn, combinedOverlayIn);
+        part3.render(matrixStackIn, builder, combinedLightIn, combinedOverlayIn);
+        part4.render(matrixStackIn, builder, combinedLightIn, combinedOverlayIn);
+        part5.render(matrixStackIn, builder, combinedLightIn, combinedOverlayIn);
+        part6.render(matrixStackIn, builder, combinedLightIn, combinedOverlayIn);
+        part7.render(matrixStackIn, builder, combinedLightIn, combinedOverlayIn);
+        part8.render(matrixStackIn, builder, combinedLightIn, combinedOverlayIn);
+        part9.render(matrixStackIn, builder, combinedLightIn, combinedOverlayIn);
+        part10.render(matrixStackIn, builder, combinedLightIn, combinedOverlayIn);
+        part11.render(matrixStackIn, builder, combinedLightIn, combinedOverlayIn);
+        part12.render(matrixStackIn, builder, combinedLightIn, combinedOverlayIn);
+        part13.render(matrixStackIn, builder, combinedLightIn, combinedOverlayIn);
+        part14.render(matrixStackIn, builder, combinedLightIn, combinedOverlayIn);
+        part15.render(matrixStackIn, builder, combinedLightIn, combinedOverlayIn);
     }
 }
--- src/main/java/me/desht/pneumaticcraft/common/block/BlockVacuumPump.java
+++ src/main/java/me/desht/pneumaticcraft/common/block/BlockVacuumPump.java
@@ -2,31 +2,60 @@ package me.desht.pneumaticcraft.common.block;
 
 import me.desht.pneumaticcraft.common.core.ModBlocks;
 import me.desht.pneumaticcraft.common.tileentity.TileEntityVacuumPump;
+import me.desht.pneumaticcraft.common.util.VoxelShapeUtils;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.IBooleanFunction;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
 
+import java.util.stream.Stream;
+
 public class BlockVacuumPump extends BlockPneumaticCraft {
-    private static final VoxelShape BASE_SHAPE = Block.box(2, 0, 2, 14, 11, 14);
-    private static final VoxelShape COLLISION_SHAPE = Block.box(2, 2, 2, 14, 14, 14);
+    private static final VoxelShape SHAPE_N = Stream.of(
+            Block.box(3, 7.75, 7.75, 9, 8.25, 8.25),
+            Block.box(15, 5, 5, 16, 11, 11),
+            Block.box(9, 5, 4, 15, 12, 12),
+            Block.box(3, 11, 5, 9, 11, 11),
+            Block.box(0, 1, 3, 16, 5, 13),
+            Block.box(13, 0, 4, 15, 1, 6),
+            Block.box(13, 0, 10, 15, 1, 12),
+            Block.box(1, 0, 4, 3, 1, 6),
+            Block.box(1, 0, 10, 3, 1, 12),
+            Block.box(3, 5, 5, 9, 11, 5),
+            Block.box(3, 5, 11, 9, 11, 11),
+            Block.box(0, 5, 5, 3, 11, 11),
+            Block.box(12.5, 12, 7, 14.5, 12.25, 9),
+            Block.box(0.5, 11, 7, 2.5, 11.25, 9)
+    ).reduce((v1, v2) -> {return VoxelShapes.combineAndSimplify(v1, v2, IBooleanFunction.OR);}).get();
+
+    private static final VoxelShape SHAPE_E = VoxelShapeUtils.rotateY(SHAPE_N, 90);
+    private static final VoxelShape SHAPE_S = VoxelShapeUtils.rotateY(SHAPE_E, 90);
+    private static final VoxelShape SHAPE_W = VoxelShapeUtils.rotateY(SHAPE_S, 90);
+
+    private static final VoxelShape[] SHAPES = new VoxelShape[] { SHAPE_E, SHAPE_S, SHAPE_W, SHAPE_N };
+
+//    private static final VoxelShape COLLISION_SHAPE = Block.box(2, 2, 2, 14, 14, 14);
 
     public BlockVacuumPump() {
         super(ModBlocks.defaultProps());
     }
 
     @Override
-    public VoxelShape getShape(BlockState p_220053_1_, IBlockReader p_220053_2_, BlockPos p_220053_3_, ISelectionContext p_220053_4_) {
-        return BASE_SHAPE;
+    public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+        Direction d = state.get(directionProperty());
+        return SHAPES[d.getHorizontalIndex()];
     }
 
-    @Override
-    public VoxelShape getCollisionShape(BlockState p_220071_1_, IBlockReader p_220071_2_, BlockPos p_220071_3_, ISelectionContext p_220071_4_) {
-        return COLLISION_SHAPE;
-    }
+//    @Override
+//    public VoxelShape getCollisionShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+//        return COLLISION_SHAPE;
+//    }
 
     @Override
     protected Class<? extends TileEntity> getTileEntityClass() {
