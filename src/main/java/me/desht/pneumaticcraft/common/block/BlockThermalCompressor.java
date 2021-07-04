1c44392baf39d500f8cab05182f49848f8d67bc4
Ridanis <khaled.chikhou@gmail.com>
2021-07-04 20:56:31 +0200

    Display Table, Tag Workbench, and Thermal Compressor

--- src/main/java/me/desht/pneumaticcraft/common/block/BlockThermalCompressor.java
+++ src/main/java/me/desht/pneumaticcraft/common/block/BlockThermalCompressor.java
@@ -17,26 +17,26 @@ import java.util.stream.Stream;
 
 public class BlockThermalCompressor extends BlockPneumaticCraft implements ColorHandlers.IHeatTintable {
     private static final VoxelShape BOUNDS = Stream.of(
-            Block.makeCuboidShape(3, 3, 0, 13, 13, 1),
-            Block.makeCuboidShape(0, 15, 0, 16, 16, 1),
-            Block.makeCuboidShape(15, 1, 0, 16, 15, 1),
-            Block.makeCuboidShape(0, 1, 0, 1, 15, 1),
-            Block.makeCuboidShape(0, 0, 0, 16, 1, 1),
-            Block.makeCuboidShape(15, 0, 1, 16, 1, 15),
-            Block.makeCuboidShape(0, 0, 15, 16, 1, 16),
-            Block.makeCuboidShape(15, 1, 15, 16, 15, 16),
-            Block.makeCuboidShape(0, 15, 15, 16, 16, 16),
-            Block.makeCuboidShape(0, 1, 15, 1, 15, 16),
-            Block.makeCuboidShape(15, 15, 1, 16, 16, 15),
-            Block.makeCuboidShape(0, 0, 1, 1, 1, 15),
-            Block.makeCuboidShape(0, 15, 1, 1, 16, 15),
-            Block.makeCuboidShape(4, 15, 4, 12, 16, 12),
-            Block.makeCuboidShape(1, 1, 1, 15, 15, 15),
-            Block.makeCuboidShape(3, 3, 15, 13, 13, 16),
-            Block.makeCuboidShape(15, 3, 3, 16, 13, 13),
-            Block.makeCuboidShape(0, 3, 3, 1, 13, 13),
-            Block.makeCuboidShape(4, 0, 4, 12, 1, 12)
-    ).reduce((v1, v2) -> {return VoxelShapes.combineAndSimplify(v1, v2, IBooleanFunction.OR);}).get();
+            Block.box(3, 3, 0, 13, 13, 1),
+            Block.box(0, 15, 0, 16, 16, 1),
+            Block.box(15, 1, 0, 16, 15, 1),
+            Block.box(0, 1, 0, 1, 15, 1),
+            Block.box(0, 0, 0, 16, 1, 1),
+            Block.box(15, 0, 1, 16, 1, 15),
+            Block.box(0, 0, 15, 16, 1, 16),
+            Block.box(15, 1, 15, 16, 15, 16),
+            Block.box(0, 15, 15, 16, 16, 16),
+            Block.box(0, 1, 15, 1, 15, 16),
+            Block.box(15, 15, 1, 16, 16, 15),
+            Block.box(0, 0, 1, 1, 1, 15),
+            Block.box(0, 15, 1, 1, 16, 15),
+            Block.box(4, 15, 4, 12, 16, 12),
+            Block.box(1, 1, 1, 15, 15, 15),
+            Block.box(3, 3, 15, 13, 13, 16),
+            Block.box(15, 3, 3, 16, 13, 13),
+            Block.box(0, 3, 3, 1, 13, 13),
+            Block.box(4, 0, 4, 12, 1, 12)
+    ).reduce((v1, v2) -> {return VoxelShapes.join(v1, v2, IBooleanFunction.OR);}).get();
 
     public BlockThermalCompressor() {
         super(ModBlocks.defaultProps());
