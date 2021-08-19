58a1b84e8f1df6186e376d5f978c53d7dd2e0e11
Des Herriott <des.herriott@gmail.com>
2021-08-19 08:36:26 +0100

    Manometer heat enhancements Can now check the ambient temperature and the temperature of various other heat-aware blocks #863

--- src/main/java/me/desht/pneumaticcraft/common/item/ItemManometer.java
+++ src/main/java/me/desht/pneumaticcraft/common/item/ItemManometer.java
@@ -3,6 +3,8 @@ package me.desht.pneumaticcraft.common.item;
 import me.desht.pneumaticcraft.api.PNCCapabilities;
 import me.desht.pneumaticcraft.api.tileentity.IManoMeasurable;
 import me.desht.pneumaticcraft.common.core.ModItems;
+import me.desht.pneumaticcraft.common.heat.HeatExchangerLogicAmbient;
+import me.desht.pneumaticcraft.common.heat.HeatExchangerManager;
 import me.desht.pneumaticcraft.common.heat.HeatUtil;
 import me.desht.pneumaticcraft.common.util.DirectionUtil;
 import me.desht.pneumaticcraft.lib.PneumaticValues;
@@ -11,9 +13,7 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUseContext;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.ActionResultType;
-import net.minecraft.util.Direction;
-import net.minecraft.util.Hand;
+import net.minecraft.util.*;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
@@ -35,28 +35,38 @@ public class ItemManometer extends ItemPressurizable {
         PlayerEntity player = context.getPlayer();
         Direction side = context.getFace();
 
-        if (world.isRemote) return ActionResultType.PASS;
+        if (world.isRemote) return ActionResultType.SUCCESS;
 
         return stack.getCapability(PNCCapabilities.AIR_HANDLER_ITEM_CAPABILITY).map(h -> {
             if (h.getAir() < PneumaticValues.USAGE_ITEM_MANOMETER) {
                 player.sendStatusMessage(xlate("pneumaticcraft.message.misc.outOfAir", stack.getDisplayName()).mergeStyle(TextFormatting.RED), true);
                 return ActionResultType.FAIL;
             }
-            TileEntity te = world.getTileEntity(context.getPos());
-
             List<ITextComponent> curInfo = new ArrayList<>();
-            te.getCapability(PNCCapabilities.AIR_HANDLER_MACHINE_CAPABILITY, side)
-                    .ifPresent(teAirHandler -> teAirHandler.printManometerMessage(player, curInfo));
 
-            if (te instanceof IManoMeasurable) {
-                ((IManoMeasurable) te).printManometerMessage(player, curInfo);
-            }
+            TileEntity te = world.getTileEntity(context.getPos());
+            if (te != null) {
+                if (te instanceof INameable) {
+                    curInfo.add(((INameable) te).getDisplayName().deepCopy().mergeStyle(TextFormatting.AQUA));
+                } else {
+                    ItemStack stack1 = new ItemStack(te.getBlockState().getBlock());
+                    curInfo.add(stack1.getDisplayName().deepCopy().mergeStyle(TextFormatting.AQUA));
+                }
 
-            te.getCapability(PNCCapabilities.HEAT_EXCHANGER_CAPABILITY)
-                    .ifPresent(exchanger -> curInfo.add(HeatUtil.formatHeatString(exchanger.getTemperatureAsInt())));
-            for (Direction d : DirectionUtil.VALUES) {
-                te.getCapability(PNCCapabilities.HEAT_EXCHANGER_CAPABILITY, d)
-                        .ifPresent(exchanger -> curInfo.add(HeatUtil.formatHeatString(exchanger.getTemperatureAsInt())));
+                te.getCapability(PNCCapabilities.AIR_HANDLER_MACHINE_CAPABILITY, side)
+                        .ifPresent(teAirHandler -> teAirHandler.printManometerMessage(player, curInfo));
+
+                if (te instanceof IManoMeasurable) {
+                    ((IManoMeasurable) te).printManometerMessage(player, curInfo);
+                }
+
+                for (Direction d : DirectionUtil.VALUES) {
+                    te.getCapability(PNCCapabilities.HEAT_EXCHANGER_CAPABILITY, d)
+                            .ifPresent(exchanger -> curInfo.add(HeatUtil.formatHeatString(d, exchanger.getTemperatureAsInt())));
+                }
+            } else {
+                HeatExchangerManager.INSTANCE.getLogic(world, context.getPos(), side)
+                        .ifPresent(logic -> curInfo.add(HeatUtil.formatHeatString(side, logic.getTemperatureAsInt())));
             }
 
             if (curInfo.size() > 0) {
@@ -65,6 +75,20 @@ public class ItemManometer extends ItemPressurizable {
             }
             return ActionResultType.SUCCESS;
         }).orElse(ActionResultType.PASS);
+    }
+
+    @Override
+    public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
+        ItemStack stack = playerIn.getHeldItem(handIn);
+        if (worldIn.isRemote) return ActionResult.resultSuccess(stack);
+        return stack.getCapability(PNCCapabilities.AIR_HANDLER_ITEM_CAPABILITY).map(h -> {
+            if (h.getPressure() >= 0.1f) {
+                double temp = HeatExchangerLogicAmbient.getAmbientTemperature(worldIn, playerIn.getPosition());
+                playerIn.sendStatusMessage(HeatUtil.formatHeatString((int) temp), false);
+                return ActionResult.resultConsume(stack);
+            }
+            return ActionResult.resultFail(stack);
+        }).orElse(ActionResult.resultPass(stack));
     }
 
     @Override
