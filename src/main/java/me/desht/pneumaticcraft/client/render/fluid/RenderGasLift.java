fbd9fa5bdc24f2af3529b3d32d698b0467946bf5
Ridanis <khaled.chikhou@gmail.com>
2021-09-25 19:30:35 +0200

    Fluid Mixer, Drill Pipes, Gas Lift, Charging Station, and Charging Pad

--- src/main/java/me/desht/pneumaticcraft/client/render/fluid/RenderGasLift.java
+++ src/main/java/me/desht/pneumaticcraft/client/render/fluid/RenderGasLift.java
@@ -2,11 +2,13 @@ package me.desht.pneumaticcraft.client.render.fluid;
 
 import com.google.common.collect.ImmutableList;
 import me.desht.pneumaticcraft.common.tileentity.TileEntityGasLift;
+import me.desht.pneumaticcraft.common.tileentity.TileEntityKeroseneLamp;
 import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.AxisAlignedBB;
 
 import java.util.Collection;
+import java.util.Collections;
 
 public class RenderGasLift extends AbstractFluidTER<TileEntityGasLift> {
     private static final AxisAlignedBB TANK_BOUNDS = new AxisAlignedBB(0.1/16f, 4.1/16f, 0.1/16f, 15.9/16f, 12.9/16f, 15.9/16f);
