312856876143592ef48f6a87bab11dce5ae8709e
Ridanis <khaled.chikhou@gmail.com>
2021-09-26 17:48:59 +0200

    Air Compressor and Advanced Air Compressor

--- src/main/java/me/desht/pneumaticcraft/common/block/BlockChargingStation.java
+++ src/main/java/me/desht/pneumaticcraft/common/block/BlockChargingStation.java
@@ -27,35 +27,61 @@ import java.util.stream.Stream;
 public class BlockChargingStation extends BlockPneumaticCraftCamo {
     public static final BooleanProperty CHARGE_PAD = BooleanProperty.create("charge_pad");
 
-//    private static final VoxelShape PAD_FRAME = Block.box(3, 1, 3, 13, 16, 13);
+    private static final VoxelShape CHARGING_STATION_N = Stream.of(
+            Block.box(0, 0, 0, 16, 1, 16),
+            Block.box(1, 1, 1, 15, 3, 15),
+            Block.box(6, 6, 0, 10, 10, 1),
+            Block.box(5, 3, 1, 11, 11, 3),
+            Block.box(3, 3, 3, 13, 4, 13),
+            Block.box(11, 7.25, 1.25, 12.5, 7.75, 1.75),
+            Block.box(12, 3.25, 1.25, 12.5, 7.25, 1.75),
+            Block.box(11.25, 3.25, 2, 11.75, 8.25, 2.5),
+            Block.box(11.25, 3.25, 2.5, 11.75, 3.75, 3),
+            Block.box(12, 3.25, 1.75, 12.5, 3.75, 3.25),
+            Block.box(3.5, 7.25, 1.25, 5, 7.75, 1.75),
+            Block.box(3.5, 3.25, 1.75, 4, 3.75, 3.25),
+            Block.box(10.75, 8.25, 2, 11.75, 8.75, 2.5),
+            Block.box(3.5, 3.25, 1.25, 4, 7.25, 1.75),
+            Block.box(4.25, 8.25, 2, 5.25, 8.75, 2.5),
+            Block.box(4.25, 3.25, 2, 4.75, 8.25, 2.5),
+            Block.box(4.25, 3.25, 2.5, 4.75, 3.75, 3),
+            Block.box(5, 1, 0, 11, 5, 1)
+    ).reduce((v1, v2) -> VoxelShapes.join(v1, v2, IBooleanFunction.OR)).get();
 
-    private static final VoxelShape SHAPE_N = Stream.of(
-        Block.box(0, 0, 0, 16, 1, 16),
-        Block.box(1, 1, 1, 15, 3, 15),
-        Block.box(6, 6, 0, 10, 10, 1),
-        Block.box(5, 3, 1, 11, 11, 3),
-        Block.box(3, 3, 3, 13, 4, 13),
-        Block.box(11, 7.25, 1.25, 12.5, 7.75, 1.75),
-        Block.box(12, 3.25, 1.25, 12.5, 7.25, 1.75),
-        Block.box(11.25, 3.25, 2, 11.75, 8.25, 2.5),
-        Block.box(11.25, 3.25, 2.5, 11.75, 3.75, 3),
-        Block.box(12, 3.25, 1.75, 12.5, 3.75, 3.25),
-        Block.box(3.5, 7.25, 1.25, 5, 7.75, 1.75),
-        Block.box(3.5, 3.25, 1.75, 4, 3.75, 3.25),
-        Block.box(10.75, 8.25, 2, 11.75, 8.75, 2.5),
-        Block.box(3.5, 3.25, 1.25, 4, 7.25, 1.75),
-        Block.box(4.25, 8.25, 2, 5.25, 8.75, 2.5),
-        Block.box(4.25, 3.25, 2, 4.75, 8.25, 2.5),
-        Block.box(4.25, 3.25, 2.5, 4.75, 3.75, 3),
-        Block.box(5, 1, 0, 11, 5, 1)
-).reduce((v1, v2) -> VoxelShapes.join(v1, v2, IBooleanFunction.OR)).get();
+    private static final VoxelShape CHARGING_STATION_E = VoxelShapeUtils.rotateY(CHARGING_STATION_N, 90);
+    private static final VoxelShape CHARGING_STATION_S = VoxelShapeUtils.rotateY(CHARGING_STATION_E, 90);
+    private static final VoxelShape CHARGING_STATION_W = VoxelShapeUtils.rotateY(CHARGING_STATION_S, 90);
+    
+    private static final VoxelShape CHARGING_PAD_N = Stream.of(
+            Block.box(4, 15.05, 4, 12, 16.05, 12),
+            Block.box(2, 2, 13, 3, 15, 14),
+            Block.box(13, 2, 13, 14, 15, 14),
+            Block.box(13, 2, 2, 14, 15, 3),
+            Block.box(2, 2, 2, 3, 15, 3),
+            Block.box(2, 15, 2, 14, 16, 14),
+            Block.box(2, 13, 3, 3, 14, 13),
+            Block.box(13, 13, 3, 14, 14, 13),
+            Block.box(3, 13, 13, 13, 14, 14),
+            Block.box(3, 13, 2, 13, 14, 3),
+            Block.box(5, 12.75, 0.25, 11, 16.05, 4),
+            Block.box(5.7, 11.2, 1.7, 6.3, 12.8, 2.3),
+            Block.box(6.95, 11.2, 1.7, 7.55, 12.8, 2.3),
+            Block.box(11.45, 9.2, 1.2, 12.05, 13.8, 1.8),
+            Block.box(10.95, 9.2, 1.2, 11.55, 9.8, 1.8),
+            Block.box(10.95, 13.2, 1.2, 11.55, 13.8, 1.8)
+    ).reduce((v1, v2) -> VoxelShapes.join(v1, v2, IBooleanFunction.OR)).get();
 
-    private static final VoxelShape SHAPE_E = VoxelShapeUtils.rotateY(SHAPE_N, 90);
-    private static final VoxelShape SHAPE_S = VoxelShapeUtils.rotateY(SHAPE_E, 90);
-    private static final VoxelShape SHAPE_W = VoxelShapeUtils.rotateY(SHAPE_S, 90);
-    private static final VoxelShape[] SHAPES = new VoxelShape[] { SHAPE_S, SHAPE_W, SHAPE_N, SHAPE_E };
+    private static final VoxelShape CHARGING_PAD_E = VoxelShapeUtils.rotateY(CHARGING_PAD_N, 90);
+    private static final VoxelShape CHARGING_PAD_S = VoxelShapeUtils.rotateY(CHARGING_PAD_E, 90);
+    private static final VoxelShape CHARGING_PAD_W = VoxelShapeUtils.rotateY(CHARGING_PAD_S, 90);
 
-//    private static final VoxelShape PAD_SHAPE = VoxelShapes.join(SHAPES, PAD_FRAME, IBooleanFunction.OR);
+    private static final VoxelShape CHARGING_STATION_WITH_PAD_N = VoxelShapes.join(CHARGING_STATION_N, CHARGING_PAD_N, IBooleanFunction.OR);
+    private static final VoxelShape CHARGING_STATION_WITH_PAD_E = VoxelShapes.join(CHARGING_STATION_E, CHARGING_PAD_E, IBooleanFunction.OR);
+    private static final VoxelShape CHARGING_STATION_WITH_PAD_S = VoxelShapes.join(CHARGING_STATION_S, CHARGING_PAD_S, IBooleanFunction.OR);
+    private static final VoxelShape CHARGING_STATION_WITH_PAD_W = VoxelShapes.join(CHARGING_STATION_W, CHARGING_PAD_W, IBooleanFunction.OR);
+
+    private static final VoxelShape[] CHARGING_STATION = new VoxelShape[] { CHARGING_STATION_S, CHARGING_STATION_W, CHARGING_STATION_N, CHARGING_STATION_E };
+    private static final VoxelShape[] CHARGING_STATION_WITH_PAD = new VoxelShape[] { CHARGING_STATION_WITH_PAD_S, CHARGING_STATION_WITH_PAD_W, CHARGING_STATION_WITH_PAD_N, CHARGING_STATION_WITH_PAD_E };
 
     public BlockChargingStation() {
         super(ModBlocks.defaultProps());
@@ -71,8 +97,7 @@ public class BlockChargingStation extends BlockPneumaticCraftCamo {
     @Override
     public VoxelShape getUncamouflagedShape(BlockState state, IBlockReader world, BlockPos pos, ISelectionContext selectionContext) {
         Direction d = state.getValue(directionProperty());
-        return SHAPES[d.get2DDataValue()];
-//        return state.getValue(CHARGE_PAD) ? PAD_SHAPE : SHAPE;
+        return state.getValue(CHARGE_PAD) ? CHARGING_STATION_WITH_PAD[d.get2DDataValue()] : CHARGING_STATION[d.get2DDataValue()];
     }
 
     @Override
