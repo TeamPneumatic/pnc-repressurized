57fe6fba11fa364887c0dc6e81af52d3b0504469
Ridanis <khaled.chikhou@gmail.com>
2021-11-07 02:06:34 +0100

    Assembly Controller and Platform

--- src/main/java/me/desht/pneumaticcraft/client/render/tileentity/RenderAssemblyController.java
+++ src/main/java/me/desht/pneumaticcraft/client/render/tileentity/RenderAssemblyController.java
@@ -1,20 +1,3 @@
-/*
- * This file is part of pnc-repressurized.
- *
- *     pnc-repressurized is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- *
- *     pnc-repressurized is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
- *
- *     You should have received a copy of the GNU General Public License
- *     along with pnc-repressurized.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 package me.desht.pneumaticcraft.client.render.tileentity;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
@@ -55,7 +38,7 @@ public class RenderAssemblyController extends AbstractTileModelRenderer<TileEnti
         matrixStackIn.translate(-0.23D, 0.50D, -0.04D);
         matrixStackIn.mulPose(Vector3f.XP.rotationDegrees(-34));
         matrixStackIn.scale(TEXT_SIZE, TEXT_SIZE, TEXT_SIZE);
-        Minecraft.getInstance().font.drawInBatch("> " + te.displayedText, 1, 4, 0xFF4ce568, false,  matrixStackIn.last().pose(), bufferIn, false, 0, combinedLightIn);
+        Minecraft.getInstance().font.drawInBatch(te.displayedText, 1, 4, 0xFF4ce568, false,  matrixStackIn.last().pose(), bufferIn, false, 0, combinedLightIn);
 
         // possible problem icon
         if (te.hasProblem) {
