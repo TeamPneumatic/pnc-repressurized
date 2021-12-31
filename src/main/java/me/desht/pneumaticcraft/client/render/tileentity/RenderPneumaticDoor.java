b7685bfe79f2e107a2fddcb7a2631983797eaa0e
Ridanis <khaled.chikhou@gmail.com>
2021-12-31 03:13:39 +0100

    Security Station, Programmer, and Pneumatic Door

--- src/main/java/me/desht/pneumaticcraft/client/render/tileentity/RenderPneumaticDoor.java
+++ src/main/java/me/desht/pneumaticcraft/client/render/tileentity/RenderPneumaticDoor.java
@@ -32,55 +32,34 @@ import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.vector.Vector3f;
 
 public class RenderPneumaticDoor extends AbstractTileModelRenderer<TileEntityPneumaticDoor> {
-    private final ModelRenderer shape1;
-    private final ModelRenderer shape2;
-    private final ModelRenderer shape3;
-    private final ModelRenderer shape4;
-    private final ModelRenderer shape5;
-    private final ModelRenderer shape6;
-    private final ModelRenderer shape7;
-    private final ModelRenderer shape8;
-    private final ModelRenderer shape9;
-
+    private final ModelRenderer door;
+    // TODO: Allow the door to be placed left or right, like Vanilla doors
     public RenderPneumaticDoor(TileEntityRendererDispatcher dispatcher) {
         super(dispatcher);
-        
-        shape1 = new ModelRenderer(64, 32, 0, 24);
-        shape1.addBox(0F, 0F, 0F, 16, 3, 3);
-        shape1.setPos(-8F, -8F, -8F);
-        shape1.mirror = true;
-        shape2 = new ModelRenderer(64, 32, 38, 0);
-        shape2.addBox(0F, 0F, 0F, 3, 3, 3);
-        shape2.setPos(-8F, -5F, -8F);
-        shape2.mirror = true;
-        shape3 = new ModelRenderer(64, 32, 50, 0);
-        shape3.addBox(0F, 0F, 0F, 2, 3, 3);
-        shape3.setPos(-1F, -5F, -8F);
-        shape3.mirror = true;
-        shape4 = new ModelRenderer(64, 32, 38, 6);
-        shape4.addBox(0F, 0F, 0F, 3, 3, 3);
-        shape4.setPos(5F, -5F, -8F);
-        shape4.mirror = true;
-        shape5 = new ModelRenderer(64, 32, 0, 24);
-        shape5.addBox(0F, 0F, 0F, 16, 2, 3);
-        shape5.setPos(-8F, -2F, -8F);
-        shape5.mirror = true;
-        shape6 = new ModelRenderer(64, 32, 38, 12);
-        shape6.addBox(0F, 0F, 0F, 3, 3, 3);
-        shape6.setPos(-8F, 0F, -8F);
-        shape6.mirror = true;
-        shape7 = new ModelRenderer(64, 32, 50, 12);
-        shape7.addBox(0F, 0F, 0F, 2, 3, 3);
-        shape7.setPos(-1F, 0F, -8F);
-        shape7.mirror = true;
-        shape8 = new ModelRenderer(64, 32, 38, 18);
-        shape8.addBox(0F, 0F, 0F, 3, 3, 3);
-        shape8.setPos(5F, 0F, -8F);
-        shape8.mirror = true;
-        shape9 = new ModelRenderer(64, 32, 0, 0);
-        shape9.addBox(0F, 0F, 0F, 16, 21, 3);
-        shape9.setPos(-8F, 3F, -8F);
-        shape9.mirror = true;
+
+        door = new ModelRenderer(64, 64, 0, 0);
+        door.setPos(-8.0F, 3.0F, -8.0F);
+        door.texOffs(0, 0).addBox(0.0F, -11.0F, 0.0F, 16.0F, 32.0F, 3.0F, 0.0F, true);
+        door.texOffs(42, 2).addBox(1.0F, -11.0F, 2.25F, 1.0F, 32.0F, 1.0F, -0.01F, false);
+        door.texOffs(38, 2).addBox(3.0F, -11.0F, 2.25F, 1.0F, 32.0F, 1.0F, -0.01F, false);
+        door.texOffs(38, 2).addBox(3.0F, -11.0F, -0.25F, 1.0F, 32.0F, 1.0F, -0.01F, false);
+        door.texOffs(42, 2).addBox(1.0F, -11.0F, -0.25F, 1.0F, 32.0F, 1.0F, -0.01F, false);
+        door.texOffs(0, 46).addBox(0.0F, -9.0F, 2.5F, 5.0F, 1.0F, 1.0F, -0.01F, false);
+        door.texOffs(0, 44).addBox(0.0F, 18.0F, 2.5F, 5.0F, 1.0F, 1.0F, -0.01F, false);
+        door.texOffs(0, 46).addBox(0.0F, -9.0F, -0.5F, 5.0F, 1.0F, 1.0F, -0.01F, false);
+        door.texOffs(0, 44).addBox(0.0F, 18.0F, -0.5F, 5.0F, 1.0F, 1.0F, -0.01F, false);
+        door.texOffs(16, 35).addBox(0.5F, 1.0F, 3.0F, 4.0F, 8.0F, 1.0F, 0.0F, false);
+        door.texOffs(16, 35).addBox(0.5F, 1.0F, -1.0F, 4.0F, 8.0F, 1.0F, 0.0F, false);
+        door.texOffs(26, 35).addBox(1.5F, 2.0F, 4.0F, 2.0F, 2.0F, 1.0F, 0.0F, false);
+        door.texOffs(26, 35).addBox(1.5F, 2.0F, -2.0F, 2.0F, 2.0F, 1.0F, 0.0F, false);
+        door.texOffs(26, 38).addBox(2.5F, 2.5F, 4.0F, 4.0F, 1.0F, 1.0F, -0.2F, false);
+        door.texOffs(26, 38).addBox(2.5F, 2.5F, -2.0F, 4.0F, 1.0F, 1.0F, -0.2F, false);
+        door.texOffs(0, 41).addBox(9.0F, -8.0F, 2.25F, 7.0F, 2.0F, 1.0F, -0.01F, false);
+        door.texOffs(0, 38).addBox(9.0F, 9.0F, 2.25F, 7.0F, 2.0F, 1.0F, -0.01F, false);
+        door.texOffs(0, 35).addBox(9.0F, 16.0F, 2.25F, 7.0F, 2.0F, 1.0F, -0.01F, false);
+        door.texOffs(0, 41).addBox(9.0F, -8.0F, -0.25F, 7.0F, 2.0F, 1.0F, -0.01F, false);
+        door.texOffs(0, 38).addBox(9.0F, 9.0F, -0.25F, 7.0F, 2.0F, 1.0F, -0.01F, false);
+        door.texOffs(0, 35).addBox(9.0F, 16.0F, -0.25F, 7.0F, 2.0F, 1.0F, -0.01F, false);
     }
 
     @Override
@@ -99,14 +78,6 @@ public class RenderPneumaticDoor extends AbstractTileModelRenderer<TileEntityPne
         matrixStackIn.mulPose(rightGoing ? Vector3f.YN.rotationDegrees(rotation) : Vector3f.YP.rotationDegrees(rotation));
         matrixStackIn.translate((rightGoing ? -1 : 1) * -6.5F / 16F, 0, 6.5F / 16F);
 
-        shape1.render(matrixStackIn, builder, combinedLightIn, combinedOverlayIn, rgb[0], rgb[1], rgb[2], 1f);
-        shape2.render(matrixStackIn, builder, combinedLightIn, combinedOverlayIn, rgb[0], rgb[1], rgb[2], 1f);
-        shape3.render(matrixStackIn, builder, combinedLightIn, combinedOverlayIn, rgb[0], rgb[1], rgb[2], 1f);
-        shape4.render(matrixStackIn, builder, combinedLightIn, combinedOverlayIn, rgb[0], rgb[1], rgb[2], 1f);
-        shape5.render(matrixStackIn, builder, combinedLightIn, combinedOverlayIn, rgb[0], rgb[1], rgb[2], 1f);
-        shape6.render(matrixStackIn, builder, combinedLightIn, combinedOverlayIn, rgb[0], rgb[1], rgb[2], 1f);
-        shape7.render(matrixStackIn, builder, combinedLightIn, combinedOverlayIn, rgb[0], rgb[1], rgb[2], 1f);
-        shape8.render(matrixStackIn, builder, combinedLightIn, combinedOverlayIn, rgb[0], rgb[1], rgb[2], 1f);
-        shape9.render(matrixStackIn, builder, combinedLightIn, combinedOverlayIn, rgb[0], rgb[1], rgb[2], 1f);
+        door.render(matrixStackIn, builder, combinedLightIn, combinedOverlayIn, rgb[0], rgb[1], rgb[2], 1f);
     }
 }
