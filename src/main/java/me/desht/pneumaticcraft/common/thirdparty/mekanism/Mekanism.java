62c253cc7fbf0d2e5e6f780ab7ff716e0210ca04
Des Herriott <des.herriott@gmail.com>
2022-07-17 06:47:42 +0100

    hopefully fix #1052

--- src/main/java/me/desht/pneumaticcraft/common/thirdparty/mekanism/Mekanism.java
+++ src/main/java/me/desht/pneumaticcraft/common/thirdparty/mekanism/Mekanism.java
@@ -24,6 +24,7 @@ import me.desht.pneumaticcraft.common.thirdparty.IThirdParty;
 import me.desht.pneumaticcraft.lib.ModIds;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -40,11 +41,14 @@ public class Mekanism implements IThirdParty {
     @SubscribeEvent
     public static void attachHeatAdapters(AttachCapabilitiesEvent<TileEntity> event) {
         if (ConfigHelper.common().integration.mekThermalEfficiencyFactor.get() != 0 && MekanismIntegration.CAPABILITY_HEAT_HANDLER != null) {
-            if (event.getObject().getType().getRegistryName().getNamespace().equals(Names.MOD_ID)) {
-                event.addCapability(RL("pnc2mek_heat_adapter"), new PNC2MekHeatProvider(event.getObject()));
-            }
-            if (event.getObject().getType().getRegistryName().getNamespace().equals(ModIds.MEKANISM)) {
-                event.addCapability(RL("mek2pnc_heat_adapter"), new Mek2PNCHeatProvider(event.getObject()));
+            ResourceLocation registryName = event.getObject().getType().getRegistryName();
+            if (registryName != null) {
+                if (registryName.getNamespace().equals(Names.MOD_ID)) {
+                    event.addCapability(RL("pnc2mek_heat_adapter"), new PNC2MekHeatProvider(event.getObject()));
+                }
+                if (registryName.getNamespace().equals(ModIds.MEKANISM)) {
+                    event.addCapability(RL("mek2pnc_heat_adapter"), new Mek2PNCHeatProvider(event.getObject()));
+                }
             }
         }
     }
