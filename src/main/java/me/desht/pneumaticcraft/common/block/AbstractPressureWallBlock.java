5da0b27aeded794e7da58e820f9ea2c24c7b3de3
Des Herriott <des.herriott@gmail.com>
2022-09-07 00:05:00 +0100

    fix pressure glass click behaviour when formed

--- src/main/java/me/desht/pneumaticcraft/common/block/AbstractPressureWallBlock.java
+++ src/main/java/me/desht/pneumaticcraft/common/block/AbstractPressureWallBlock.java
@@ -36,6 +36,8 @@ import net.minecraft.world.phys.BlockHitResult;
 import net.minecraftforge.network.NetworkHooks;
 import org.jetbrains.annotations.Nullable;
 
+import static me.desht.pneumaticcraft.common.block.PressureChamberValveBlock.FORMED;
+
 public abstract class AbstractPressureWallBlock extends AbstractPneumaticCraftBlock implements IBlockPressureChamber, PneumaticCraftEntityBlock {
     public static final EnumProperty<PressureChamberWallBlock.WallState> WALL_STATE = EnumProperty.create("wall_state", PressureChamberWallBlock.WallState.class);
 
@@ -60,8 +62,7 @@ public abstract class AbstractPressureWallBlock extends AbstractPneumaticCraftBl
     @Override
     public InteractionResult use(BlockState state, Level world, BlockPos pos, Player player, InteractionHand hand, BlockHitResult brtr) {
         if (world.isClientSide) {
-            return !state.hasProperty(WALL_STATE) || state.getValue(WALL_STATE) == PressureChamberWallBlock.WallState.NONE ?
-                    InteractionResult.PASS : InteractionResult.SUCCESS;
+            return isFormed(state) ? InteractionResult.SUCCESS : InteractionResult.PASS;
         }
         // forward activation to the pressure chamber valve, which will open the GUI
         return PneumaticCraftUtils.getTileEntityAt(world, pos, PressureChamberWallBlockEntity.class).map(te -> {
@@ -72,6 +73,16 @@ public abstract class AbstractPressureWallBlock extends AbstractPneumaticCraftBl
             }
             return InteractionResult.FAIL;
         }).orElse(InteractionResult.FAIL);
+    }
+
+    private boolean isFormed(BlockState state) {
+        if (state.hasProperty(WALL_STATE)) {
+            return state.getValue(WALL_STATE) != PressureChamberWallBlock.WallState.NONE;
+        } else if (state.hasProperty(FORMED)) {
+            return state.getValue(FORMED);
+        } else {
+            return false;
+        }
     }
 
     @Override
