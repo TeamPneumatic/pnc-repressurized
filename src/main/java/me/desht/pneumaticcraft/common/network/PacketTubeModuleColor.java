7e3a5a106221bd1ddc090e8d9a7d03977fc020b8
Des Herriott <des.herriott@gmail.com>
2022-09-18 09:58:48 +0100

    Fix logistics module behaviour on non-basic pressure tubes #1086

--- src/main/java/me/desht/pneumaticcraft/common/network/PacketTubeModuleColor.java
+++ src/main/java/me/desht/pneumaticcraft/common/network/PacketTubeModuleColor.java
@@ -17,58 +17,41 @@
 
 package me.desht.pneumaticcraft.common.network;
 
-import me.desht.pneumaticcraft.common.block.entity.PressureTubeBlockEntity;
 import me.desht.pneumaticcraft.common.tubemodules.AbstractTubeModule;
 import me.desht.pneumaticcraft.common.tubemodules.INetworkedModule;
-import me.desht.pneumaticcraft.common.util.PneumaticCraftUtils;
-import net.minecraft.core.Direction;
 import net.minecraft.network.FriendlyByteBuf;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraftforge.network.NetworkEvent;
-
-import java.util.function.Supplier;
+import net.minecraft.world.entity.player.Player;
 
 /**
  * Received on: SERVER
  * Sent by client when logistics module colour is updated via GUI
  */
-public class PacketTubeModuleColor extends LocationIntPacket {
-    private final int ourColor;
-    private final Direction side;
+public class PacketTubeModuleColor extends PacketUpdateTubeModule {
+    private final int color;
 
     public PacketTubeModuleColor(AbstractTubeModule module) {
-        super(module.getTube().getBlockPos());
+        super(module);
 
-        this.ourColor = ((INetworkedModule) module).getColorChannel();
-        this.side = module.getDirection();
+        this.color = ((INetworkedModule) module).getColorChannel();
     }
 
     PacketTubeModuleColor(FriendlyByteBuf buffer) {
         super(buffer);
 
-        this.ourColor = buffer.readByte();
-        this.side = Direction.from3DDataValue(buffer.readByte());
+        this.color = buffer.readByte();
     }
 
     @Override
     public void toBytes(FriendlyByteBuf buf) {
         super.toBytes(buf);
 
-        buf.writeByte(ourColor);
-        buf.writeByte(side.get3DDataValue());
+        buf.writeByte(color);
     }
 
-    public void handle(Supplier<NetworkEvent.Context> ctx) {
-        ctx.get().enqueueWork(() -> {
-            ServerPlayer player = ctx.get().getSender();
-            if (player != null) {
-                PneumaticCraftUtils.getTileEntityAt(player.level, pos, PressureTubeBlockEntity.class).ifPresent(tube -> {
-                    if (tube.getModule(side) instanceof INetworkedModule net) {
-                        net.setColorChannel(ourColor);
-                    }
-                });
-            }
-        });
-        ctx.get().setPacketHandled(true);
+    @Override
+    protected void onModuleUpdate(AbstractTubeModule module, Player player) {
+        if (module instanceof INetworkedModule net) {
+            net.setColorChannel(color);
+        }
     }
 }
