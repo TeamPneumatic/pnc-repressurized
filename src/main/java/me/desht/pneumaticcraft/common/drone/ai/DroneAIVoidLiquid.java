9b5b81079c8e9971dbf2cd87a927d54c42cf84b2
Des Herriott <des.herriott@gmail.com>
2025-04-08 15:00:04 +0100

    fix void fluid widget locking up with no fluid filter
    #1424

--- src/main/java/me/desht/pneumaticcraft/common/drone/ai/DroneAIVoidLiquid.java
+++ src/main/java/me/desht/pneumaticcraft/common/drone/ai/DroneAIVoidLiquid.java
@@ -34,7 +34,7 @@ public class DroneAIVoidLiquid extends Goal {
 
     @Override
     public boolean canUse() {
-        return widget.isFluidValid(drone.getFluidTank().getFluid().getFluid());
+        return drone.getFluidTank().getFluidAmount() > 0 && widget.isFluidValid(drone.getFluidTank().getFluid().getFluid());
     }
 
     @Override
