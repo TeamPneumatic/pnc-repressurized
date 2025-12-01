a1751fd7ba54cb2aea2fc174d990c7a187d85a3d
Des Herriott <des.herriott@gmail.com>
2025-12-01 09:34:13 +0000

    A bit of extra defensive coding for semiblock removal checks

--- src/main/java/me/desht/pneumaticcraft/common/entity/semiblock/AbstractSemiblockEntity.java
+++ src/main/java/me/desht/pneumaticcraft/common/entity/semiblock/AbstractSemiblockEntity.java
@@ -59,6 +59,8 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.chunk.LevelChunk;
+import net.minecraft.world.level.chunk.status.ChunkStatus;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.HitResult;
@@ -340,8 +342,8 @@ public abstract class AbstractSemiblockEntity extends Entity implements ISemiBlo
                 getDrops().forEach(this::dropItem);
             }
 
-            if (level.isLoaded(blockPos)) {
-                level.markAndNotifyBlock(blockPos, level.getChunkAt(blockPos), getBlockState(), getBlockState(), Block.UPDATE_ALL, 512);
+            if (level.getChunk(blockPos.getX() >> 4, blockPos.getZ() >> 4, ChunkStatus.FULL, false) instanceof LevelChunk lc) {
+                level.markAndNotifyBlock(blockPos, lc, getBlockState(), getBlockState(), Block.UPDATE_ALL, 512);
             }
         }
 
