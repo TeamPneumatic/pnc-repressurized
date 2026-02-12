c7da1435457020fe67037fb7e6f279edd53c5187
iskandaralex2 <iskandaralexfixed@gmail.com>
2026-02-13 00:32:31 +0800

    Fixed PressureGaugeRenderer3D.java where it breaks with batched rendering mods such as Oculus.
    Co-authored-by: Ash <96162140+Tecc-src@users.noreply.github.com>

--- src/main/java/me/desht/pneumaticcraft/client/render/pressure_gauge/PressureGaugeRenderer3D.java
+++ src/main/java/me/desht/pneumaticcraft/client/render/pressure_gauge/PressureGaugeRenderer3D.java
@@ -20,6 +20,7 @@ package me.desht.pneumaticcraft.client.render.pressure_gauge;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import me.desht.pneumaticcraft.client.render.ModRenderTypes;
+import me.desht.pneumaticcraft.client.render.pressure_gauge.PressureGaugeRenderer2D.TextScaler;
 import me.desht.pneumaticcraft.client.util.RenderUtils;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
@@ -61,11 +62,11 @@ public class PressureGaugeRenderer3D {
         Matrix3f normal = poseStack.last().normal();
 
         // Draw the green and red surface in the gauge.
-        RenderUtils.renderWithType(poseStack, buffer, ModRenderTypes.TRIANGLE_FAN, (posMat, builder) ->
+        RenderUtils.renderWithType(poseStack, buffer, ModRenderTypes.TRIANGLES, (posMat, builder) ->
                 drawGaugeBackground(posMat, builder, minPressure, maxPressure, dangerPressure, minWorkingPressure, xPos, yPos));
 
         // Draw the surrounding circle in the foreground colour
-        RenderUtils.renderWithType(poseStack, buffer, RenderType.LINE_STRIP, (posMat, builder) ->
+        RenderUtils.renderWithType(poseStack, buffer, RenderType.LINES, (posMat, builder) ->
                 drawGaugeSurround(poseStack, builder, xPos, yPos, fgColor));
 
         // Draw the scale
@@ -75,7 +76,7 @@ public class PressureGaugeRenderer3D {
                 drawScale(poseStack, builder, minPressure, maxPressure, xPos, yPos, currentScale, textScalers));
 
         // Draw the needle.
-        RenderUtils.renderWithType(poseStack, buffer, RenderType.LINE_STRIP, (posMat, builder) -> {
+        RenderUtils.renderWithType(poseStack, buffer, RenderType.LINES, (posMat, builder) -> {
                     float angleIndicator = GAUGE_POINTS - (int) ((currentPressure - minPressure) / (maxPressure - minPressure) * GAUGE_POINTS);
                     angleIndicator = -angleIndicator / CIRCLE_POINTS * 2F * PI_F - STOP_ANGLE;
                     drawNeedle(poseStack, builder, xPos, yPos, angleIndicator, fgColor);
@@ -86,10 +87,7 @@ public class PressureGaugeRenderer3D {
     }
 
     private static void drawGaugeBackground(Matrix4f posMat, VertexConsumer builder, float minPressure, float maxPressure, float dangerPressure, float minWorkingPressure, int xPos, int yPos) {
-        // vertex builder is set up to draw GL_TRIANGLE_FAN
         float[] color = RED;
-
-        builder.addVertex(posMat, xPos, yPos, 0f).setColor(0.5f, 0.5f, 0.1f, 1f);
 
         int explodeBoundary = GAUGE_POINTS - (int) ((dangerPressure - minPressure) / (maxPressure - minPressure) * GAUGE_POINTS);
         int workingBoundary = GAUGE_POINTS - (int) ((minWorkingPressure - minPressure) / (maxPressure - minPressure) * GAUGE_POINTS);
@@ -100,20 +98,21 @@ public class PressureGaugeRenderer3D {
         for (int i = 0; i < GAUGE_POINTS; i++) {
             if (i == explodeBoundary && !changedColorGreen) {
                 color = minWorkingPressure < 0 && minWorkingPressure >= -1 ? YELLOW : GREEN;
-                builder.addVertex(posMat, xPos, yPos, 0f).setColor(color[0], color[1], color[2], color[3]);
-                i--;
                 changedColorGreen = true;
             }
             if (i == workingBoundary && !changedColorYellow) {
                 color = minWorkingPressure < 0 && minWorkingPressure >= -1 ? GREEN : YELLOW;
-                builder.addVertex(posMat, xPos, yPos, 0f).setColor(color[0], color[1], color[2], color[3]);
-                i--;
                 changedColorYellow = true;
             }
             float angle = -i / (float) CIRCLE_POINTS * 2F * PI_F - STOP_ANGLE;
+            float angle2 = -(i + 1) / (float) CIRCLE_POINTS * 2F * PI_F - STOP_ANGLE;
+
+            builder.addVertex(posMat, xPos, yPos, 0f).setColor(0.5f, 0.5f, 0.1f, 1f);
+            
             builder.addVertex(posMat, Mth.cos(angle) * RADIUS + xPos, Mth.sin(angle) * RADIUS + yPos, 0f)
-                    .setColor(color[0], color[1], color[2], color[3])
-                    ;
+                .setColor(color[0], color[1], color[2], color[3]);
+            builder.addVertex(posMat, Mth.cos(angle2) * RADIUS + xPos, Mth.sin(angle2) * RADIUS + yPos, 0f)
+            	.setColor(color[0], color[1], color[2], color[3]);
         }
     }
 
@@ -127,19 +126,19 @@ public class PressureGaugeRenderer3D {
     }
 
     private static void drawGaugeSurround(PoseStack poseStack, VertexConsumer builder, int xPos, int yPos, int fgColor) {
-        // vertex builder is set up for VertexMode.LINE_STRIP
+        // vertex builder is set up for VertexMode.LINES
         float[] cols = RenderUtils.decomposeColorF(fgColor);
         for (int i = 0; i < CIRCLE_POINTS; i++) {
             RenderUtils.normalLine(builder, poseStack,
                     GAUGE_SURROUND[i][0] + xPos, GAUGE_SURROUND[i][1] + yPos, 0f,
                     GAUGE_SURROUND[i + 1][0] + xPos, GAUGE_SURROUND[i + 1][1] + yPos, 0f,
                     cols[0], cols[1], cols[2], cols[3],
-                    true);
+                    false);
         }
     }
 
     private static void drawScale(PoseStack poseStack, VertexConsumer builder, float minPressure, float maxPressure, int xPos, int yPos, int currentScale, List<TextScaler> textScalers) {
-        // vertex builder is set up for VertexMode.LINE
+        // vertex builder is set up for VertexMode.LINES
         for (int i = 0; i <= GAUGE_POINTS; i++) {
             float angle = -i / (float) CIRCLE_POINTS * 2F * PI_F - STOP_ANGLE;
             if (i == GAUGE_POINTS - (int) ((currentScale - minPressure) / (maxPressure - minPressure) * GAUGE_POINTS)) {
@@ -159,20 +158,37 @@ public class PressureGaugeRenderer3D {
     }
 
     private static void drawNeedle(PoseStack poseStack, VertexConsumer builder, int xPos, int yPos, float angle, int fgColor) {
-        // vertex builder is set up for VertexMode.LINE_STRIP
         float[] cols = RenderUtils.decomposeColorF(fgColor);
 
         float x1 = Mth.cos(angle + 0.89F * PI_F) * RADIUS * 0.3F + xPos;
         float y1 = Mth.sin(angle + 0.89F * PI_F) * RADIUS * 0.3F + yPos;
+
         float x2 = Mth.cos(angle + 1.11F * PI_F) * RADIUS * 0.3F + xPos;
         float y2 = Mth.sin(angle + 1.11F * PI_F) * RADIUS * 0.3F + yPos;
+
         float x3 = Mth.cos(angle) * RADIUS * 0.8F + xPos;
         float y3 = Mth.sin(angle) * RADIUS * 0.8F + yPos;
 
-        RenderUtils.normalLine(builder, poseStack, x1, y1, 0f, x2, y2, 0f, cols[0], cols[1], cols[2], cols[3], true);
-        RenderUtils.normalLine(builder, poseStack, x2, y2, 0f, x3, y3, 0f, cols[0], cols[1], cols[2], cols[3], true);
-        RenderUtils.normalLine(builder, poseStack, x3, y3, 0f, x1, y1, 0f, cols[0], cols[1], cols[2], cols[3], true);
-        RenderUtils.normalLine(builder, poseStack, x1, y1, 0f, x2, y2, 0f, cols[0], cols[1], cols[2], cols[3], true);
+        // Edge 1: x1 -> x2
+        RenderUtils.normalLine(builder, poseStack,
+                x1, y1, 0f,
+                x2, y2, 0f,
+                cols[0], cols[1], cols[2], cols[3],
+                false);
+
+        // Edge 2: x2 -> x3
+        RenderUtils.normalLine(builder, poseStack,
+                x2, y2, 0f,
+                x3, y3, 0f,
+                cols[0], cols[1], cols[2], cols[3],
+                false);
+
+        // Edge 3: x3 -> x1
+        RenderUtils.normalLine(builder, poseStack,
+                x3, y3, 0f,
+                x1, y1, 0f,
+                cols[0], cols[1], cols[2], cols[3],
+                false);
     }
 
     private static void drawText(PoseStack matrixStack, MultiBufferSource buffer, int xPos, int yPos, int fgColor, List<TextScaler> textScalers) {
